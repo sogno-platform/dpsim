6cd0246bface8a2502450a902aaaa81bbc22051c
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2016-11-29 19:25:31 +0100

    update docu and synchronous generator model
    Former-commit-id: b33c81c10d1350404ab74878e9078597e21ce599

--- Source/Components/SynchronousGenerator.cpp
+++ Source/Components/SynchronousGenerator.cpp
@@ -1,1 +1,36 @@
 #include "SynchronousGenerator.h"
+
+SynchronousGenerator::SynchronousGenerator(std::string name, int src, int dest, double statorRes, double leakInd, double mutInd_d,
+	double mutInd_q, double fieldRes, double fieldLeakInd, double dampRes_d, double dampLeakInd_d, double dampRes1_q, double dampLeakInd1_q,
+	double dampRes2_q, double dampLeakInd2_q, double inertia, int poleNumber) : BaseComponent(name, src, dest) {
+
+	mStatorRes = statorRes;
+	mLeakInd = leakInd;
+	mMutInd_d = mutInd_d;
+	mMutInd_q = mutInd_q;
+	mFieldRes = fieldRes;
+	mFieldLeakInd = fieldLeakInd;
+	mDampRes_d = dampRes_d;
+	mDampRes1_q = dampRes1_q;
+	mDampRes2_q = dampRes2_q;
+	mDampLeakInd1_q = dampLeakInd1_q;
+	mDampLeakInd2_q = dampLeakInd2_q;
+	mInertia = inertia;
+	mPoleNumber = poleNumber;
+}
+
+void SynchronousGenerator::applyMatrixStamp(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, double dt) {
+	// Compensated current source
+}
+
+void SynchronousGenerator::Init(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, double dt) {
+	
+}
+
+void SynchronousGenerator::Step(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, double dt, double t) {
+	// Euler forward step with state space equations
+}
+
+void SynchronousGenerator::PostStep(DPSMatrix& g, DPSMatrix& j, DPSMatrix& vt, int compOffset, double om, double dt, double t) {
+	// Calculate actual voltage fir next step
+}
--- Source/Components/SynchronousGenerator.h
+++ Source/Components/SynchronousGenerator.h
@@ -3,24 +3,59 @@
 
 #include "BaseComponent.h"
 
-class InterfacedInductor : public BaseComponent {
-protected:
-	double inductance;
-	double voltageRe;
-	double voltageIm;
-	double currentRe;
-	double currentIm;
-	double currentStepRe;
-	double currentStepIm;
+/// Synchronous generator model
+/// all parameters are referred to the stator and variables are in rotor reference frame
 
-public:
-	InterfacedInductor() { };
-	InterfacedInductor(std::string name, int src, int dest, double inductance);
 
-	void applyMatrixStamp(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, double dt);
-	void Init(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, double dt);
-	void Step(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, double dt, double t);
-	virtual void PostStep(DPSMatrix& g, DPSMatrix& j, DPSMatrix& vt, int compOffset, double om, double dt, double t);
+class SynchronousGenerator : public BaseComponent {
+	protected:
+		/// nominal power Pn [VA]
+		double mNomPower;
+		/// nominal voltage Vn [V] (RMS)
+		double mNomVolt;
+		/// nominal frequency fn [Hz]
+		double mNomFreq;
+		/// nominal field current Ifn [A]
+		double mNomFieldCur;
+		/// stator resistance Rs[Ohm]
+		double mStatorRes;
+		/// leakage inductance Ll [H]
+		double mLeakInd;
+		/// d-axis mutual inductance Lmd [H]
+		double mMutInd_d;
+		/// q-axis mutual inductance Lmq [H]
+		double mMutInd_q;
+		/// field resistance Rfd [Ohm]
+		double mFieldRes;
+		/// field leakage inductance Llfd [H]
+		double mFieldLeakInd;
+		/// d-axis damper resistance Rkd [Ohm]
+		double mDampRes_d;
+		/// d-axis damper leakage inductance Llkd [H]
+		double mDampLeakInd_d;
+		/// q-axis damper resistance 1 Rkq1 [Ohm]
+		double mDampRes1_q;
+		/// q-axis damper leakage inductance 1 Llkq1 [H]
+		double mDampLeakInd1_q;
+		/// q-axis damper resistance 2 Rkq2 [Ohm]
+		double mDampRes2_q;
+		/// q-axis damper leakage inductance 2 Llkq2 [H]
+		double mDampLeakInd2_q;
+		/// inertia J [kg*m^2]
+		double mInertia;
+		/// number of poles
+		int mPoleNumber;
+
+	public:
+		SynchronousGenerator() { };
+		SynchronousGenerator(std::string name, int src, int dest, double statorRes, double leakInd, double mutInd_d, 
+			double mutInd_q, double fieldRes, double fieldLeakInd, double dampRes_d, double dampLeakInd_d, double dampRes1_q, double dampLeakInd1_q,
+			double dampRes2_q, double dampLeakInd2_q, double inertia, int poleNumber);
+		
+		void applyMatrixStamp(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, double dt);
+		void Init(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, double dt);
+		void Step(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, double dt, double t);
+		void PostStep(DPSMatrix& g, DPSMatrix& j, DPSMatrix& vt, int compOffset, double om, double dt, double t);
 
 };
 #endif
