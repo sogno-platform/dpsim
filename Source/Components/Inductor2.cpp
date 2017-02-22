c5f2305caa72b391c41ae6bb5b792076dd8a5011
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2017-02-22 15:36:31 +0100

    adapted inductor2
    Former-commit-id: 7c5935ff6f3e9fe2ba974285e1556df75a3deaf0

--- Source/Components/Inductor2.cpp
+++ Source/Components/Inductor2.cpp
@@ -4,7 +4,7 @@ Inductor2::Inductor2(std::string name, int src, int dest, double inductance) : B
 	this->inductance = inductance;
 }
 
-void Inductor2::applyMatrixStamp(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, double dt) {
+void Inductor2::applySystemMatrixStamp(DPSMatrix& g, int compOffset, double om, double dt) {
 	double a = dt/(2*inductance);
 	double b = dt*om/2;
 
@@ -14,36 +14,35 @@ void Inductor2::applyMatrixStamp(DPSMatrix& g, DPSMatrix& j, int compOffset, dou
 	pi = 2 * b / (1 + b*b);
 
 
-	if (node1 >= 0) {
-		g(node1, node1) = g(node1, node1) + glr;
-		g(compOffset + node1, compOffset + node1) = g(compOffset + node1, compOffset + node1) + glr;
-		g(compOffset + node1, node1) = g(compOffset + node1, node1) + gli;
-		g(node1, compOffset + node1) = g(node1, compOffset + node1) - gli;
+	if (mNode1 >= 0) {
+		g(mNode1, mNode1) = g(mNode1, mNode1) + glr;
+		g(compOffset + mNode1, compOffset + mNode1) = g(compOffset + mNode1, compOffset + mNode1) + glr;
+		g(compOffset + mNode1, mNode1) = g(compOffset + mNode1, mNode1) + gli;
+		g(mNode1, compOffset + mNode1) = g(mNode1, compOffset + mNode1) - gli;
 	}
 
-	else if (node2 >= 0) {
-		g(node2, node2) = g(node2, node2) + glr;
-		g(compOffset + node2, compOffset + node2) = g(compOffset + node2, compOffset + node2) + glr;
-		g(compOffset + node2, node2) = g(compOffset + node2, node2) + gli;
-		g(node2, compOffset + node2) = g(node2, compOffset + node2) - gli;
+	else if (mNode2 >= 0) {
+		g(mNode2, mNode2) = g(mNode2, mNode2) + glr;
+		g(compOffset + mNode2, compOffset + mNode2) = g(compOffset + mNode2, compOffset + mNode2) + glr;
+		g(compOffset + mNode2, mNode2) = g(compOffset + mNode2, mNode2) + gli;
+		g(mNode2, compOffset + mNode2) = g(mNode2, compOffset + mNode2) - gli;
 	}
 
-	if (node1 >= 0 && node2 >= 0) {
-		g(node1, node2) = g(node1, node2) - glr;
-		g(compOffset + node1, compOffset + node2) = g(compOffset + node1, compOffset + node2) - glr;
-		g(compOffset + node1, node2) = g(compOffset + node1, node2) - gli;
-		g(node1, compOffset + node2) = g(node1, compOffset + node2) + gli;
+	if (mNode1 >= 0 && mNode2 >= 0) {
+		g(mNode1, mNode2) = g(mNode1, mNode2) - glr;
+		g(compOffset + mNode1, compOffset + mNode2) = g(compOffset + mNode1, compOffset + mNode2) - glr;
+		g(compOffset + mNode1, mNode2) = g(compOffset + mNode1, mNode2) - gli;
+		g(mNode1, compOffset + mNode2) = g(mNode1, compOffset + mNode2) + gli;
 
-		g(node2, node1) = g(node2, node1) - glr;
-		g(compOffset + node2, compOffset + node1) = g(compOffset + node2, compOffset + node1) - glr;
-		g(compOffset + node2, node1) = g(compOffset + node2, node1) - gli;
-		g(node2, compOffset + node1) = g(node2, compOffset + node1) + gli;
+		g(mNode2, mNode1) = g(mNode2, mNode1) - glr;
+		g(compOffset + mNode2, compOffset + mNode1) = g(compOffset + mNode2, compOffset + mNode1) - glr;
+		g(compOffset + mNode2, mNode1) = g(compOffset + mNode2, mNode1) - gli;
+		g(mNode2, compOffset + mNode1) = g(mNode2, compOffset + mNode1) + gli;
 	}
 }
 
-void Inductor2::Init(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, double dt) {
-	applyMatrixStamp(g, j, compOffset, om, dt);
-
+void Inductor2::init(double om, double dt) {
+	
 	// Initialize internal state
 	currr = 0;
 	curri = 0;
@@ -54,7 +53,7 @@ void Inductor2::Init(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, doub
 }
 
 
-void Inductor2::Step(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, double dt, double t) {
+void Inductor2::step(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, double dt, double t) {
 	
 
 	// Initialize internal state
@@ -63,35 +62,35 @@ void Inductor2::Step(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, doub
 
 	//cout << "cureq = " << cureq << endl;
 
-	if (node1 >= 0) {
-		j(node1, 0) = j(node1, 0) - cureqr;
-		j(compOffset + node1, 0) = j(compOffset + node1, 0) - cureqi;
+	if (mNode1 >= 0) {
+		j(mNode1, 0) = j(mNode1, 0) - cureqr;
+		j(compOffset + mNode1, 0) = j(compOffset + mNode1, 0) - cureqi;
 	}
 
-	if (node2 >= 0) {
-		j(node2, 0) = j(node2, 0) + cureqr;
-		j(compOffset + node2, 0) = j(compOffset + node2, 0) + cureqi;
+	if (mNode2 >= 0) {
+		j(mNode2, 0) = j(mNode2, 0) + cureqr;
+		j(compOffset + mNode2, 0) = j(compOffset + mNode2, 0) + cureqi;
 	}
 }
 
 
-void Inductor2::PostStep(DPSMatrix& g, DPSMatrix& j, DPSMatrix& vt, int compOffset, double om, double dt, double t) {
+void Inductor2::postStep(DPSMatrix& g, DPSMatrix& j, DPSMatrix& vt, int compOffset, double om, double dt, double t) {
 	double vposr, vnegr;
 	double vposi, vnegi;
 
 	// extract solution
-	if (node1 >= 0) {
-		vposr = vt(node1, 0);
-		vposi = vt(compOffset + node1, 0);
+	if (mNode1 >= 0) {
+		vposr = vt(mNode1, 0);
+		vposi = vt(compOffset + mNode1, 0);
 	}
 	else {
 		vposr = 0;
 		vposi = 0;
 	}
 
-	if (node2 >= 0) {
-		vnegr = vt(node2, 0);
-		vnegi = vt(compOffset + node2, 0);
+	if (mNode2 >= 0) {
+		vnegr = vt(mNode2, 0);
+		vnegi = vt(compOffset + mNode2, 0);
 	}
 	else {
 		vnegr = 0;
--- Source/Components/Inductor2.h
+++ Source/Components/Inductor2.h
@@ -20,9 +20,10 @@ public:
 	Inductor2() { };
 	Inductor2(std::string name, int src, int dest, double inductance);
 
-	void applyMatrixStamp(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, double dt);
-	void Init(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, double dt);
-	void Step(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, double dt, double t);
-	void PostStep(DPSMatrix& g, DPSMatrix& j, DPSMatrix& vt, int compOffset, double om, double dt, double t);
+	void applySystemMatrixStamp(DPSMatrix& g, int compOffset, double om, double dt);
+	void applyRightSideVectorStamp(DPSMatrix& j, int compOffset, double om, double dt) { }
+	void init(double om, double dt);
+	void step(DPSMatrix& g, DPSMatrix& j, int compOffset, double om, double dt, double t);
+	void postStep(DPSMatrix& g, DPSMatrix& j, DPSMatrix& vt, int compOffset, double om, double dt, double t);
 };
 #endif
