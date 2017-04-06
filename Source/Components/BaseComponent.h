0aa58f7fbe378f52e3d29d524e6cdcde25bdeba4
Viviane <vsapucaia@eonerc.rwth-aachen.de>
2017-04-06 17:42:08 +0200

    comment added to code
    Former-commit-id: 941f1e095ed73e9dfc5bd5241164701b091931a0

--- Source/Components/BaseComponent.h
+++ Source/Components/BaseComponent.h
@@ -13,9 +13,17 @@ namespace DPsim {
 	/// Base class for all elements that might be added to the matrix.
 	class BaseComponent {
 	protected:
+
+		/// Component name
 		std::string mName;
+
+		/// Component node 1
 		int mNode1;
+
+		/// Component node 2
 		int mNode2;
+
+		/// Component node 3
 		int mNode3;
 
 	public:
@@ -26,16 +34,28 @@ namespace DPsim {
 		BaseComponent(std::string name, int node1, int node2, int node3) { this->mName = name;  this->mNode1 = node1 - 1; this->mNode2 = node2 - 1; this->mNode3 = node3 - 1; }
 		virtual ~BaseComponent() { }
 
+		/// get value of node1
 		int getNode1() { return mNode1; }
+		/// get value of node2
 		int getNode2() { return mNode2; }
+		/// get value of node3
 		int getNode3() { return mNode3; }
 
 		std::string getName() { return mName; }
 
+		/// Initializes variables of components
 		virtual void init(Real om, Real dt) { }
+
+		/// Stamps conductance matrix
 		virtual void applySystemMatrixStamp(SystemModel& system) = 0;
-		virtual void applyRightSideVectorStamp(SystemModel& system) { }		
+
+		/// Stamps current source vector
+		virtual void applyRightSideVectorStamp(SystemModel& system) { }
+
+		/// Upgrade values on the current source vector
 		virtual void step(SystemModel& system, Real time) { }
+
+		/// Upgrade variable values based on the solution of the step
 		virtual void postStep(SystemModel& system) { }
 	};
 }
