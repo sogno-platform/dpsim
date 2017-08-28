5dbf0a407da567b576fa619583799425a3b0557b
Georg Reinke <georg.reinke@rwth-aachen.de>
2017-08-28 10:42:30 +0200

    add basic command-line argument parsing
    Former-commit-id: b552be917496ecdf7aca6a9c5b4f5fe426c2296c

--- Source/DPsimMain.cpp
+++ Source/DPsimMain.cpp
@@ -5,20 +5,86 @@
 
 using namespace DPsim;
 
-int main(int argc, char* argv[]) {
-	// TODO: parse frequency, timestep etc. as command-line flags
-	CIMReader reader(2*PI*50);
-	Logger log("cim.log"), llog("lvector-cim.csv"), rlog("rvector-cim.csv");
-	for (int i = 1; i < argc; i++) {
-		if (!reader.addFile(argv[i]))
+void usage() {
+	std::cerr << "usage: DPsim [OPTIONS] CIM_FILE..." << std::endl
+	  << "Possible options:" << std::endl
+	  << "  -d/--duration DURATION:   simulation duration in seconds (default: 0.3)" << std::endl
+	  << "  -h/--help:                show this help and exit" << std::endl
+	  << "  -f/--frequency FREQUENCY: system frequency in Hz (default: 50)" << std::endl
+	  << "  -t/--timestep TIMESTEP:   simulation timestep in seconds (default: 1e-3)" << std::endl;
+}
+
+bool parseFloat(const char *s, double *d) {
+	char *end;
+	*d = std::strtod(s, &end);
+	return (end != s && !*end);
+}
+
+int main(int argc, const char* argv[]) {
+	Real frequency = 2*PI*50, timestep = 0.001, duration = 0.3;
+	int i;
+
+	// Parse arguments
+	for (i = 1; i < argc; i++) {
+		if (!strcmp(argv[i], "-f") || !strcmp(argv[i], "--frequency")) {
+			if (i == argc-1) {
+				std::cerr << "Missing argument for -f/--frequency; see 'DPsim --help' for usage" << std::endl;
+				return 1;
+			}
+			if (!parseFloat(argv[++i], &frequency) || frequency <= 0) {
+				std::cerr << "Invalid setting " << argv[i] << " for system frequency" << std::endl;
+				return 1;
+			}
+			frequency *= 2*PI;
+		} else if (!strcmp(argv[i], "-t") || !strcmp(argv[i], "--timestep")) {
+			if (i == argc-1) {
+				std::cerr << "Missing argument for -t/--timestep; see 'DPsim --help' for usage" << std::endl;
+				return 1;
+			}
+			if (!parseFloat(argv[++i], &timestep) || timestep <= 0) {
+				std::cerr << "Invalied setting " << argv[i] << " for the timestep" << std::endl;
+				return 1;
+			}
+		} else if (!strcmp(argv[i], "-d") || !strcmp(argv[i], "--duration")) {
+			if (i == argc-1) {
+				std::cerr << "Missing argument for -d/--duration; see 'DPsim --help' for usage" << std::endl;
+				return 1;
+			}
+			if (!parseFloat(argv[++i], &duration) || duration <= 0) {
+				std::cerr << "Invalid setting " << argv[i] << " for the duration" << std::endl;
+				return 1;
+			}
+		} else if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")) {
+			usage();
+			return 0;
+		} else if (argv[i][0] == '-') {
+			std::cerr << "Unknown option " << argv[i] << " ; see 'DPsim --help' for usage" << std::endl;
+			return 1;
+		} else {
+			// remaining arguments treated as input files
+			break;
+		}
+	}
+	if (i == argc) {
+		std::cerr << "No input files given (see DPsim --help for usage)" << std::endl;
+		return 1;
+	}
+
+	// Parse CIM files
+	CIMReader reader(frequency);
+	for (; i < argc; i++) {
+		if (!reader.addFile(argv[i])) {
 			std::cerr << "Failed to read file " << argv[i] << std::endl;
+			return 1;
+		}
 	}
 	std::vector<BaseComponent*> components = reader.mapComponents();
-	Simulation sim(components, 2*PI*50, 0.001, 0.3, log);
-	std::cout << "Start simulation." << std::endl;
+
+	// Do the actual simulation
+	Logger log("cim.log"), llog("lvector-cim.csv"), rlog("rvector-cim.csv");
+	Simulation sim(components, frequency, timestep, duration, log);
 	while (sim.step(log, llog, rlog))
 		sim.increaseByTimeStep();
-	std::cout << "Simulation finished." << std::endl;
-	return 0;	
+	return 0;
 }
 
