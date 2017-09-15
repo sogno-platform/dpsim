4a9609ad0685c6f24b03fb18d1d486924e08c71f
Georg Reinke <georg.reinke@rwth-aachen.de>
2017-09-15 17:58:18 +0200

    simplify load_cim
    Former-commit-id: d110691993033d1c165c83ddc08ed924efac9ea8

--- Source/PyComponent.cpp
+++ Source/PyComponent.cpp
@@ -108,19 +108,8 @@ PyObject* DPsim::pyLoadCim(PyObject* self, PyObject* args) {
 	std::vector<BaseComponent*> comps = reader->getComponents();
 	list = PyList_New(comps.size());
 	for (int i = 0; i < comps.size(); i++) {
-		// corresponds to "pyComp = dpsim.Component()" in Python
-		PyObject *emptyTuple = PyTuple_New(0);
-		PyComponent* pyComp = (PyComponent*) PyObject_CallObject((PyObject*) &PyComponentType, emptyTuple);
-		Py_DECREF(emptyTuple);
-		if (!pyComp) {
-			Py_DECREF(list);
-			delete reader;
-			return nullptr;
-		}
-		/*
 		PyComponent* pyComp = PyObject_New(PyComponent, &PyComponentType);
 		PyObject_Init((PyObject*) pyComp, &PyComponentType);
-		*/
 		pyComp->comp = comps[i];
 		PyList_SET_ITEM(list, i, (PyObject*) pyComp);
 	}
