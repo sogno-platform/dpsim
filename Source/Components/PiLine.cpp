fed1b300adf37f81a4d971ce4b664825118a7dcb
Steffen Vogel <post@steffenvogel.de>
2017-10-07 19:58:57 +0200

    added GPLv3 headers to source files (closes #9)
    Former-commit-id: ad2dea7fb287fa4cfe2a1a8ef2e9386537f5f35d

--- Source/Components/PiLine.cpp
+++ Source/Components/PiLine.cpp
@@ -1,3 +1,25 @@
+/** Pi Line
+ *
+ * @author Markus Mirz <mmirz@eonerc.rwth-aachen.de>
+ * @copyright 2017, Institute for Automation of Complex Power Systems, EONERC
+ * @license GNU General Public License (version 3)
+ *
+ * DPsim
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *********************************************************************************/
+
 #include "PiLine.h"
 
 using namespace DPsim;
