d292367100a16de2a5c6b342123102cc0e57472d
Steffen Vogel <post@steffenvogel.de>
2017-10-08 03:25:11 +0200

    fix include path
    Former-commit-id: 2550a63b15edd7fa0e6b0ab816678fa59fbab5f1

--- Source/Components/SynchronGeneratorEMTFnP.cpp
+++ Source/Components/SynchronGeneratorEMTFnP.cpp
@@ -21,7 +21,7 @@
  *********************************************************************************/
 
 #include "SynchronGeneratorEMTFnP.h"
-#include "..\IntegrationMethod.h"
+#include "IntegrationMethod.h"
 
 using namespace DPsim;
 
