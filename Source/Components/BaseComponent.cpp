09ee417ea493dab486207131d13a0d0b83dbaaf3
Steffen Vogel <post@steffenvogel.de>
2017-11-07 18:19:54 +0100

    remove #pragma once from source file
    Former-commit-id: a8ea943c33803581c3bfe29fdc558c5d76fe1414

--- Source/Components/BaseComponent.cpp
+++ Source/Components/BaseComponent.cpp
@@ -20,8 +20,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *********************************************************************************/
 
-#pragma once
-
 #ifndef _MSC_VER
   #include <cxxabi.h>
 #endif
