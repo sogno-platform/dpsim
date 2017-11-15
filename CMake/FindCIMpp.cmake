6673dc814bda8b4fef09bf5f30ff1bec4ffca563
Steffen Vogel <post@steffenvogel.de>
2017-11-15 16:57:43 +0100

    more improvements to CMake
    Former-commit-id: 9069f2e53cca043406e38f3a94c29461f3b2ddb6

--- CMake/FindCIMpp.cmake
+++ CMake/FindCIMpp.cmake
@@ -1,26 +1,34 @@
-option(CIMPP_DIR "Location of CIMpp library" ${CMAKE_CURRENT_SOURCE_DIR}/../cimpp)
+option(CIMPP_DIR "CIM++ installation directory" ../CIMpp)
 
-if(IS_DIRECTORY ${CIMPP_DIR})
-    set(CIMPP_INCLUDE_DIR ${CIMPP_DIR})
-else()
-    find_path(CIMPP_INCLUDE_DIR
-        NAMES CIMModel.hpp
-        PATH_SUFFIXES
-            cimpp/16v29a_12v08
-            cimpp/16v29a
-            cimpp/17v07
-    )
-endif()
+file(TO_CMAKE_PATH $ENV{APPDATA} APPDATA)
 
-find_library(CIMPP_LIBRARY NAMES cimpp libcimpp)
+find_path(CIMPP_INCLUDE_DIR
+	NAMES CIMModel.hpp
+	PATH_SUFFIXES
+		cimpp/16v29a_12v08
+		cimpp/16v29a
+		cimpp/17v07
+	PATHS
+		../CIMpp/include
+		${APPDATA}/CIMpp/include
+		${CIMPP_DIR}
+)
+
+find_library(CIMPP_LIBRARY
+	NAMES cimpp
+	PATH_SUFFIXES
+		lib/static
+	PATHS
+		../CIMpp
+		${APPDATA}/CIMpp
+		${CIMPP_DIR}
+)
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set CIMPP_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(CIMpp DEFAULT_MSG
-        CIMPP_LIBRARY CIMPP_INCLUDE_DIR)
-
+find_package_handle_standard_args(CIMpp DEFAULT_MSG CIMPP_LIBRARY CIMPP_INCLUDE_DIR)
 mark_as_advanced(CIMPP_INCLUDE_DIR CIMPP_LIBRARY)
 
 set(CIMPP_LIBRARIES ${CIMPP_LIBRARY})
-set(CIMPP_INCLUDE_DIRS ${CIMPP_INCLUDE_DIR} ${CIMPP_DIR}/cmake-build-debug/include)
+set(CIMPP_INCLUDE_DIRS ${CIMPP_INCLUDE_DIR})
