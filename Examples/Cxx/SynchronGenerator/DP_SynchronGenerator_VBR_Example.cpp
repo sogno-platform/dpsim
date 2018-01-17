627a19c7779adc81aada56a3c502dc4952c830d2
Viviane Sapucaia <vsapucaia@eonerc.rwth-aachen.de>
2018-01-17 15:47:46 +0100

    Changed name of example DP_SynchronGenerator_VBR
    Former-commit-id: 64ad86cd2910a52974ecbd840a4b58fa7cd6fa9b

--- Examples/Cxx/SynchronGenerator/DP_SynchronGenerator_VBR_Example.cpp
+++ Examples/Cxx/SynchronGenerator/DP_SynchronGenerator_VBR_Example.cpp
@@ -1,4 +1,4 @@
-/** SynGenVBRDP Example
+﻿/** SynGenVBRDP Example
  *
  * @author Markus Mirz <mmirz@eonerc.rwth-aachen.de>
  * @copyright 2017, Institute for Automation of Complex Power Systems, EONERC
@@ -58,23 +58,23 @@ int main(int argc, char* argv[])
 	//Real Llkq2 = 0;
 
 	// Declare circuit components
-	Components::Base::Ptr gen = SynchronGeneratorVBR::make("gen", 1, 2, 3,
+	Components::Base::Ptr gen = SynchronGeneratorVBRNew::make("gen", 0, 1, 2,
 		nomPower, nomPhPhVoltRMS, nomFreq, poleNum, nomFieldCurr,
 		Rs, Ll, Lmd, Lmd0, Lmq, Lmq0, Rfd, Llfd, Rkd, Llkd, Rkq1, Llkq1, Rkq2, Llkq2, H);
 	Real loadRes = 1037.8378;
-	Components::Base::Ptr r1 = Resistor::make("r1", 1, 0, loadRes);
-	Components::Base::Ptr r2 = Resistor::make("r2", 2, 0, loadRes);
-	Components::Base::Ptr r3 = Resistor::make("r3", 3, 0, loadRes);
+	Components::Base::Ptr r1 = Resistor::make("r1", 0, GND, loadRes);
+	Components::Base::Ptr r2 = Resistor::make("r2", 1, GND, loadRes);
+	Components::Base::Ptr r3 = Resistor::make("r3", 2, GND, loadRes);
 
 	Components::Base::List comps = { gen, r1, r2, r3 };
 
 	// Declare circuit components for resistance change
 	Real breakerRes = 0.001;
-	Components::Base::Ptr rBreaker1 = Resistor::make("rbreak1", 1, 0, breakerRes);
-	Components::Base::Ptr rBreaker2 = Resistor::make("rbreak2", 2, 0, breakerRes);
-	Components::Base::Ptr rBreaker3 = Resistor::make("rbreak3", 3, 0, breakerRes);
+	Components::Base::Ptr rBreaker1 = Resistor::make("rbreak1", 0, GND, breakerRes);
+	Components::Base::Ptr rBreaker2 = Resistor::make("rbreak2", 1, GND, breakerRes);
+	Components::Base::Ptr rBreaker3 = Resistor::make("rbreak3", 2, GND, breakerRes);
 
-	Components::Base::List compsBreakerOn = { rBreaker1, rBreaker2, rBreaker3, r1, r2, r3 };
+	Components::Base::List compsBreakerOn = { gen, rBreaker1, rBreaker2, rBreaker3, r1, r2, r3 };
 
 	// Set up simulation
 	Real tf, dt, t;
@@ -93,7 +93,7 @@ int main(int argc, char* argv[])
 	Real initVoltAngle = -DPS_PI / 2;
 	Real fieldVoltage = 7.0821;
 	Real mechPower = 5.5558e5;
-	auto genPtr = std::dynamic_pointer_cast<Components::DP::SynchronGeneratorVBR>(gen);
+	auto genPtr = std::dynamic_pointer_cast<Components::DP::SynchronGeneratorVBRNew>(gen);
 	genPtr->initialize(om, dt, initActivePower, initReactivePower, initTerminalVolt, initVoltAngle, fieldVoltage, mechPower);
 
 	std::cout << "A matrix:" << std::endl;
