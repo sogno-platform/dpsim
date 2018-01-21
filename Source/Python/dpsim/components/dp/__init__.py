ae70bf1bbdf5b6490a4fe4113bbe3c7bbf250acb
Steffen Vogel <post@steffenvogel.de>
2018-01-21 15:25:29 +0100

    python: update wrapper code
    Former-commit-id: 88d56c0264b72b4eb2d0191fa86fec401fb96021

--- Source/Python/dpsim/components/dp/__init__.py
+++ Source/Python/dpsim/components/dp/__init__.py
@@ -1,8 +1,8 @@
+from _dpsim import CapacitorDP as Capacitor
 from _dpsim import InductorDP as Inductor
 from _dpsim import ResistorDP as Resistor
 from _dpsim import VoltageSourceNortonDP as VoltageSourceNorton
 from _dpsim import VoltageSourceDP as VoltageSource
 from _dpsim import CurrentSourceDP as CurrentSource
 
-
-__all__ = [ 'Inductor', 'Resistor' ]
+__all__ = [ 'Inductor', 'Resistor', 'VoltageSourceNorton', 'VoltageSource', 'CurrentSource' ]
