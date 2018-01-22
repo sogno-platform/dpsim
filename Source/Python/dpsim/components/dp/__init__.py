b9aa57f473cb9a880a09826ba59a453e1a8c422a
Steffen Vogel <post@steffenvogel.de>
2018-01-22 20:17:43 +0100

    python: fix typo
    Former-commit-id: 4b95949c37f184878b145fd75c6b2eedcb8bd0f8

--- Source/Python/dpsim/components/dp/__init__.py
+++ Source/Python/dpsim/components/dp/__init__.py
@@ -8,4 +8,4 @@ from _dpsim import VoltageSourceNortonDP as VoltageSourceNorton
 from _dpsim import VoltageSourceDP as VoltageSource
 from _dpsim import CurrentSourceDP as CurrentSource
 
-__all__ = [ 'LoadPQ', 'LinePi', 'LineRx'. 'Inductor', 'Resistor', 'Capacitor', 'VoltageSourceNorton', 'VoltageSource', 'CurrentSource' ]
+__all__ = [ 'LoadPQ', 'LinePi', 'LineRx', 'Inductor', 'Resistor', 'Capacitor', 'VoltageSourceNorton', 'VoltageSource', 'CurrentSource' ]
