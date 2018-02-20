36e29206de4eb20593aefd5c721a9f5285dadb47
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2018-02-20 17:40:21 +0100

    included cimpp submodule in make files
    Former-commit-id: 30921cd7f2da45c30b4a9016b3c8d41557e50276

--- CMake/FindCIMpp.cmake
+++ CMake/FindCIMpp.cmake
@@ -1,37 +1,41 @@
 option(CIMPP_DIR "CIM++ installation directory" ../CIMpp)
+option(BUILD_WITH_CIM_SUBMODULE "Build with CIMpp as submodule" ON)
 
 if(NOT DEFINED CIM_VERSION)
-	set(CIM_VERSION 16v29a)
+	set(CIM_VERSION 16v29a)	
 endif()
 
+if (CIM_VERSION STREQUAL 16v29a)
+	set(USE_CIM_VERSION "IEC61970_16v29a")
+endif()
 
-find_path(CIMPP_INCLUDE_DIR
-	NAMES CIMModel.hpp
-	PATH_SUFFIXES
-		cimpp/${CIM_VERSION}
-		${CIM_VERSION}
-		include/src
-	PATHS
-		${CIMPP_DIR}
-		../CIMpp
-		../Libraries/CIMpp
-		../dpsim-libraries/CIMpp
-)
-
-find_library(CIMPP_LIBRARY
-	NAMES cimpp${CIM_VERSION}
-	PATH_SUFFIXES
-		lib/static
-		Debug
-		Release
-	PATHS
-		../CIMpp
-		../Libraries/CIMpp
-		../dpsim-libraries/CIMpp
-		${CIMPP_DIR}
-)
-
-if (WIN32)
+if (BUILD_WITH_CIM_SUBMODULE)
+	add_subdirectory(Source/CIM/libcimpp)	
+	set(CIMPP_LIBRARY libcimpp)
+else()
+	find_path(CIMPP_INCLUDE_DIR
+		NAMES CIMModel.hpp
+		PATH_SUFFIXES
+			cimpp/${CIM_VERSION}
+			${CIM_VERSION}
+			include/src
+		PATHS
+			${CIMPP_DIR}
+			../CIMpp
+	)
+	find_library(CIMPP_LIBRARY
+		NAMES cimpp${CIM_VERSION}
+		PATH_SUFFIXES
+			lib/static
+			Debug
+			Release
+		PATHS
+			${CIMPP_DIR}
+			../CIMpp		
+	)
+	#### Deprecated beginning ####
+	#if (WIN32)
+	if (NOT)
 	find_path(ARABICA_INCLUDE_DIR
 		NAMES convert
 		PATH_SUFFIXES
@@ -53,7 +57,11 @@ if (WIN32)
 		../Libraries/Arabica
 		../dpsim-libraries/Arabica
 	)
-endif ()
+	endif ()
+	#### Deprecated end ####
+endif()
+
+
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set CIMPP_FOUND to TRUE
