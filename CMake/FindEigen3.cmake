64c2ea0456fba33c8cc20329634844dcc1565659
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2018-03-07 18:07:02 +0100

    adding eigen github mirror for windows build
    Former-commit-id: f47aaf830468254f72f82b9e5ed54e275ffd850e

--- CMake/FindEigen3.cmake
+++ CMake/FindEigen3.cmake
@@ -1,19 +1,29 @@
-# We first try to find a version inside our local Libraries folder
-find_path(EIGEN3_INCLUDE_DIR
-	NAMES signature_of_eigen3_matrix_library
-	PATHS
-		../Eigen
-		../Libraries/Eigen
-		../dpsim-libraries/Eigen
-)
+option(WITH_EIGEN_SUBMODULE "Build with Eigen as submodule" OFF)
 
-if(NOT EIGEN3_INCLUDE_DIR)
-	set(TEMP_PATH ${CMAKE_MODULE_PATH})
+if (WITH_CIM_SUBMODULE OR WIN32)
+	# This could be used when an optimized version of eigen is required
+	# add_subdirectory(Dependencies/eigen)
+	find_path(EIGEN3_INCLUDE_DIR
+		NAMES signature_of_eigen3_matrix_library
+		PATHS
+			Dependencies/eigen
+	)
+else()
+	# We first try to find a version inside our local Libraries folder
+	find_path(EIGEN3_INCLUDE_DIR
+		NAMES signature_of_eigen3_matrix_library
+		PATHS
+			../Eigen
+			../Libraries/Eigen
+	)
 
-	# We temporarily clear the module path to avoid recursion
-	set(CMAKE_MODULE_PATH "")
-	find_package(Eigen3 REQUIRED)
-	set(CMAKE_MODULE_PATH ${TEMP_PATH})
+	if(NOT EIGEN3_INCLUDE_DIR)
+		set(TEMP_PATH ${CMAKE_MODULE_PATH})
+		# We temporarily clear the module path to avoid recursion
+		set(CMAKE_MODULE_PATH "")
+		find_package(Eigen3 REQUIRED)
+		set(CMAKE_MODULE_PATH ${TEMP_PATH})
+	endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
