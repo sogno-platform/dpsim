46884cd0da005824c151f949da5ec7b4ed1bdd7f
Steffen Vogel <post@steffenvogel.de>
2018-04-06 16:10:35 +0200

    fix whitespaces
    Former-commit-id: e06b4e4b0e5e807bb86cf3b6c5e79f6592bb8a87

--- Source/Python/Simulation.cpp
+++ Source/Python/Simulation.cpp
@@ -82,39 +82,49 @@ void Python::Simulation::simThreadFunctionRT(Python::Simulation *pySim)
 	int timerfd;
 	struct itimerspec ts;
 	uint64_t overrun;
+
 	std::unique_lock<std::mutex> lk(*pySim->mut, std::defer_lock);
 
 	pySim->numStep = 0;
+
 	// RT method is limited to timerfd right now; to implement it with Exceptions,
 	// look at Simulation::runRT
 	timerfd = timerfd_create(CLOCK_REALTIME, 0);
+
 	// TODO: better error mechanism (somehow pass an Exception to the Python thread?)
 	if (timerfd < 0) {
 		throw SystemError("Failed to create timerfd");
 	}
+
 	ts.it_value.tv_sec = (time_t) pySim->sim->getTimeStep();
 	ts.it_value.tv_nsec = (long) (pySim->sim->getTimeStep() *1e9);
 	ts.it_interval = ts.it_value;
+
 	// optional start synchronization
 	if (pySim->startSync) {
 		pySim->sim->step(false); // first step, sending the initial values
 		pySim->sim->step(true); // blocking step for synchronization + receiving the initial state of the other network
 		pySim->sim->increaseByTimeStep();
 	}
+
 	if (timerfd_settime(timerfd, 0, &ts, 0) < 0) {
 		throw SystemError("Failed to arm timerfd");
 	}
+
 	while (pySim->running && notDone) {
 		notDone = pySim->sim->step();
 		if (read(timerfd, timebuf, 8) < 0) {
 			throw SystemError("Read from timerfd failed");
 		}
+
 		overrun = *((uint64_t*) timebuf);
 		if (overrun > 1) {
 			std::cerr << "timerfd overrun of " << overrun-1 << " at " << pySim->sim->getTime() << std::endl;
 		}
+
 		pySim->numStep++;
 		pySim->sim->increaseByTimeStep();
+
 		// in case it wasn't obvious, pausing a RT simulation is a bad idea
 		// as it will most likely lead to overruns, but it's possible nonetheless
 		if (pySim->sigPause) {
@@ -126,8 +136,10 @@ void Python::Simulation::simThreadFunctionRT(Python::Simulation *pySim)
 			lk.unlock();
 		}
 	}
+
 	close(timerfd);
 	lk.lock();
+
 	pySim->state = StateDone;
 	pySim->cond->notify_one();
 }
