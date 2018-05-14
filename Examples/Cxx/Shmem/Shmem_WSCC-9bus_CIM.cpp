353a24aa85ac77a4434e7f594e34cf5ce1acc8d5
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2018-05-14 16:26:24 +0200

    adding shmem cim example
    Former-commit-id: 7e4fd5cd7605ab0fe611a2990cdcd12a69f9d1fe

--- Examples/Cxx/Shmem/Shmem_WSCC-9bus_CIM.cpp
+++ Examples/Cxx/Shmem/Shmem_WSCC-9bus_CIM.cpp
@@ -23,19 +23,22 @@
 #include <list>
 
 #include "DPsim_MNA.h"
-#include "cps/CIM/Reader.h"
 #include "cps/Interfaces/ShmemInterface.h"
+#include "cps/CIM/Reader.h"
 
 using namespace DPsim;
+using namespace CPS;
 using namespace CPS::Components::DP;
 
 int main(int argc, char *argv[]) {
+
 	// Specify CIM files
 #ifdef _WIN32
 	String path("..\\..\\..\\..\\dpsim\\Examples\\CIM\\WSCC-09_Neplan_RX\\");
 #elif defined(__linux__) || defined(__APPLE__)
 	String path("../../../dpsim/Examples/CIM/IEEE-09_Neplan_RX/");
 #endif
+
 	std::list<String> filenames = {
 		path + "WSCC-09_Neplan_RX_DI.xml",
 		path + "WSCC-09_Neplan_RX_EQ.xml",
@@ -43,28 +46,37 @@ int main(int argc, char *argv[]) {
 		path + "WSCC-09_Neplan_RX_TP.xml"
 	};
 
-	// Read CIM data
 	CIM::Reader reader(50, Logger::Level::INFO, Logger::Level::INFO);
-	reader.addFiles(filenames);
-	reader.parseFiles();
-	SystemTopology system = reader.getSystemTopology();
+	SystemTopology sys = reader.loadCIM(filenames);
 
-	// Extend system with current source
-	auto ecs = CurrentSource::make("v_s", 0, GND, Complex(0, 0));
-	system.mComponents.push_back(ecs);
-
-	// Add shared memory interface
+	Simulation sim(sys, 0.0001, 0.1,
+		Solver::Domain::DP, Solver::Type::MNA, Logger::Level::DEBUG);
+	
+	// Create shmem interface
 	struct shmem_conf conf;
-	conf.samplelen = 4;
+	conf.samplelen = 64;
 	conf.queuelen = 1024;
 	conf.polling = false;
-	in = "/dpsim10";
-	out = "/dpsim01";
-	ShmemInterface shmem(in, out, &conf);
-	shmem.registerControllableSource(ecs, GND, 0);
-	shmem.registerExportedVoltage(ecs, 0, 1);
+	String in  = "/dpsim10";
+	String out = "/dpsim01";
+	ShmemInterface shmem(out, in, &conf);
 
-	MnaSimulation sim("CIM", system, 0.0001, 0.1, SimulationType::DP, Logger::Level::DEBUG);
+	// Register exportable node voltages
+	shmem.registerExportedAttribute(sys.mNodes[0]->findAttribute<Complex>("voltage"), 1.0, 0, 1);
+	shmem.registerExportedAttribute(sys.mNodes[1]->findAttribute<Complex>("voltage"), 1.0, 1, 2);
+	shmem.registerExportedAttribute(sys.mNodes[2]->findAttribute<Complex>("voltage"), 1.0, 3, 4);
+	shmem.registerExportedAttribute(sys.mNodes[3]->findAttribute<Complex>("voltage"), 1.0, 5, 6);
+	shmem.registerExportedAttribute(sys.mNodes[4]->findAttribute<Complex>("voltage"), 1.0, 7, 8);
+	shmem.registerExportedAttribute(sys.mNodes[5]->findAttribute<Complex>("voltage"), 1.0, 9, 10);
+	shmem.registerExportedAttribute(sys.mNodes[6]->findAttribute<Complex>("voltage"), 1.0, 11, 12);
+	shmem.registerExportedAttribute(sys.mNodes[7]->findAttribute<Complex>("voltage"), 1.0, 13, 14);
+	shmem.registerExportedAttribute(sys.mNodes[8]->findAttribute<Complex>("voltage"), 1.0, 15, 16);
+
+	// TODO
+	// Extend system with controllable load
+	// Register controllable load	
+
+	sim.addInterface(&shmem);
 	sim.run();
 
 	return 0;
