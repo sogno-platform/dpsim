0e74fca210aa57d4fb89486a00307705583aca3e
Steffen Vogel <post@steffenvogel.de>
2018-05-16 12:09:21 +0200

    examples: update Python version of ShmemDistributedDirect test
    Former-commit-id: 1dc4020b4335e253f4d4320e77704fee6e2c0429

--- Examples/Python/Shmem/ShmemDistributedDirect.py
+++ Examples/Python/Shmem/ShmemDistributedDirect.py
@@ -1,48 +1,70 @@
 import sys
+import threading
 import dpsim as dps
 import dpsim.components.dp as dp
 
-def left():
-    # Nodes
-    gnd = dps.Node.GND()
-    n1  = dps.Node("n1")
-    n2  = dps.Node("n2")
+class Left(threading.Thread):
+    def run(self):
+        # Nodes
+        gnd = dps.Node.GND()
+        n1  = dps.Node("n1")
+        n2  = dps.Node("n2")
 
-    evs = dp.VoltageSource("v_ext", [n2, gnd], 0+0j)
-    vs = dp.VoltageSourceNorton("v_s", [n1, gnd], 10000+0j, 1)
-    l1 = dp.Inductor("l_1", [n1, n2], 1e-3)
+        vs = dp.VoltageSourceNorton("v_s", [n1, gnd], 10000+0j, 1)
+        evs = dp.VoltageSource("v_ext", [n2, gnd], 0+0j)
+        l1 = dp.Inductor("l_1", [n1, n2], 1e-3)
 
-    intf = dps.open_shmem_interface("/dpsim12", "/dpsim21", samplelen = 2)
-    intf.import_attribute(evs, "voltage_ref", 0, 1)
-    intf.export_attribute(evs, "current", 0, 1)
+        intf = dps.open_interface("/dpsim12", "/dpsim21", samplelen = 2)
+        intf.import_attribute(evs, "voltage_ref", 1, 0, 1)
+        intf.export_attribute(evs, "comp_current", 1, 0, 1)
 
-    sys = dps.SystemTopology(50, [gnd, n1, n2], [evs, vs, l1])
+        sys = dps.SystemTopology(50, [gnd, n1, n2], [evs, vs, l1])
 
-    sim = dps.Simulation("shmem1", sys, duration = 1)
-    sim.add_interface(intf)
-    sim.run()
+        sim = dps.Simulation("shmem1", sys, duration = 1)
+        sim.add_interface(intf)
 
-def right():
-    # Nodes
-    gnd = dps.Node.GND()
-    n3  = dps.Node("n3")
+        print("Starting simulation on left side")
+        sim.run()
 
-    # Components
-    ecs = dp.CurrentSource("i_ext", [n3, gnd], 0+0j)
-    r1 = dp.Resistor("r_1", [n3, gnd], 1)
+class Right(threading.Thread):
+    def run(self):
+        # Nodes
+        gnd = dps.Node.GND()
+        n3  = dps.Node("n3")
 
-    intf = dps.open_interface("/dpsim21", "/dpsim12", samplelen = 2)
-    intf.import_attribute(ecs, "current_ref", 0, 1)
-    intf.export_attribute(r1, "voltage", 0, 1)
+        # Components
+        ecs = dp.CurrentSource("i_ext", [n3, gnd], 0+0j)
+        r1 = dp.Resistor("r_1", [n3, gnd], 1)
 
-    sys = dps.SystemTopology(50, [gnd, n3], [ecs, r1])
+        intf = dps.open_interface("/dpsim21", "/dpsim12", samplelen = 2)
+        intf.import_attribute(ecs, "current_ref", 1, 0, 1)
+        intf.export_attribute(r1, "comp_voltage", 1, 0, 1)
 
-    sim = dps.Simulation("shmem2", sys, duration = 1)
-    sim.add_interface(intf)
-    sim.run()
+        sys = dps.SystemTopology(50, [gnd, n3], [ecs, r1])
+
+        sim = dps.Simulation("shmem2", sys, duration = 1)
+        sim.add_interface(intf)
+
+        print("Starting simulation on right side")
+        sim.run()
+
+def test_ShmemDistributedDirect():
+    left_thread = Left()
+    right_thread = Right()
+
+    left_thread.start()
+    right_thread.start()
+
+    left_thread.join();
+    right_thread.join()
 
 if __name__ == "__main__":
-    if sys.argv[1] == "left":
-        left()
-    elif sys.argv[1] == "right":
-        right()
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "left":
+            left = Left()
+            left.run()
+        elif sys.argv[1] == "right":
+            right = Right()
+            right.run()
+    else:
+        test_ShmemDistributedDirect()
