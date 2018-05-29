86462f5678309ba1c040e4b0611e7f5b22af359f
henri.lubjuhn <henri.lubjuhn@rwth-aachen.de>
2018-05-29 15:19:01 +0200

    Fixed comments and corrected new IDA solving structure
    Former-commit-id: fdf3f1ce71cb7e8dbda42d2cac2af17f151bc8ed

--- Include/dpsim/DAESimulation.h
+++ Include/dpsim/DAESimulation.h
@@ -3,39 +3,39 @@
 #include <iostream>
 #include <vector>
 #include <list>
-#include "Simulation.h"
+#include "Solver.h"
 #include "cps/SystemTopology.h"
 //#include "Logger.h"
 #include <ida/ida.h>
-#include <ida/ida_dense.h>
+#include <ida/ida_direct.h>
+#include <sunlinsol/sunlinsol_dense.h>
+#include <sundials/sundials_types.h>
 #include <nvector/nvector_serial.h>
 
-#define NVECTOR_DATA(vec) NV_DATA_S (vec) // returns pointer to the first element of array vec
+#define NVECTOR_DATA(vec) NV_DATA_S (vec) // Returns pointer to the first element of array vec
 
 using namespace DPsim ;
 
-	class DAESimulation : public Simulation{
- //inherit from Simulation?
+	/// Simulation class which uses Differential Algebraic Equation(DAE) Systems
+	class DAESimulation : public Solver{
 	protected:
-		/// Simulation name
-		String mName;
-		///stores Simulation parameters
+		// General simulation parameters
+		/// Local copy of the SystemTopology 
 		SystemTopology DAESys;
-		// stores the required offsets for adding new components to the residual vector
+		/// Offsets vector for adding new equations to the residual vector
 		std::vector<int> offsets; 
-		//Time step
-		Real timestep;
+		/// Constant Time step
+		Real mTimestep;
 
 	public:
-		/// #### Create DAE System ####
-		//TO-DO: initilaize state vector with nodes and components;
+		/// Create solve object with given parameters
 		DAESimulation(String name,  SystemTopology system, Real dt, Real tfinal);
 		virtual ~DAESimulation() { };
-		//TO-DO: initialize Components/Nodes with inital values
+		/// Initialize Components & Nodes with inital values
 		void initialize(Component::List comps);
-		// Residual Function to be used by IDA
+		/// Residual Function of entire System
 		int DAE_residualFunction(realtype ttime, N_Vector state, N_Vector dstate_dt, N_Vector resid, void *user_data);
-		/// Run simulation until total time is elapsed.
+		/// Run simulation until total time is elapsed
 		void run();
 	};
 
--- Source/DAESimulation.cpp
+++ Source/DAESimulation.cpp
@@ -5,9 +5,9 @@
 DAESimulation::DAESimulation(String name, SystemTopology system, Real dt, Real tfinal) : DAESys(system), mName(name), timestep(dt)
 {
 
-	//defines offset vector which is composed as follows:
-	//offset[0]= # nodal voltage equations
-	//offset[1]= # of componets and their respective equations (1 per component for now as inductance is not yet considered) 
+	// defines offset vector which is composed as follows:
+	// offset[0]= # nodal voltage equations
+	// offset[1]= # of componets and their respective equations (1 per component for now as inductance is not yet considered) 
 	
 	offsets.push_back(0);
 	offsets.push_back(0);
@@ -21,46 +21,55 @@ void DAESimulation::initialize(Component::List newComponents)
 		comp->initializePowerflow(DAESys.mSystemFrequency);
 	}
 
-	// TO-DO: move init functions from run
+	// TODO: move init functions from run
 }
 
  
 void DAESimulation::run()
 {
-	void *mem = NULL;
-	//initialize state vectors
-	N_Vector state, dstate_dt; //avtol ;
-	state=state_dt = NULL;
 
 	 /* 
+	 	All required variables : 
+	    state = Vector of relevant variables for solving
+		dstate_dt = derivates with respect to time of the state vector
 	 	t0 = starting time (usually 0)
 	 	tout = time for desired solution;
 		rtol = relative tolerance
 	 	tret = time IDA reached while solving
 		abstol = scalar absolute tolerance 
+		A = template Jacobian required for solver
+		LS = linear solver object
 	 */
 
-	realtype t0, tout, rtol, ,tret , sval, *s_dtval, abstol//*atval;
+	//Allocation of all variables required by IDA
+	void *mem = NULL;
+	N_Vector state, dstate_dt; //avtol ;
+	state=state_dt = NULL;
+	realtype t0, tout, rtol ,tret , *sval, *s_dtval, abstol //*atval;
 	sval = s_dtval =NULL;
+	SUNMatrix A;
+	SUNLinearSolver LS;
+	A = NULL;
+	LS = NULL;
 	int NEQ = DAESys.mComponents.size()+DAESys.mNodes.size(); 
 	
+	// allocate state vectors
 	state = N_VNew_Serial(NEQ);
-   // if(check_flag((void *)state, "N_VNew_Serial", 0)) return;
+   
     dstate_dt = N_VNew_Serial(NEQ);
-    //if(check_flag((void *)state_dt, "N_VNew_Serial", 0)) return;
+
 	avtol = N_VNew_Serial(NEQ);
-    //if(check_flag((void *)avtol, "N_VNew_Serial", 0)) return;
 
-	///set intial values for state vector 
-
-	//set inital values of components
+	// set inital values of components
 	initialize(DAESys.mComponents);
 
-	/*vector definintion: 
+
+   /* Seting intial values for all required vectors. 
+	The state vector is  defined as follows: 
 	
 	state[0]=node0_voltage
 	state[1]=node1_voltage
-	....
+	...
 	state[n]=noden_voltage
 	state[n+1]=component0_voltage
 	state[n+2]=component0_inductance (not yet implemented)
@@ -71,49 +80,52 @@ void DAESimulation::run()
 	*/
 
 	int counter = 0;
-	sval = N_VGetArrayPointer_Serial(state);
+	sval = N_VGetArrayPointer_Serial(state); 
 	for (auto node : DAESys.mNodes) {
-
+		//initialize nodal values of state vector
 		sval[counter++]=node->getVoltage();
 
 	}
 	
 	for (auto comp : DAESys.mComponents){
-
+		//initialize  component values of state vector
 		sval[counter++]=comp->getVoltage();
 		//sval[counter++]=component inductance;
 
 	}	
 	
-	s_dtval = N_VGetArrayPointer_Serial(dstate_dt);
+	s_dtval = N_VGetArrayPointer_Serial(dstate_dt); 
 
 	/*
 		set inital values for state derivative
 		for now all equal to 0
 	*/
+
 	for (int i =0, i<(DAESys.mNodes.size()+DAESys.mComponents.size()-1), i++)
-		s_dtval[i] = 0; //TODO: add derivative calculation
+		s_dtval[i] = 0; // TODO: add derivative calculation
 
 	/*
 	atval = N_VGetArrayPointer_Serial(avtol);
 		set inital values for absolute tolerance if noise differs for each component
 	*/
 	
-	rtol = RCONST(1.0e-6); //set relative tolerance, is this correct?
-	abstol = RCONST(1.0e-1);//set absolute error
+	rtol = RCONST(1.0e-6); // set relative tolerance, is this correct?
+	abstol = RCONST(1.0e-1); // set absolute error
 	
 
 	mem = IDACreate();
-	if(check_flag((void *)mem, "IDACreate", 0)) return(1);
 
 	int retval = IDAInit(mem, DAE_residualFunction, t0, state, dstate_dt); 
-	//if(check_flag(&retval, "IDAInit", 1)) return(1);
+	// if(check_flag(&retval, "IDAInit", 1)) return(1);
 
 	retval = IDASStolerances(mem, rtol, abstol);
- 	//if(check_flag(&retval, "IDASStolerances", 1)) return(1);
+ 	// if(check_flag(&retval, "IDASStolerances", 1)) return(1);
 
 
-	retval = IDADense(mem, NEQ); // choose right solver
+	//allocate and connect Matrix A and solver LS to IDA 
+	A = SUNDenseMatrix(NEQ, NEQ); 
+	LS = SUNDenseLinearSolver(state, A);
+	retval = IDADlsSetLinearSolver(mem, LS, A);
 	
 	t0 = 0;
 	tout=timestep;
@@ -139,6 +151,8 @@ void DAESimulation::run()
 	 //N_VDestroy(avtol);
 	 N_VDestroy(state);
 	 N_VDestroy(state_dt);
+	 SUNLinSolFree(LS);
+	 SUNMatDestroy(A);
 	 
 }
 
@@ -150,12 +164,13 @@ int DAESimulation::DAE_residualFunction(realtype ttime, N_Vector state, N_Vector
 		residual[offsets[0]]=tempstate[offsets[0]]-node->voltage;
 		offsets[0]=offsets[0]+1;
 	}
-	for (auto comp : DAESys.mComponents){  	//currently only supports DP_Resistor and DP_VoltageSource
+	for (auto comp : DAESys.mComponents){  	// currently only supports DP_Resistor and DP_VoltageSource
 		comp->residual(ttime, NVECTOR_DATA(state), NVECTOR_DATA(dstate_dt), NVECTOR_DATA(resid), offsets);
 	}
 		int ret=0;
 	/*
 	Do Error checking with variable ret
 	*/
-	return ret; //if successful; positive value if recoverable error, negative if fatal error
+
+	return ret; // if successful; positive value if recoverable error, negative if fatal error
 }
