516ae53c5e052b68cb935a944cbf2c075aa0fbc9
Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
2018-09-27 10:41:41 +0200

    timer: fix invalid conversion of Timer::StartClock::time_point
    Former-commit-id: 54f9c317eaa5ce8dac1eb100cda252244945d91d

--- Include/dpsim/Timer.h
+++ Include/dpsim/Timer.h
@@ -125,17 +125,17 @@ struct timespec to_timespec(std::chrono::duration<Rep, Period> dur) {
 #include <iostream>
 #include <ctime>
 
-#ifndef _MSC_VER
 template<typename Clock, typename Duration>
-std::ostream &operator<<(std::ostream &stream,
-	const std::chrono::time_point<Clock, Duration> &time_point) {
-	const std::time_t time = Clock::to_time_t(time_point);
+std::ostream &operator<<(std::ostream &stream, const std::chrono::time_point<Clock, Duration> &time_point) {
+	const auto sys_time_point = std::chrono::time_point_cast<Clock::duration, Clock>(time_point);
+	const std::time_t time = Clock::to_time_t(sys_time_point);
+	
 #if __GNUC__ > 4 || ((__GNUC__ == 4) && __GNUC_MINOR__ > 8 && __GNUC_REVISION__ > 1)
 	// Maybe the put_time will be implemented later?
 	struct tm tm;
 	localtime_r(&time, &tm);
 	return stream << std::put_time(&tm, "%c"); // Print standard date&time
-#elif defined _MSC_VER
+#elif defined(_MSC_VER)
 	char buffer[26];
 	ctime_s(buffer, 26, &time);
 	buffer[24] = '\0';  // Removes the newline that is added
@@ -147,7 +147,6 @@ std::ostream &operator<<(std::ostream &stream,
 	return stream << buffer;
 #endif
 }
-#endif
 
 template<typename Rep, typename Period>
 std::ostream &operator<<(std::ostream &stream,
