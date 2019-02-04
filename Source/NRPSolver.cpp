1e36a4c6f4a5d9ae579168a2f4964d606cc47614
Junjie Zhang <junjie.zhang@eonerc.rwth-aachen.de>
2019-02-04 21:58:13 +0100

    add load PQ update according to profile
    Former-commit-id: 19a6bfa128e70483047432b97a7f0faa3e49b842

--- Source/NRPSolver.cpp
+++ Source/NRPSolver.cpp
@@ -802,7 +802,9 @@ void NRpolarSolver::set_solution() {
 				baseVoltage_ = line->attribute<CPS::Real>("base_Voltage")->get();
 			}
 		}
-		std::dynamic_pointer_cast<CPS::Node<CPS::Complex>>(node)->updateVoltage(sol_V_complex(node->simNode())*baseVoltage_);
+		sol_V_complex(node->simNode()) *= baseVoltage_;
+		sol_V(node->simNode()) *= baseVoltage_;
+		std::dynamic_pointer_cast<CPS::Node<CPS::Complex>>(node)->updateVoltage(sol_V_complex(node->simNode()));
 	}
 }
     
