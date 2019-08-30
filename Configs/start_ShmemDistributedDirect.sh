b88fb638edf03cc69151a9aa84c1ca86edb58aba
Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
2019-08-30 11:40:55 +0200

    configs: cleanup script
    Former-commit-id: 31b307ef57658a3fb07709c183c64bb0d866a94a

--- Configs/start_ShmemDistributedDirect.sh
+++ Configs/start_ShmemDistributedDirect.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
+
 build/Examples/Cxx/ShmemDistributedDirect 0 & P1=$!
-    build/Examples/Cxx/ShmemDistributedDirect 1 & P2=$!
+build/Examples/Cxx/ShmemDistributedDirect 1 & P2=$!
     
-    for job in $P1 $P2; do
-        wait $job || exit 1
-    done
-    
-    exit 0
+for job in $P1 $P2; do
+    wait $job || exit 1
+done
