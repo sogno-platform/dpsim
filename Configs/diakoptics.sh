01183f9b4d4196ff3fd09dd5714ff7598a2c20d5
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2019-09-30 15:42:21 +0200

    update test scripts
    Former-commit-id: 733e1518ae179bfd4aeea68dd2c4cd20998c6fc3

--- Configs/diakoptics.sh
+++ Configs/diakoptics.sh
@@ -3,7 +3,7 @@ for (( i = 0; i < 20 ; i++ ))
 do
     for (( j = 0 ; j <= 12; j = j+2 ))
     do
-        for (( k = 0 ; k <= $i; k++ ))
+        for (( k = 0 ; k <= $i+1; k++ ))
         do
             build/Examples/Cxx/WSCC_9bus_mult_diakoptics -ocopies=$i -othreads=$j -osplits=$k -oseq=0
         done
