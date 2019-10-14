8cdb1a3376b37d39bf8a7aff7362218a34e85823
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2019-10-14 14:48:38 +0200

    update rt test scripts
    Former-commit-id: 033c10ac77c57f24c073546b4a8bf7db5012cee3

--- Configs/diakoptics.sh
+++ Configs/diakoptics.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-for (( i = 0; i < 20 ; i++ ))
+for (( m = 1 ; m <= 10; m++ ))
 do
-    for (( j = 0 ; j <= 12; j = j+2 ))
+    for (( i = 0; i < 20 ; i++ ))
     do
-        for (( k = 0 ; k <= $i+1; k++ ))
+        for (( j = 0 ; j <= 12; j = j+2 ))
         do
-            build/Examples/Cxx/WSCC_9bus_mult_diakoptics -ocopies=$i -othreads=$j -osplits=$k -oseq=0
+            for (( k = 0 ; k <= $i+1; k++ ))
+            do
+                sudo taskset --all-tasks --cpu-list 12-23 chrt --fifo 99 build/Examples/Cxx/WSCC_9bus_mult_diakoptics -ocopies=$i -othreads=$j -osplits=$k -oseq=$m
+            done
         done
     done
 done
