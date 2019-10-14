dbec6e34dea99eb8826f29c39b8bd0b6661c659a
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2019-10-14 18:27:16 +0200

    update 9bus rt tests nb
    Former-commit-id: b24e569bcc9cdf5048c97187ccaeeaf2bc082dc5

--- Configs/realtime_tests/coupled.sh
+++ Configs/realtime_tests/coupled.sh
@@ -4,7 +4,7 @@ do
     for (( j = 0; j <= 12; j = j+2 ))
     do
         for (( k = 1; k <= 10; k++ ))
-	do
+	    do
             sudo taskset --all-tasks --cpu-list 12-23 chrt --fifo 99 build/Examples/Cxx/WSCC_9bus_mult_coupled -ocopies=$i -othreads=$j -oseq=$k
         done
     done
