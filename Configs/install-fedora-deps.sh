f7ddb80a30ad13cfcda530604aede69c2f5da38b
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2020-02-20 14:27:25 +0100

    add install scripts

--- Configs/install-fedora-deps.sh
+++ Configs/install-fedora-deps.sh
@@ -23,6 +23,13 @@ dnf --refresh -y install \
 	sundials-devel \
 	gsl-devel
 
+# Python
+pip3 install --user -r requirements.txt
+pip3 install --user -r requirements-jupyter.txt
+dnf -y --refresh install npm
+jupyter nbextension enable --py widgetsnbextension
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
 # VILLAS dependencies
 sudo dnf install \
     openssl-devel \
@@ -33,9 +40,17 @@ sudo dnf install \
     jansson-devel \
     libwebsockets-devel \
     mosquitto-devel \
-    
-pip3 install --user -r requirements.txt
-pip3 install --user -r requirements-jupyter.txt
-dnf -y --refresh install npm
-jupyter nbextension enable --py widgetsnbextension
-jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
+# FEIN libraries
+
+cd /tmp && \
+git clone --recurse-submodules https://github.com/CIM-IEC/libcimpp.git && \
+mkdir -p libcimpp/build && cd libcimpp/build && \
+cmake -DCMAKE_INSTALL_LIBDIR=/usr/local/lib64 -DUSE_CIM_VERSION=IEC61970_16v29a -DBUILD_SHARED_LIBS=ON -DBUILD_ARABICA_EXAMPLES=OFF -DARABICA_XML_BACKEND=USE_EXPAT .. && sudo make -j$(nproc) install && \
+rm -rf /tmp/libcimpp
+
+cd /tmp && \
+git clone --recurse-submodules https://git.rwth-aachen.de/acs/public/villas/node.git villasnode && \
+mkdir -p villasnode/build && cd villasnode/build && \
+cmake -DWITH_NODE_ETHERCAT=OFF -DCMAKE_INSTALL_LIBDIR=/usr/local/lib64 .. && sudo make -j$(nproc) install && \
+rm -rf /tmp/villasnode
