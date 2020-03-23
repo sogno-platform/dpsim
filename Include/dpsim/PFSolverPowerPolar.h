ef7c65162ab6c39a6c15d8fc41b4e1eafa543376
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2020-03-23 20:07:05 +0100

    update file headers and replace gpl with mpl in most files

--- Include/dpsim/PFSolverPowerPolar.h
+++ Include/dpsim/PFSolverPowerPolar.h
@@ -1,21 +1,9 @@
-/** PFSolverPowerPolar
- * @author Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>
- * @copyright 2019, Institute for Automation of Complex Power Systems, EONERC
+/* Copyright 2017-2020 Institute for Automation of Complex Power Systems,
+ *                     EONERC, RWTH Aachen University
  *
- * DPsim
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *********************************************************************************/
 
 #pragma once
@@ -53,7 +41,7 @@ namespace DPsim {
         void setSolution();
         /// Calculate mismatch
         void calculateMismatch();
-        
+
         // Helper methods
         /// Resize solution vector
         void resize_sol(CPS::Int n);
