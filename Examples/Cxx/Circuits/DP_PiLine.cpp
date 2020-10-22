93205f9092a3a90a2e1a4ed4e0865aa3e5f2fa75
Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>
2020-10-22 14:05:22 +0200

    update DP_Line validation examples

--- Examples/Cxx/Circuits/DP_PiLine.cpp
+++ Examples/Cxx/Circuits/DP_PiLine.cpp
@@ -21,7 +21,7 @@ using namespace CPS::DP;
 
 void simElements() {
 	Real timeStep = 0.00005;
-	Real finalTime = 0.1;
+	Real finalTime = 1;
 	String simName = "DP_PiLine_Elements";
 	Logger::setLogDir("logs/"+simName);
 
@@ -34,17 +34,24 @@ void simElements() {
 	auto vs = Ph1::VoltageSource::make("v_1");
 	vs->setParameters(CPS::Math::polar(100000, -PI/2.));
 
+	// Parametrization of components
 	Real resistance = 5;
 	Real inductance = 0.16;
-	Real capacitance = 1.0e-6 / 2.;
+	Real capacitance = 1.0e-6;
+	Real conductance = 1e-6;
+
 	auto res = Ph1::Resistor::make("R_line");
 	res->setParameters(resistance);
 	auto ind = Ph1::Inductor::make("L_line");
 	ind->setParameters(inductance);
 	auto cap1 = Ph1::Capacitor::make("Cp_1");
-	cap1->setParameters(capacitance);
+	cap1->setParameters(capacitance/2.);
 	auto cap2 = Ph1::Capacitor::make("Cp_2");
-	cap2->setParameters(capacitance);
+	cap2->setParameters(capacitance/2.);
+	auto con1 = Ph1::Resistor::make("Gp_1");
+	con1->setParameters(2./conductance);
+	auto con2 = Ph1::Resistor::make("Gp_2");
+	con2->setParameters(2./conductance);
 
 	auto load = Ph1::Resistor::make("R_load");
 	load->setParameters(10000);
@@ -55,11 +62,14 @@ void simElements() {
 	ind->connect({vn1, n2});
 	cap1->connect({n1, SimNode::GND});
 	cap2->connect({n2, SimNode::GND});
+	con1->connect({n1, SimNode::GND});
+	con2->connect({n2, SimNode::GND});
 	load->connect({ n2, SimNode::GND });
 
 	auto sys = SystemTopology(50,
 		SystemNodeList{n1, n2, vn1},
-		SystemComponentList{vs, res, ind, cap1, cap2, load});
+		SystemComponentList{vs, res, ind, cap1, cap2, con1, con2, load});
+		//SystemComponentList{vs, res, ind, cap1, cap2, load});
 		//SystemComponentList{vs, res, ind, load});
 
 	// Logging
@@ -78,7 +88,7 @@ void simElements() {
 
 void simPiLine() {
 	Real timeStep = 0.00005;
-	Real finalTime = 0.1;
+	Real finalTime = 1;
 	String simName = "DP_PiLine_Component";
 	Logger::setLogDir("logs/"+simName);
 
@@ -90,9 +100,14 @@ void simPiLine() {
 	auto vs = Ph1::VoltageSource::make("v_1");
 	vs->setParameters(CPS::Math::polar(100000, -PI/2.));
 
-	// R=5, X=50 (L=0.16), B=0.003 (C=1e-6)
+	// Parametrization of components
+	Real resistance = 5;
+	Real inductance = 0.16;
+	Real capacitance = 1.0e-6;
+	Real conductance = 1e-6;
+
 	auto line = Ph1::PiLine::make("Line");
-	line->setParameters(5, 0.16, 1e-6);
+	line->setParameters(resistance, inductance, capacitance, conductance);
 
 	auto load = Ph1::Resistor::make("R_load");
 	load->setParameters(10000);
