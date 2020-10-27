8d0d7b013cdb4b028f1821a9060e550cded3cf17
Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>
2020-10-27 17:33:32 +0100

    add emt3ph vs dp1ph comparison example for capacitor

--- Examples/Cxx/Circuits/EMT_DP_VS_RLC_Circuits.cpp
+++ Examples/Cxx/Circuits/EMT_DP_VS_RLC_Circuits.cpp
@@ -190,10 +190,105 @@ void voltageSourceInductorDP1ph() {
 	sim.run();
 }
 
+void voltageSourceCapacitorEMT3ph() {
+	Real timeStep = 0.00005;
+	Real finalTime = 1;
+	String simName = "EMT_VoltageSource_Capacitor";
+	Logger::setLogDir("logs/"+simName);
+
+	// Nodes
+	auto n1 = SimNode<Real>::make("n1", PhaseType::ABC);
+	auto n2 = SimNode<Real>::make("n2", PhaseType::ABC);
+
+	// Components
+	auto vs = EMT::Ph3::VoltageSource::make("vs1", Logger::Level::debug);
+	vs->setParameters(CPS::Math::polar(100000, 0), 50);
+
+	auto res = EMT::Ph3::Resistor::make("R1");
+	res->setParameters(Reader::singlePhaseParameterToThreePhase(5));
+
+	auto cap = EMT::Ph3::Capacitor::make("C1", Logger::Level::debug);
+	cap->setParameters(Reader::singlePhaseParameterToThreePhase(10e-3));
+
+	// Topology
+	vs->connect({ SimNode<Real>::GND, n1 });
+	res->connect({n1, n2});
+	cap->connect({n2, SimNode<Real>::GND});
+
+	auto sys = SystemTopology(50,
+		SystemNodeList{n1, n2},
+		SystemComponentList{vs, res, cap});
+
+	// Logging
+	auto logger = DataLogger::make(simName);
+	logger->addAttribute("v1", n1->attribute("v"));
+	logger->addAttribute("v2", n2->attribute("v"));
+	logger->addAttribute("iR", res->attribute("i_intf"));
+	logger->addAttribute("iC", cap->attribute("i_intf"));
+
+	// Simulation
+	Simulation sim(simName);
+	sim.setSystem(sys);
+	sim.setTimeStep(timeStep);
+	sim.setFinalTime(finalTime);
+	sim.setDomain(Domain::EMT);
+	sim.addLogger(logger);
+	sim.run();
+}
+
+void voltageSourceCapacitorDP1ph() {
+	Real timeStep = 0.00005;
+	Real finalTime = 1;
+	String simName = "DP_VoltageSource_Capacitor";
+	Logger::setLogDir("logs/"+simName);
+
+	// Nodes
+	auto n1 = SimNode<Complex>::make("n1");
+	auto n2 = SimNode<Complex>::make("n2");
+
+	// Components
+	auto vs = DP::Ph1::VoltageSource::make("vs1", Logger::Level::debug);
+	vs->setParameters(CPS::Math::polar(100000, 0));
+
+	auto res = DP::Ph1::Resistor::make("R1");
+	res->setParameters(5);
+
+	auto cap = DP::Ph1::Capacitor::make("C1", Logger::Level::debug);
+	cap->setParameters(10e-3);
+
+	// Topology
+	vs->connect({ SimNode<Complex>::GND, n1 });
+	res->connect({n1, n2});
+	cap->connect({n2, SimNode<Complex>::GND});
+
+	auto sys = SystemTopology(50,
+		SystemNodeList{n1, n2},
+		SystemComponentList{vs, res, cap});
+
+	// Logging
+	auto logger = DataLogger::make(simName);
+	logger->addAttribute("v1", n1->attribute("v"));
+	logger->addAttribute("v2", n2->attribute("v"));
+	logger->addAttribute("iR", res->attribute("i_intf"));
+	logger->addAttribute("iC", cap->attribute("i_intf"));
+
+	// Simulation
+	Simulation sim(simName);
+	sim.setSystem(sys);
+	sim.setTimeStep(timeStep);
+	sim.setFinalTime(finalTime);
+	sim.setDomain(Domain::DP);
+	sim.addLogger(logger);
+	sim.run();
+}
+
 int main(int argc, char* argv[]) {
 	voltageSourceResistorEMT3ph();
 	voltageSourceResistorDP1ph();
 
 	voltageSourceInductorEMT3ph();
 	voltageSourceInductorDP1ph();
+
+	voltageSourceCapacitorEMT3ph();
+	voltageSourceCapacitorDP1ph();
 }
