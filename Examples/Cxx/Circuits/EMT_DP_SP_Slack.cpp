d06c9cccc7f975dadb911e8de0a126758f8b9bfd
Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>
2020-11-15 12:24:57 +0100

    add slack validation for sp1ph

--- Examples/Cxx/Circuits/EMT_DP_SP_Slack.cpp
+++ Examples/Cxx/Circuits/EMT_DP_SP_Slack.cpp
@@ -20,6 +20,82 @@ using namespace DPsim;
 using namespace CPS;
 using namespace CPS::CIM;
 
+void simElementsSP1ph() {
+	Real timeStep = 0.00005;
+	Real finalTime = 1;
+	String simName = "SP_Slack_Elements";
+	Logger::setLogDir("logs/"+simName);
+
+	// Nodes
+	auto n1 = SimNode<Complex>::SimNode::make("n1");
+
+	// Components
+	auto vs = SP::Ph1::VoltageSource::make("v_1");
+	vs->setParameters(CPS::Math::polar(100000, 0));
+
+	auto load = SP::Ph1::Resistor::make("R_load");
+	load->setParameters(10000);
+
+	// Topology
+	vs->connect({ SimNode<Complex>::GND, n1 });
+	load->connect({ n1, SimNode<Complex>::GND });
+
+	auto sys = SystemTopology(50,
+		SystemNodeList{n1},
+		SystemComponentList{vs, load});
+
+	// Logging
+	auto logger = DataLogger::make(simName);
+	logger->addAttribute("v1", n1->attribute("v"));
+	logger->addAttribute("i1", vs->attribute("i_intf"));
+
+	Simulation sim(simName);
+	sim.setSystem(sys);
+	sim.setTimeStep(timeStep);
+	sim.setFinalTime(finalTime);
+	sim.addLogger(logger);
+
+	sim.run();
+}
+
+void simComponentSP1ph() {
+	Real timeStep = 0.00005;
+	Real finalTime = 1;
+	String simName = "SP_Slack_Component";
+	Logger::setLogDir("logs/"+simName);
+
+	// Nodes
+	auto n1 = SimNode<Complex>::make("n1");
+
+	// Components
+	auto sl = SP::Ph1::NetworkInjection::make("v_1");
+	sl->setParameters(CPS::Math::polar(100000, 0));
+
+	auto load = SP::Ph1::Resistor::make("R_load");
+	load->setParameters(10000);
+
+	// Topology
+	sl->connect({ n1 });
+	load->connect({ n1, SimNode<Complex>::GND });
+
+	auto sys = SystemTopology(50,
+		SystemNodeList{n1},
+		SystemComponentList{sl, load});
+
+	// Logging
+	auto logger = DataLogger::make(simName);
+	logger->addAttribute("v1", n1->attribute("v"));
+	logger->addAttribute("i1", sl->attribute("i_intf"));
+
+	Simulation sim(simName);
+	sim.setSystem(sys);
+	sim.setTimeStep(timeStep);
+	sim.setFinalTime(finalTime);
+	sim.addLogger(logger);
+
+	sim.run();
+}
+
 void simElementsDP1ph() {
 	Real timeStep = 0.00005;
 	Real finalTime = 1;
@@ -175,6 +251,9 @@ void simComponentEMT3ph() {
 }
 
 int main(int argc, char* argv[]) {
+	simElementsSP1ph();
+	simComponentSP1ph();
+
 	simElementsDP1ph();
     simComponentDP1ph();
 
