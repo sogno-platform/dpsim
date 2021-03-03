4544777c4d6f2d166f2d3ff4ed413a199ee7516c
Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>
2021-03-03 09:07:30 +0100

    add machine param processing to smib full order example
    Signed-off-by: Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>

--- Examples/Cxx/Circuits/EMT_SynGenDQ7odTrapez_OperationalParams_SMIB_Fault.cpp
+++ Examples/Cxx/Circuits/EMT_SynGenDQ7odTrapez_OperationalParams_SMIB_Fault.cpp
@@ -7,40 +7,93 @@ using namespace DPsim;
 using namespace CPS;
 using namespace CPS::CIM;
 
-// General grid parameters
-Real VnomMV = 24e3;
-Real VnomHV = 230e3;
-Real nomFreq = 60;
-Real ratio = VnomMV/VnomHV;
-Real nomOmega= nomFreq* 2*PI;
-
-Examples::Components::SynchronousGeneratorKundur::MachineParameters syngenKundur;
-Examples::Components::CIGREHVAmerican::LineParameters lineCIGREHV;
-
-// HV line parameters referred to MV side
-Real lineLength = 100;
-Real lineResistance = lineCIGREHV.lineResistancePerKm*lineLength*std::pow(ratio,2);
-Real lineInductance = lineCIGREHV.lineReactancePerKm*lineLength*std::pow(ratio,2)/nomOmega;
-Real lineCapacitance = lineCIGREHV.lineSusceptancePerKm*lineLength/std::pow(ratio,2)/nomOmega;
-Real lineConductance = 8e-2; //change to allow bigger time steps and to stabilize simulation (8e-2 used for 10us)
-
-// Breaker to trigger fault between the two lines
-Real BreakerOpen = 1e9;
-Real BreakerClosed = 0.001;
-
-//Synchronous generator 
-Real setPointActivePower=300e6;
-Real setPointVoltage=1.05*VnomMV;
-
-//Simulation parameters
-Real finalTime = 1.0;
-Real timeStep = 10e-6;
-Real startTimeFault=0.2;
-
 int main(int argc, char* argv[]) {	
 
+	// ----- PARAMETRIZATION -----
+	// General grid parameters
+	Real VnomMV = 24e3;
+	Real VnomHV = 230e3;
+	Real nomFreq = 60;
+	Real ratio = VnomMV/VnomHV;
+	Real nomOmega= nomFreq* 2*PI;
+
+	// Machine parameters synchronous generator 
+	const Examples::Components::SynchronousGeneratorKundur::MachineParameters syngenKundur;
+	Real H = syngenKundur.H;
+	Real Rs = syngenKundur.Rs;
+	Real Ld = syngenKundur.Ld;
+	Real Lq = syngenKundur.Lq;
+	Real Ld_t = syngenKundur.Ld_t;
+	Real Lq_t = syngenKundur.Lq_t;
+	Real Ld_s = syngenKundur.Ld_s;
+	Real Lq_s = syngenKundur.Lq_s;
+	Real Ll = syngenKundur.Ll;
+	Real Td0_t = syngenKundur.Td0_t;
+	Real Tq0_t = syngenKundur.Tq0_t;
+	Real Td0_s = syngenKundur.Td0_s;
+	Real Tq0_s= syngenKundur.Tq0_s;	
+
+	// Operation point synchronous generator 
+	Real setPointActivePower=300e6;
+	Real setPointVoltage=1.05*VnomMV;
+
+	// Breaker
+	Real BreakerOpen = 1e9;
+	Real BreakerClosed = 0.001;
+
+	// Line 
+	const Examples::Components::CIGREHVAmerican::LineParameters lineCIGREHV;
+	Real lineLength = 100;
+	Real lineResistance = lineCIGREHV.lineResistancePerKm*lineLength*std::pow(ratio,2); // HV parameters referred to MV side
+	Real lineInductance = lineCIGREHV.lineReactancePerKm*lineLength*std::pow(ratio,2)/nomOmega;
+	Real lineCapacitance = lineCIGREHV.lineSusceptancePerKm*lineLength/std::pow(ratio,2)/nomOmega;
+	Real lineConductance = 8e-2; //change to allow bigger time steps and to stabilize simulation (8e-2 used for 10us)
+
+	// Simulation parameters
+	String simName = "EMT_SynGenDQ7odTrapez_OperationalParams_SMIB_Fault_with_PF_Init";
+	Real finalTime = 1.0;
+	Real timeStep = 10e-6;
+	Real startTimeFault=0.2;
+
+	// Command line args processing
+	CommandLineArgs args(argc, argv);
+	if (argc > 1) {
+
+		// Simulation parameters
+		if (args.name != "dpsim")
+			simName = args.name;
+
+		// Machine parameters
+		if (args.options.find("H") != args.options.end())
+			H = args.options["H"];
+		if (args.options.find("Rs") != args.options.end())
+			Rs = args.options["Rs"];
+		if (args.options.find("Ld") != args.options.end())
+			Ld = args.options["Ld"];
+		if (args.options.find("Lq") != args.options.end())
+			Lq = args.options["Lq"];
+		if (args.options.find("Ld_t") != args.options.end())
+			Ld_t = args.options["Ld_t"];
+		if (args.options.find("Lq_t") != args.options.end())
+			Lq_t = args.options["Lq_t"];
+		if (args.options.find("Ld_s") != args.options.end())
+			Ld_s = args.options["Ld_s"];
+		if (args.options.find("Lq_s") != args.options.end())
+			Lq_s = args.options["Lq_s"];
+		if (args.options.find("Ll") != args.options.end())
+			Ll = args.options["Ll"];
+		if (args.options.find("Td0_t") != args.options.end())
+			Td0_t = args.options["Td0_t"];
+		if (args.options.find("Tq0_t") != args.options.end())
+			Tq0_t = args.options["Tq0_t"];
+		if (args.options.find("Td0_s") != args.options.end())
+			Td0_s = args.options["Td0_s"];
+		if (args.options.find("Tq0_s") != args.options.end())
+			Tq0_s = args.options["Tq0_s"];
+	}
+
 	// ----- POWERFLOW FOR INITIALIZATION -----
-	String simNamePF = "EMT_PFinit";
+	String simNamePF = simName + "_PF";
 	Logger::setLogDir("logs/" + simNamePF);
 	Real timeStepPF = finalTime;
 	Real finalTimePF = finalTime+timeStepPF;
@@ -94,8 +147,7 @@ int main(int argc, char* argv[]) {
 	simPF.addLogger(loggerPF);
 	simPF.run();
 
-	// ----- Dynamic simulation ------	
-	String simName = "EMT_3ph_SynGenDQ7odTrapez_OperationalParams_ThreePhFault";
+	// ----- DYNAMIC SIMULATION ------	
 	Logger::setLogDir("logs/"+simName);
 	
 	// Extract relevant powerflow results
@@ -114,8 +166,8 @@ int main(int argc, char* argv[]) {
 	auto gen = CPS::EMT::Ph3::SynchronGeneratorDQTrapez::make("SynGen");
 	gen->setParametersOperationalPerUnit(
 		syngenKundur.nomPower, syngenKundur.nomVoltage, syngenKundur.nomFreq, syngenKundur.poleNum, syngenKundur.nomFieldCurr,
-		syngenKundur.Rs, syngenKundur.Ld, syngenKundur.Lq, syngenKundur.Ld_t, syngenKundur.Lq_t, syngenKundur.Ld_s,
-		syngenKundur.Lq_s, syngenKundur.Ll, syngenKundur.Td0_t, syngenKundur.Tq0_t, syngenKundur.Td0_s, syngenKundur.Tq0_s, syngenKundur.H,
+		Rs, Ld, Lq, Ld_t, Lq_t, Ld_s,
+		Lq_s, Ll, Td0_t, Tq0_t, Td0_s, Tq0_s, H,
 	 	initActivePower, initReactivePower, initTerminalVolt,
 	 	initVoltAngle, syngenKundur.fieldVoltage, initMechPower
 	); 
