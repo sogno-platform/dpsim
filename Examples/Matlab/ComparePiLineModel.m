9c9eb3dcdeb21da3234b85dc18ebced3744c5ed8
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2021-03-26 11:58:37 +0100

    remove deprecated examples
    Signed-off-by: Markus Mirz <mmirz@eonerc.rwth-aachen.de>

--- Examples/Matlab/ComparePiLineModel.m
+++
@@ -1,8 +0,0 @@
-% circuit with R, L and C
-fileName1='../VisualStudio/DPsimVS2015/Logs/LeftVectorLog_SimulationExamplePiLine_0.001.csv'
-
-%model with 3 nodes
-fileName2='../VisualStudio/DPsimVS2015/Logs/LeftVectorLog_SimulationExamplePiLine2_0.001.csv';
-
-compareDpResults(fileName1,fileName2,1,1,'Model with 3 nodes x Resistor + Inductor');
-
