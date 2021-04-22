3e101e51c4148959ea75267f191cc91accb012ae
Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>
2021-04-22 20:08:17 +0200

    add sp1ph wscc together with validation notebook
    Signed-off-by: Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>

---
+++ Examples/Cxx/CIM/SP_WSCC-9bus_CIM_Dyn_Switch.cpp
@@ -0,0 +1,88 @@
+/* Copyright 2017-2020 Institute for Automation of Complex Power Systems,
+ *                     EONERC, RWTH Aachen University
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *********************************************************************************/
+
+#include <iostream>
+#include <list>
+
+#include <DPsim.h>
+
+using namespace DPsim;
+using namespace CPS::SP;
+
+int main(int argc, char *argv[]) {
+
+	// Find CIM files
+	std::list<fs::path> filenames;
+	if (argc <= 1) {
+		filenames = Utils::findFiles({
+			"WSCC-09_RX_DI.xml",
+			"WSCC-09_RX_EQ.xml",
+			"WSCC-09_RX_SV.xml",
+			"WSCC-09_RX_TP.xml"
+		}, "build/_deps/cim-data-src/WSCC-09/WSCC-09_RX_Dyn", "CIMPATH");
+	}
+	else {
+		filenames = std::list<fs::path>(argv + 1, argv + argc);
+	}
+
+	String simName = "SP_WSCC-9bus_dyn_switch";
+	Logger::setLogDir("logs/"+simName);
+
+	CPS::CIM::Reader reader(simName, Logger::Level::debug, Logger::Level::info);
+	SystemTopology sys = reader.loadCIM(60, filenames, Domain::SP, PhaseType::Single, CPS::CIM::Reader::GeneratorType::Transient);
+
+	// Extend topology with switch
+	auto sw = Ph1::Switch::make("Fault", Logger::Level::info);
+	sw->setParameters(1e12, 0.1*529);
+	sw->connect({ SimNode::GND, sys.node<SimNode>("BUS7") });
+	sw->open();
+	sys.addComponent(sw);
+
+	// Use omegNom for torque conversion in SG models for validation with PSAT
+	sys.component<Ph1::SynchronGeneratorTrStab>("GEN1")->setModelFlags(false, false);
+	sys.component<Ph1::SynchronGeneratorTrStab>("GEN2")->setModelFlags(false, false);
+	sys.component<Ph1::SynchronGeneratorTrStab>("GEN3")->setModelFlags(false, false);
+
+	// Logging
+	auto logger = DataLogger::make(simName);
+	logger->addAttribute("v1", sys.node<SimNode>("BUS1")->attribute("v"));
+	logger->addAttribute("v2", sys.node<SimNode>("BUS2")->attribute("v"));
+	logger->addAttribute("v3", sys.node<SimNode>("BUS3")->attribute("v"));
+	logger->addAttribute("v4", sys.node<SimNode>("BUS4")->attribute("v"));
+	logger->addAttribute("v5", sys.node<SimNode>("BUS5")->attribute("v"));
+	logger->addAttribute("v6", sys.node<SimNode>("BUS6")->attribute("v"));
+	logger->addAttribute("v7", sys.node<SimNode>("BUS7")->attribute("v"));
+	logger->addAttribute("v8", sys.node<SimNode>("BUS8")->attribute("v"));
+	logger->addAttribute("v9", sys.node<SimNode>("BUS9")->attribute("v"));
+	logger->addAttribute("wr_1", sys.component<Ph1::SynchronGeneratorTrStab>("GEN1")->attribute("w_r"));
+	logger->addAttribute("wr_2", sys.component<Ph1::SynchronGeneratorTrStab>("GEN2")->attribute("w_r"));
+	logger->addAttribute("wr_3", sys.component<Ph1::SynchronGeneratorTrStab>("GEN3")->attribute("w_r"));
+	logger->addAttribute("delta_r_1", sys.component<Ph1::SynchronGeneratorTrStab>("GEN1")->attribute("delta_r"));
+	logger->addAttribute("delta_r_2", sys.component<Ph1::SynchronGeneratorTrStab>("GEN2")->attribute("delta_r"));
+	logger->addAttribute("delta_r_3", sys.component<Ph1::SynchronGeneratorTrStab>("GEN3")->attribute("delta_r"));
+	logger->addAttribute("P_elec_1", sys.component<Ph1::SynchronGeneratorTrStab>("GEN1")->attribute("P_elec"));
+	logger->addAttribute("P_elec_2", sys.component<Ph1::SynchronGeneratorTrStab>("GEN2")->attribute("P_elec"));
+	logger->addAttribute("P_elec_3", sys.component<Ph1::SynchronGeneratorTrStab>("GEN3")->attribute("P_elec"));
+	logger->addAttribute("P_elec_1", sys.component<Ph1::SynchronGeneratorTrStab>("GEN1")->attribute("P_elec"));
+	logger->addAttribute("P_elec_2", sys.component<Ph1::SynchronGeneratorTrStab>("GEN2")->attribute("P_elec"));
+	logger->addAttribute("P_elec_3", sys.component<Ph1::SynchronGeneratorTrStab>("GEN3")->attribute("P_elec"));
+
+
+	Simulation sim(simName, sys, 0.0001, 2,
+		Domain::SP, Solver::Type::MNA, Logger::Level::info, true);
+
+	auto swEvent1 = SwitchEvent::make(0.2-0.0001, sw, true);
+	//auto swEvent2 = SwitchEvent::make(0.07, sw, false);
+
+	sim.addEvent(swEvent1);
+	//sim.addEvent(swEvent2);
+	sim.addLogger(logger);
+	sim.run();
+
+	return 0;
+}
