4aa7d80ac59d12d48fc592fe533a864775fb9665
Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>
2021-07-21 09:26:52 +0000

    Add base class to EMT Ph3 Switch
    Signed-off-by: Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>

--- Source/pybind/EMTComponents.cpp
+++ Source/pybind/EMTComponents.cpp
@@ -113,7 +113,7 @@ void addEMTPh3Components(py::module_ mEMTPh3) {
         .def("set_parameters", &CPS::EMT::Ph3::RXLoad::setParameters, "active_power"_a, "reactive_power"_a, "volt"_a)
 		.def("connect", &CPS::EMT::Ph3::RXLoad::connect);
 
-	py::class_<CPS::EMT::Ph3::Switch, std::shared_ptr<CPS::EMT::Ph3::Switch>, CPS::SimPowerComp<CPS::Real>>(mEMTPh3, "Switch", py::multiple_inheritance())
+	py::class_<CPS::EMT::Ph3::Switch, std::shared_ptr<CPS::EMT::Ph3::Switch>, CPS::SimPowerComp<CPS::Real>, CPS::Base::Ph3::Switch>(mEMTPh3, "Switch", py::multiple_inheritance())
         .def(py::init<std::string, CPS::Logger::Level>(), "name"_a, "loglevel"_a = CPS::Logger::Level::off)
         .def("set_parameters", &CPS::EMT::Ph3::Switch::setParameters, "open_resistance"_a, "closed_resistance"_a, "closed"_a = false)
 		.def("open", &CPS::EMT::Ph3::Switch::openSwitch)
