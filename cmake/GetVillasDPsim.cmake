c6a0fb5c089e1e8158d1a4abca538462514d5951
Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>
2021-09-22 11:07:15 +0000

    restructure cmake inclusion of dpsim-villas
    Signed-off-by: Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>

--- cmake/GetVillasDPsim.cmake
+++ cmake/GetVillasDPsim.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 FetchContent_Declare(
   dpsim-villas
-  GIT_REPOSITORY https://github.com/sogno-platform/dpsim-villas.git
+  GIT_REPOSITORY https://github.com/sogno-platform/dpsim-villas
   GIT_TAG        main
 )
 
@@ -9,11 +9,11 @@ if(${CMAKE_VERSION} VERSION_LESS "3.14.0")
   FetchContent_GetProperties(dpsim-villas)
   if(NOT dpsim-villas_POPULATED)
     FetchContent_Populate(dpsim-villas)
-    add_subdirectory(${dpsim-villas_SOURCE_DIR}/examples)
+    add_subdirectory(${dpsim-villas_SOURCE_DIR})
   endif()
 else()
+  #Auto-calls add_subdirectory if the downloaded content contains a top-level CMakeLists.txt
   FetchContent_MakeAvailable(dpsim-villas)
-  add_subdirectory(${dpsim-villas_SOURCE_DIR}/examples)
 endif()
 
 FetchContent_GetProperties(dpsim-villas)
