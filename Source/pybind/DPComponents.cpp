baa1795c3bc62c6c773808f3f20d39426209ad74
Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>
2021-11-17 11:50:17 +0000

    update python trafo example
    Signed-off-by: Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>

--- Source/pybind/DPComponents.cpp
+++ Source/pybind/DPComponents.cpp
@@ -147,6 +147,7 @@ void addDPPh1Components(py::module_ mDPPh1) {
         .def(py::init<std::string, CPS::Logger::Level>(), "name"_a, "loglevel"_a = CPS::Logger::Level::off)
 		.def(py::init<std::string, std::string, CPS::Logger::Level, CPS::Bool>(), "uid"_a, "name"_a, "loglevel"_a = CPS::Logger::Level::off, "with_resistive_losses"_a = false) // cppcheck-suppress assignBoolToPointer
 		.def("set_parameters", py::overload_cast<CPS::Real, CPS::Real, CPS::Real, CPS::Real, CPS::Real, CPS::Real>(&CPS::DP::Ph1::Transformer::setParameters), "nom_voltage_end_1"_a, "nom_voltage_end_2"_a, "ratio_abs"_a, "ratio_phase"_a, "resistance"_a, "inductance"_a)
+		.def("set_parameters", py::overload_cast<CPS::Real, CPS::Real, CPS::Real, CPS::Real, CPS::Real, CPS::Real, CPS::Real>(&CPS::DP::Ph1::Transformer::setParameters), "nom_voltage_end_1"_a, "nom_voltage_end_2"_a, "rated_power"_a, "ratio_abs"_a, "ratio_phase"_a, "resistance"_a, "inductance"_a)
 		.def("connect", &CPS::DP::Ph1::Transformer::connect);
 }
 
