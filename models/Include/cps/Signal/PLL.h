9b664f493d817e71bf97b1940fff8d7c6aaf4a82
Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>
2022-02-13 11:04:45 +0000

    update attributes for Signal::PLL
    Signed-off-by: Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>

--- models/Include/cps/Signal/PLL.h
+++ models/Include/cps/Signal/PLL.h
@@ -31,19 +31,6 @@ namespace Signal {
 		/// Integration time step
         Real mTimeStep;
 
-        /// Previous Input
-        Matrix mInputPrev = Matrix::Zero(2,1);
-        /// Current Input
-        Matrix mInputCurr = Matrix::Zero(2,1);
-        /// Previous State
-        Matrix mStatePrev = Matrix::Zero(2,1);
-        /// Current State
-        Matrix mStateCurr = Matrix::Zero(2,1);
-        /// Previous Output
-        Matrix mOutputPrev = Matrix::Zero(2,1);
-        /// Current Output
-        Matrix mOutputCurr = Matrix::Zero(2,1);
-
 		/// matrix A of state space model
 		Matrix mA = Matrix::Zero(2, 2);
 		/// matrix B of state space model
@@ -54,6 +41,23 @@ namespace Signal {
 		Matrix mD = Matrix::Zero(2, 2);
 
 	public:
+
+		///FIXME: This is never explicitely set to reference anything, so the outside code is responsible for setting up the reference.
+		Attribute<Real>::Ptr mInputRef;
+
+		/// Previous Input
+        const Attribute<Matrix>::Ptr mInputPrev;
+        /// Current Input
+        const Attribute<Matrix>::Ptr mInputCurr;
+        /// Previous State
+        const Attribute<Matrix>::Ptr mStatePrev;
+        /// Current State
+        const Attribute<Matrix>::Ptr mStateCurr;
+        /// Previous Output
+        const Attribute<Matrix>::Ptr mOutputPrev;
+        /// Current Output
+        const Attribute<Matrix>::Ptr mOutputCurr;
+
 		PLL(String name, Logger::Level logLevel = Logger::Level::off);
 		/// Setter for PLL parameters
 		void setParameters(Real kpPLL, Real kiPLL, Real omegaNom);
