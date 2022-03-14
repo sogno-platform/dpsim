862c1d24a23250008c9e6720e141a0f911c3367d
Ghassen Nakti <ghassen.nakti@eonerc.rwth-aachen.de>
2022-03-14 17:38:40 +0100

    fix pipeline build error
    Signed-off-by: Ghassen Nakti <ghassen.nakti@eonerc.rwth-aachen.de>

--- dpsim-models/src/EMT/EMT_Ph3_SynchronGeneratorTrStab.cpp
+++ dpsim-models/src/EMT/EMT_Ph3_SynchronGeneratorTrStab.cpp
@@ -277,7 +277,7 @@ void EMT::Ph3::SynchronGeneratorTrStab::step(Real time) {
 
 	// #### Calculate states for time step k+1 applying semi-implicit Euler ####
 	// Mechanical speed at time step k+1 applying Euler forward
-	if (mBehaviour == Behaviour::Simulation)
+	if (mBehaviour == Behaviour::MNASimulation)
 		**mOmMech = **mOmMech + mTimeStep * dOmMech;
 		
 	// Derivative of rotor angle at time step k + 1
@@ -296,7 +296,7 @@ void EMT::Ph3::SynchronGeneratorTrStab::step(Real time) {
 
 	// Rotor angle at time step k + 1 applying Euler backward
 	// Update emf - only phase changes
-	if (mBehaviour == Behaviour::Simulation) {
+	if (mBehaviour == Behaviour::MNASimulation) {
 		**mDelta_p = **mDelta_p + mTimeStep * dDelta_p;
 		**mEp = Complex(**mEp_abs * cos(**mDelta_p), **mEp_abs * sin(**mDelta_p));
 	}
