1e8c339debb7b4aefe1a71fd7f8a4c9bb093e753
Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>
2022-04-04 15:06:08 +0000

    use getPtr for dynamic_pointer_cast
    Signed-off-by: Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>

--- Source/pybind/Attributes.cpp
+++ Source/pybind/Attributes.cpp
@@ -24,42 +24,42 @@ void addAttributes(py::module_ m) {
 
     py::class_<CPS::AttributeBase, std::shared_ptr<CPS::AttributeBase>>(m, "Attribute")
 		.def("derive_real", [](CPS::AttributeBase::Ptr &attr) {
-			if (auto attrComp = std::dynamic_pointer_cast<CPS::Attribute<CPS::Complex>>(attr)) {
+			if (auto attrComp = std::dynamic_pointer_cast<CPS::Attribute<CPS::Complex>>(attr.getPtr())) {
 				return attrComp->deriveReal();
 			} else {
 				throw CPS::InvalidAttributeException();
 			}
 		})
 		.def("derive_imag", [](CPS::AttributeBase::Ptr &attr) {
-			if (auto attrComp = std::dynamic_pointer_cast<CPS::Attribute<CPS::Complex>>(attr)) {
+			if (auto attrComp = std::dynamic_pointer_cast<CPS::Attribute<CPS::Complex>>(attr.getPtr())) {
 				return attrComp->deriveImag();
 			} else {
 				throw CPS::InvalidAttributeException();
 			}
 		})
 		.def("derive_mag", [](CPS::AttributeBase::Ptr &attr) {
-			if (auto attrComp = std::dynamic_pointer_cast<CPS::Attribute<CPS::Complex>>(attr)) {
+			if (auto attrComp = std::dynamic_pointer_cast<CPS::Attribute<CPS::Complex>>(attr.getPtr())) {
 				return attrComp->deriveMag();
 			} else {
 				throw CPS::InvalidAttributeException();
 			}
 		})
 		.def("derive_phase", [](CPS::AttributeBase::Ptr &attr) {
-			if (auto attrComp = std::dynamic_pointer_cast<CPS::Attribute<CPS::Complex>>(attr)) {
+			if (auto attrComp = std::dynamic_pointer_cast<CPS::Attribute<CPS::Complex>>(attr.getPtr())) {
 				return attrComp->derivePhase();
 			} else {
 				throw CPS::InvalidAttributeException();
 			}
 		})
         .def("derive_scaled_comp", [](CPS::AttributeBase::Ptr &attr, CPS::Complex scale) {
-			if (auto attrComp = std::dynamic_pointer_cast<CPS::Attribute<CPS::Complex>>(attr)) {
+			if (auto attrComp = std::dynamic_pointer_cast<CPS::Attribute<CPS::Complex>>(attr.getPtr())) {
 				return attrComp->deriveScaled(scale);
             } else {
 				throw CPS::InvalidAttributeException();
 			}
 		})
         .def("derive_scaled_real", [](CPS::AttributeBase::Ptr &attr, CPS::Real scale) {
-			if (auto attrReal = std::dynamic_pointer_cast<CPS::Attribute<CPS::Real>>(attr)) {
+			if (auto attrReal = std::dynamic_pointer_cast<CPS::Attribute<CPS::Real>>(attr.getPtr())) {
             
 				return attrReal->deriveScaled(scale);
 			} else {
@@ -67,14 +67,14 @@ void addAttributes(py::module_ m) {
 			}
 		})
         .def("derive_coeff_comp", [](CPS::AttributeBase::Ptr &attr, CPS::MatrixVar<CPS::Complex>::Index row, CPS::MatrixVar<CPS::Complex>::Index column) {
-			if (auto attrMatComp = std::dynamic_pointer_cast<CPS::Attribute<CPS::MatrixVar<CPS::Complex>>>(attr)) {
+			if (auto attrMatComp = std::dynamic_pointer_cast<CPS::Attribute<CPS::MatrixVar<CPS::Complex>>>(attr.getPtr())) {
 				return attrMatComp->deriveCoeff<CPS::Complex>(row, column);
             } else {
 				throw CPS::InvalidAttributeException();
 			}
 		})
         .def("derive_coeff_real", [](CPS::AttributeBase::Ptr &attr, CPS::MatrixVar<CPS::Real>::Index row, CPS::MatrixVar<CPS::Real>::Index column) {
-			if (auto attrMatReal = std::dynamic_pointer_cast<CPS::Attribute<CPS::MatrixVar<CPS::Real>>>(attr)) {
+			if (auto attrMatReal = std::dynamic_pointer_cast<CPS::Attribute<CPS::MatrixVar<CPS::Real>>>(attr.getPtr())) {
 				return attrMatReal->deriveCoeff<CPS::Real>(row, column);
             } else {
 				throw CPS::InvalidAttributeException();
