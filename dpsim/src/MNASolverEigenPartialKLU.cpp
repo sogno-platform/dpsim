e6ccf8dab265c8c3c8878c4c2a5abd8452ab057a
Lennart Schumacher <lennart.schumacher@rwth-aachen.de>
2022-06-27 17:06:45 +0200

    corrected code
    Signed-off-by: Lennart Schumacher <lennart.schumacher@rwth-aachen.de>

--- dpsim/src/MNASolverEigenPartialKLU.cpp
+++ dpsim/src/MNASolverEigenPartialKLU.cpp
@@ -52,9 +52,9 @@ void MnaSolverEigenPartialKLU<VarType>::stampVariableSystemMatrix() {
 	this->mSLog->flush();
 
 	// Calculate factorization of current matrix
-	this->mLuFactorizationVariableSystemMatrix.analyzePattern(this->mVariableSystemMatrix, this->mListVariableSystemMatrixEntries, 0);
+	this->mLuFactorizationVariableSystemMatrix.analyzePattern(this->mVariableSystemMatrix, this->mListVariableSystemMatrixEntries, 1);
 	auto start = std::chrono::steady_clock::now();
-	this->mLuFactorizationVariableSystemMatrix.fctorize_partial(this->mVariableSystemMatrix, this->mListVariableSystemMatrixEntries);
+	this->mLuFactorizationVariableSystemMatrix.factorize_partial(this->mVariableSystemMatrix, this->mListVariableSystemMatrixEntries, 1);
 	auto end = std::chrono::steady_clock::now();
 	std::chrono::duration<double> diff = end-start;
 	this->mLUTimes.push_back(diff.count());
