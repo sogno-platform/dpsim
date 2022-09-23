fde642b16ec936bc03f34714e13b42239aab87f6
Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>
2022-09-23 10:33:23 +0000

    replace bash loop with python script (thanks to @pipeacosta)
    Signed-off-by: Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>

--- examples/villas/dpsim-mqtt.py
+++ examples/villas/dpsim-mqtt.py
@@ -57,10 +57,8 @@ intf = dpsimpyvillas.InterfaceVillas(name='dpsim-mqtt', config=mqtt_config)
 sim.add_interface(intf, True)
 
 sim.import_attribute(evs.attr('V_ref'), 0)
-sim.export_attribute(evs.attr('i_intf').derive_coeff(0, 0), 0)
+sim.export_attribute(r12.attr('i_intf').derive_coeff(0, 0), 0)
 
 sim.add_logger(logger)
-
-evs.set_intf_current([[complex(5, 0)]])
 
 sim.run(1)
