50c1ee248e8a05eb3ba19e9df474464e5d67d982
Martin Moraga <martin.moraga@eonerc.rwth-aachen.de>
2022-10-25 10:05:46 +0200

    add PSS type 2 and add base class for exciter
    Signed-off-by: Martin Moraga <martin.moraga@eonerc.rwth-aachen.de>

--- dpsim-models/include/dpsim-models/Signal/ExciterDC1Simp.h
+++ dpsim-models/include/dpsim-models/Signal/ExciterDC1Simp.h
@@ -55,6 +55,7 @@ namespace Signal {
 		public SharedFactory<ExciterDC1Simp> {
 
 	private: 
+<<<<<<< HEAD
 		/// Exciter Parameters
 		std::shared_ptr<ExciterDC1SimpParameters> mParameters;
 
@@ -94,6 +95,68 @@ namespace Signal {
 		void initialize(Real Vh_init, Real Vf_init) final;
 		/// Performs an step to update field voltage value
 		Real step(Real mVd, Real mVq, Real dt, Real Vpss = 0) final;
+=======
+		// ### Exciter Parameters ####
+		/// Ampliﬁer time constant (s)
+		Real mTa;
+		/// Ampliﬁer gain (pu/pu)
+		Real mKa;
+		/// Field circuit integral deviation
+		Real mKe;
+		/// Field circuit time constant (s)
+		Real mTe;
+		/// Stabilizer gain (s pu/pu)
+		Real mKf;
+		/// Stabilizer time constant (s)
+		Real mTf;
+		/// Measurement time constant (s)
+		Real mTr;
+		/// First ceiling coefficient
+		Real mAef;
+		/// Second ceiling coefficient
+		Real mBef;
+
+		/// Reference voltage (with effect of PSS)
+		Real mVref = 0;
+		/// Output of voltage transducer at time k-1
+		Real mVm_prev = 0;
+		/// Output of stablizing feedback at time k-1
+		Real mVis_prev = 0;
+		/// Output of ceiling function at time k-1
+		Real mVse_prev = 0;
+		/// Output of regulator output at time k-1
+		Real mVr_prev = 0;
+		/// Exciter output at time k-1
+		Real mEf_prev = 0;
+		/// Maximum regulator voltage (p.u.)
+		Real mMaxVr;
+		/// Minumum regulator voltage (p.u.)
+		Real mMinVr;
+
+	protected:
+		/// Output of voltage transducer at time k-1
+		const Attribute<Real>::Ptr mVm;
+		/// Input of voltage transducer
+		const Attribute<Real>::Ptr mVh;
+		/// Output of stablizing feedback at time k
+		const Attribute<Real>::Ptr mVis;
+		/// Regulator output at time k
+		const Attribute<Real>::Ptr mVr;
+		/// Output of ceiling function at time k-1
+		const Attribute<Real>::Ptr mVse;
+		/// Exciter output at time k (induced emf by the field current under no-load conditions)
+		const Attribute<Real>::Ptr mEf;
+
+	public:
+		/// Constructor
+		ExciterDC1Simp(const String & name, CPS::Logger::Level logLevel = Logger::Level::debug);
+		/// Initializes exciter parameters
+		void setParameters(Base::ExciterParameters parameters) override;
+		/// Initializes exciter variables
+		void initialize(Real Vh_init, Real Vf_init) override;
+		/// Performs an step to update field voltage value
+		Real step(Real mVd, Real mVq, Real dt, Real Vpss = 0) override;
+>>>>>>> 74ca12243 (add PSS type 2 and add base class for exciter)
 	};
 }
 }
