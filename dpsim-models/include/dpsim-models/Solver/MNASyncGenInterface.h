8fc252640d088f9804de06d2bdd5017ad430939a
Martin Moraga <martin.moraga@eonerc.rwth-aachen.de>
2022-11-16 16:11:31 +0100

    refactor iterative SG 4Order
    Signed-off-by: Martin Moraga <martin.moraga@eonerc.rwth-aachen.de>

--- dpsim-models/include/dpsim-models/Solver/MNASyncGenInterface.h
+++ dpsim-models/include/dpsim-models/Solver/MNASyncGenInterface.h
@@ -15,9 +15,68 @@
 namespace CPS {
 	/// Interface to be used by synchronous generators
 	class MNASyncGenInterface {
+	protected:
+		// #### Model specific variables ####
+		/// generator terminal at time k-1
+		Matrix mVdq_prev;
+		/// predicted generator current at time k
+		Matrix mIdq_pred;
+		/// corrected generator current at time k
+		Matrix mIdq_corr;
+		/// voltage behind the transient/subtransiet impedance at time k-1
+		Attribute<Matrix>::Ptr mEdq;
+		/// predicted voltage behind the transient and subtransient impedance at time k+1
+		Matrix mEdq_pred;
+		/// corrected voltage behind the transient and subtransient impedance at time k+1
+		Matrix mEdq_corr;
+		/// corrected electrical torque
+		Real mElecTorque_corr;
+		/// predicted mechanical omega at time k
+		Real mOmMech_pred;
+		/// corrected mechanical omega at time k
+		Real mOmMech_corr;
+		/// prediction mechanical system angle at time k
+		Real mThetaMech_pred;
+		/// corrected mechanical system angle at time k
+		Real mThetaMech_corr;
+		/// predicted delta at time k
+		Real mDelta_pred;
+		/// corrected delta at time k
+		Real mDelta_corr;
+
+		/// State Matrix backward euler: Edq(k) = A * Edq(k) + B * Idq + C * Ef
+		/// State Matrix trapezoidal rule (corrector step): x(k+1) = mA_prev * Edq(k-1) + mA_corr * Edq_corr(k) + B_corr * Idq_corr(k) + C * Ef
+		/// A Matrix
+		Matrix mA;
+		Matrix mA_prev;
+		Matrix mA_corr;
+		/// B Matrix
+		Matrix mB;
+		Matrix mB_corr;
+		/// Constant Matrix
+		Matrix mC;
+
+		/// Transformation matrix dp->dq
+		MatrixComp mDpToDq;
+
+		/// Vector to create abc vector from a component
+		MatrixComp mShiftVector;
+
 	public:
 		typedef std::shared_ptr<MNASyncGenInterface> Ptr;
 		typedef std::vector<Ptr> List;
+
+		/// Constructor
+		MNASyncGenInterface() {
+			// inizialize matrix that are not model dependent
+			mIdq_pred = Matrix::Zero(2,1);
+			mIdq_corr = Matrix::Zero(2,1);
+
+			// Vector to convert 1phase to 3phase
+
+			mShiftVector = Matrix::Zero(3,1);
+			mShiftVector << Complex(1., 0), SHIFT_TO_PHASE_B, SHIFT_TO_PHASE_C;
+		}
 
 		/// Returns true if it needs to iterate
 		virtual void correctorStep()=0;
@@ -25,6 +84,23 @@ namespace CPS {
 		virtual void updateVoltage(const Matrix& leftVector)=0;
 		///
 		virtual bool checkVoltageDifference() {return false;}
+		///
+		Matrix parkTransform(Real theta, const Matrix& abcVector) {
+			Matrix dq0Vector(3, 1);
+			Matrix dqVector(2, 1);
+			Matrix abcToDq0(3, 3);
+
+			// Park transform according to Kundur
+			abcToDq0 <<
+		 		2./3.*cos(theta),	2./3.*cos(theta - 2.*PI/3.),  2./3.*cos(theta + 2.*PI/3.),
+				-2./3.*sin(theta), -2./3.*sin(theta - 2.*PI/3.), -2./3.*sin(theta + 2.*PI/3.),
+		 		1./3., 			1./3., 						  1./3.;
+
+			dq0Vector = abcToDq0 * abcVector;
+			dqVector << dq0Vector(0,0), dq0Vector(1,0);
+
+			return dqVector;
+		}
 
 		/// Setters
 		/// 
