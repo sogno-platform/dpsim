bc0ca84ac9455171239fe87a50efb4e94a9a2601
Steffen Vogel <steffen.vogel@opal-rt.com>
2023-01-18 09:44:22 +0100

    cmake: do shallow Git clone to speed up builds
    Signed-off-by: Steffen Vogel <steffen.vogel@opal-rt.com>

--- cmake/FetchGridData.cmake
+++ cmake/FetchGridData.cmake
@@ -2,6 +2,8 @@ include(FetchContent)
 FetchContent_Declare(cim-data
 	GIT_REPOSITORY https://git.rwth-aachen.de/acs/public/grid-data/cim-grid-data.git
 	GIT_TAG        master
+	GIT_SHALLOW    TRUE
+	GIT_PROGRESS   TRUE
 )
 
 FetchContent_MakeAvailable(cim-data)
@@ -9,6 +11,8 @@ FetchContent_MakeAvailable(cim-data)
 FetchContent_Declare(profile-data
 	GIT_REPOSITORY https://git.rwth-aachen.de/acs/public/grid-data/grid-profiles.git
 	GIT_TAG        master
+	GIT_SHALLOW    TRUE
+	GIT_PROGRESS   TRUE
 )
 
 FetchContent_MakeAvailable(profile-data)
