6712317a8d4edd73e230747cf9eb9dd517589359
Steffen Vogel <steffen.vogel@opal-rt.com>
2023-01-18 09:44:22 +0100

    cmake: do shallow Git clone to speed up builds
    Signed-off-by: Steffen Vogel <steffen.vogel@opal-rt.com>

--- cmake/FetchPybind.cmake
+++ cmake/FetchPybind.cmake
@@ -2,6 +2,8 @@ include(FetchContent)
 FetchContent_Declare(pybind-module
 	GIT_REPOSITORY https://github.com/pybind/pybind11.git
 	GIT_TAG        v2.10.3
+	GIT_SHALLOW    TRUE
+	GIT_PROGRESS   TRUE
 )
 
 FetchContent_MakeAvailable(pybind-module)
--- cmake/FetchVILLASnode.cmake
+++ cmake/FetchVILLASnode.cmake
@@ -1,6 +1,8 @@
 include(FetchContent)
 FetchContent_Declare(villas-node-module
 	GIT_REPOSITORY https://github.com/VILLASframework/node.git
+	GIT_SHALLOW    TRUE
+	GIT_PROGRESS   TRUE
 )
 
 FetchContent_MakeAvailable(villas-node-module)
