6ebc5f585408b96fd1dd458ffb5b3db4a8d17866
pipeacosta <pipeacosta@gmail.com>
2023-02-04 06:38:05 -0500

    Set include directories and library names
    Signed-off-by: pipeacosta <pipeacosta@gmail.com>

--- cmake/FetchVILLASnode.cmake
+++ cmake/FetchVILLASnode.cmake
@@ -6,5 +6,19 @@ FetchContent_Declare(villas-node-module
 )
 
 FetchContent_MakeAvailable(villas-node-module)
+message(STATUS "Path to villas-node-module: " ${villas-node-module_SOURCE_DIR})
 
-set(VILLASnode_FOUND TRUE)
+if(NOT WIN32)
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
+endif()
+
+# When adding VILLASnode via a subdirectory, all the neccessary
+# details about include directories, library name and path
+# are associated with the VILLASnode target 'villas'
+add_subdirectory(${villas-node-module_SOURCE_DIR})
+set(VILLASNODE_LIBRARY villas)
+set(VILLASNODE_LIBRARIES ${VILLASNODE_LIBRARY})
+set(VILLASNODE_INCLUDE_DIRS "")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(VILLASnode DEFAULT_MSG VILLASNODE_LIBRARY)
