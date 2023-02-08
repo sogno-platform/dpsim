968fed24fe3e17424b0bef3e2824a5926a94e954
pipeacosta <pipeacosta@gmail.com>
2023-02-08 10:38:10 -0500

    Improvements to the example dpsim-villas-cosim: first working version
    Signed-off-by: pipeacosta <pipeacosta@gmail.com>

--- examples/villas/dpsim-villas-cosim.py
+++ examples/villas/dpsim-villas-cosim.py
@@ -18,7 +18,7 @@ log = logging.getLogger(base)
 def dpsim0():
     sim_name = "DistributedVILLAS0"
     time_step = 0.01
-    final_time = 10
+    final_time = 1.0
 
     n1 = dpsimpy.dp.SimNode('n1', dpsimpy.PhaseType.Single, [10])
     n2 = dpsimpy.dp.SimNode('n2', dpsimpy.PhaseType.Single, [5])
@@ -34,7 +34,7 @@ def dpsim0():
 
     evs.connect([dpsimpy.dp.SimNode.gnd, n2])
     vs1.connect([dpsimpy.dp.SimNode.gnd, n1])
-    r12.connect([n1, n2])
+    r12.connect([n2, n1])
 
     sys = dpsimpy.SystemTopology(50, [n1, n2], [evs, vs1, r12])
 
@@ -73,24 +73,28 @@ def dpsim0():
     dpsimpy.Logger.set_log_dir('logs/' + sim_name)
 
     logger = dpsimpy.Logger(sim_name)
-    logger.log_attribute('v1', 'v', n1)
-    logger.log_attribute('v2', 'v', n2)
-    logger.log_attribute('r12', 'i_intf', r12)
-    logger.log_attribute('ievs', 'i_intf', evs)
-    logger.log_attribute('vevs', 'v_intf', evs)
+    logger.log_attribute('1_v_1', 'v', n1)
+    logger.log_attribute('2_v_2', 'v', n2)
+    logger.log_attribute('3_i_r12', 'i_intf', r12)
+    logger.log_attribute('4_i_evs', 'i_intf', evs)
+    logger.log_attribute('5_v_evs', 'v_intf', evs)
     
     intf = dpsimpyvillas.InterfaceVillas(name="dpsim0-dpsim1", config=intf_config)
 
     sim.add_interface(intf)
-    intf.import_attribute(evs.attr('V_ref'), 0)
-    intf.export_attribute(evs.attr('i_intf').derive_coeff(0,0), 0)
+    sim.add_logger(logger)
+
+    evs.set_intf_current([[complex(5, 0)]])
+
+    intf.import_attribute(evs.attr('V_ref'), 0, False, False)
+    intf.export_attribute(evs.attr('i_intf').derive_coeff(0,0), 0, True)
   
     sim.run(1)
 
 def dpsim1():
     sim_name = "DistributedVILLAS1"
     time_step = 0.01
-    final_time = 10
+    final_time = 1.0
 
     n2 = dpsimpy.dp.SimNode('n2', dpsimpy.PhaseType.Single, [5])
 
@@ -139,16 +143,17 @@ def dpsim1():
     dpsimpy.Logger.set_log_dir('logs/' + sim_name)
 
     logger = dpsimpy.Logger(sim_name)
-    logger.log_attribute('v2', 'v', n2)
-    logger.log_attribute('r02', 'i_intf', r02)
-    logger.log_attribute('vecs', 'v_intf', ecs)
-    logger.log_attribute('iecs', 'i_intf', ecs)
+    logger.log_attribute('1_v_2', 'v', n2)
+    logger.log_attribute('2_i_r02', 'i_intf', r02)
+    logger.log_attribute('3_v_ecs', 'v_intf', ecs)
+    logger.log_attribute('4_i_ecs', 'i_intf', ecs)
 
     intf = dpsimpyvillas.InterfaceVillas(name="dpsim1-dpsim0", config=intf_config)
 
     sim.add_interface(intf)
-    intf.import_attribute(ecs.attr('I_ref'), 0)
-    intf.export_attribute(ecs.attr('v_intf').derive_coeff(0,0).derive_scaled(complex(-1,0)), 0)
+    sim.add_logger(logger)
+    intf.import_attribute(ecs.attr('I_ref'), 0, False, False)
+    intf.export_attribute(ecs.attr('v_intf').derive_coeff(0,0).derive_scaled(complex(-1,0)), 0, True)
   
     sim.run(1)
 
