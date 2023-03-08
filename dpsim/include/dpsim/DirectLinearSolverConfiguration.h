53500a27fcb374be49fe17ec011b430eddcfe1c1
Lennart Schumacher <lennart.schumacher@rwth-aachen.de>
2023-03-08 13:15:09 +0100

    added class files
    Signed-off-by: Lennart Schumacher <lennart.schumacher@rwth-aachen.de>

---
+++ dpsim/include/dpsim/DirectLinearSolverConfiguration.h
@@ -0,0 +1,72 @@
+/* Copyright 2017-2021 Institute for Automation of Complex Power Systems,
+*                     EONERC, RWTH Aachen University
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*********************************************************************************/
+
+#pragma once
+
+#include <dpsim/Config.h>
+#include <dpsim/Definitions.h>
+
+using namespace DPsim;
+
+namespace DPsim
+{
+	// Define method to scale the input matrix
+	enum class SCALING_METHOD {
+		NO_SCALING,
+		SUM_SCALING,
+		MAX_SCALING
+	};
+
+	// Define preordering to reduce fill-in
+	enum class FILL_IN_REDUCTION_METHOD {
+		AMD,
+		AMD_NV,
+		AMD_RA,
+		COLAMD
+	};
+
+	// Define partial refactorization method, if applicable
+	enum class PARTIAL_REFACTORIZATION_METHOD {
+		NO_PARTIAL_REFACTORIZATION,
+		FACTORIZATION_PATH,
+		REFACTORIZATION_RESTART
+	};
+
+	// Define BTF usage
+	enum class USE_BTF {
+		NO_BTF,
+		DO_BTF
+	};
+
+	class DirectLinearSolverConfiguration
+	{
+		SCALING_METHOD m_scalingMethod;
+		FILL_IN_REDUCTION_METHOD m_fillInReductionMethod;
+		PARTIAL_REFACTORIZATION_METHOD m_partialRefactorizationMethod;
+		USE_BTF m_useBTF;
+
+		public:
+		DirectLinearSolverConfiguration();
+
+		void setFillInReductionMethod(FILL_IN_REDUCTION_METHOD);
+
+		void setScalingMethod(SCALING_METHOD);
+
+		void setPartialRefactorizationMethod(PARTIAL_REFACTORIZATION_METHOD);
+
+		void setBTF(USE_BTF);
+
+		SCALING_METHOD getScalingMethod() const;
+
+		FILL_IN_REDUCTION_METHOD getFillInReductionMethod() const;
+
+		PARTIAL_REFACTORIZATION_METHOD getPartialRefactorizationMethod() const;
+
+		USE_BTF getBTF() const;
+	};
+}
---
+++ dpsim/src/DirectLinearSolverConfiguration.cpp
@@ -0,0 +1,62 @@
+/* Copyright 2017-2021 Institute for Automation of Complex Power Systems,
+*                     EONERC, RWTH Aachen University
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*********************************************************************************/
+
+#include <dpsim/DirectLinearSolverConfiguration.h>
+
+using namespace DPsim;
+
+namespace DPsim
+{
+	DirectLinearSolverConfiguration::DirectLinearSolverConfiguration()
+	{
+		m_scalingMethod = SCALING_METHOD::NO_SCALING;
+		m_partialRefactorizationMethod = PARTIAL_REFACTORIZATION_METHOD::NO_PARTIAL_REFACTORIZATION;
+		// m_fillInReductionMethod = FILL_IN_REDUCTION_METHOD::AMD;
+		// m_preprocessingMethod = PREPROCESSING_METHOD::BTF;
+	}
+
+	void DirectLinearSolverConfiguration::setFillInReductionMethod(FILL_IN_REDUCTION_METHOD fillInReductionMethod)
+	{
+		m_fillInReductionMethod = fillInReductionMethod;
+	}
+
+	void DirectLinearSolverConfiguration::setScalingMethod(SCALING_METHOD scalingMethod)
+	{
+		m_scalingMethod = scalingMethod;
+	}
+
+	void DirectLinearSolverConfiguration::setPartialRefactorizationMethod(PARTIAL_REFACTORIZATION_METHOD partialRefactorizationMethod)
+	{
+		m_partialRefactorizationMethod = partialRefactorizationMethod;
+	}
+
+	void DirectLinearSolverConfiguration::setBTF(USE_BTF useBTF)
+	{
+		m_useBTF = useBTF;
+	}
+
+	SCALING_METHOD DirectLinearSolverConfiguration::getScalingMethod() const
+	{
+		return m_scalingMethod;
+	}
+
+	FILL_IN_REDUCTION_METHOD DirectLinearSolverConfiguration::getFillInReductionMethod() const
+	{
+		return m_fillInReductionMethod;
+	}
+
+	PARTIAL_REFACTORIZATION_METHOD DirectLinearSolverConfiguration::getPartialRefactorizationMethod() const
+	{
+		return m_partialRefactorizationMethod;
+	}
+
+	USE_BTF DirectLinearSolverConfiguration::DirectLinearSolverConfiguration::getBTF() const
+	{
+		return m_useBTF;
+	}
+}
