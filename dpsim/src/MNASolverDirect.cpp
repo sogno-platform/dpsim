a6151a338a57e2a932007443c0ba458d1855ac57
Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>
2023-03-10 09:53:30 +0100

    add missing update of internal iterative model vars after prediction step system solution
    Signed-off-by: Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>

--- dpsim/src/MNASolverDirect.cpp
+++ dpsim/src/MNASolverDirect.cpp
@@ -211,6 +211,11 @@ void MnaSolverDirect<VarType>::solve(Real time, Int timeStepCount) {
 	if (mSwitchedMatrices.size() > 0)
 		**mLeftSideVector = mDirectLinearSolvers[mCurrentSwitchStatus][0]->solve(mRightSideVector);
 
+	// CHECK: Is this really required? Or can operations actually become part of
+	// correctorStep and mnaPostStep?
+	for (auto syncGen : mSyncGen)
+		syncGen->updateVoltage(**mLeftSideVector);
+
 	// Reset number of iterations
 	mIter = 0;
 
