f049b42068a28caf9c59509a3ae5d4ccd83377be
Lennart Schumacher <lennart.schumacher@rwth-aachen.de>
2023-03-16 09:21:38 +0100

    improved cmake
    Signed-off-by: Lennart Schumacher <lennart.schumacher@rwth-aachen.de>

--- cmake/FetchNICSLU.cmake
+++ cmake/FetchNICSLU.cmake
@@ -1,9 +1,9 @@
 include(FetchContent)
 
 FetchContent_Declare(
-				nicslu-module
-				GIT_REPOSITORY git@git-ce.rwth-aachen.de:acs/private/research/rte/nicslu-customlu.git
-				GIT_TAG        main
+	nicslu-module
+	GIT_REPOSITORY git@git-ce.rwth-aachen.de:acs/private/research/rte/nicslu-customlu.git
+	GIT_TAG        main
 )
 
 FetchContent_GetProperties(nicslu-module)
@@ -11,14 +11,15 @@ if(NOT nicslu-module_POPULATED)
 	FetchContent_Populate(nicslu-module)
 endif()
 
-FetchContent_GetProperties(nicslu-module)
+FetchContent_MakeAvailable(nicslu-module)
 
 add_subdirectory(${nicslu-module_SOURCE_DIR} ${nicslu-module_BINARY_DIR})
 
-set(NICSLU_LIBRARY ${nicslu-module_BINARY_DIR}/libnicslu.so)
+set(NICSLU_LIBRARY ${nicslu-module_BINARY_DIR}/libnicslu.a)
 
 set(NICSLU_LIBRARIES ${NICSLU_LIBRARY})
 set(NICSLU_INCLUDE_DIR ${nicslu-module_SOURCE_DIR}/include)
 set(NICSLU_INCLUDE_DIRS ${NICSLU_INCLUDE_DIR})
+
 message(STATUS "Path to nicslu-module: " ${nicslu-module_SOURCE_DIR})
 
