bb34825689e1e311eb7b5098c5138abb649a223f
pipeacosta <pipeacosta@gmail.com>
2023-04-09 23:59:26 +0200

    Rename co-simulation example
    Signed-off-by: pipeacosta <pipeacosta@gmail.com>

--- dpsim-villas/examples/cxx/CosimExample_ext_control.cpp
+++ dpsim-villas/examples/cxx/CosimExample_ext_control.cpp
@@ -7,53 +7,133 @@
 #include <dpsim-villas/InterfaceVillas.h>
 
 using namespace DPsim;
-using namespace CPS::EMT;
-using namespace CPS::EMT::Ph1;
+using namespace CPS;
 using namespace std;
 
 Simulation setDPsim1(float ts, float tf, float u10) {
-	String simName = "SocketsCosim_example1";
+	float r1_r = 0.1;
+	float c1_c = 1;
+	float rLine_r = 0.1;
+	
+	// ----- POWERFLOW FOR INITIALIZATION -----
+	// String simNamePF = "Cosim_example1";
+	// CPS::Logger::setLogDir("logs/"+simNamePF);
+
+	// auto n1PF = SP::SimNode::make("n1");
+	// auto n2PF = SP::SimNode::make("n2");
+
+	// auto evsPF = SP::Ph1::VoltageSource::make("v_intf");
+	// evsPF->setParameters(u10);
+
+	// auto r1PF =  SP::Ph1::Resistor::make("r_1");
+	// r1PF->setParameters(r1_r);
+	// auto c1PF = SP::Ph1::Capacitor::make("c_1");
+	// c1PF->setParameters(c1_c);
+	// auto rLinePF = SP::Ph1::Resistor::make("r_line");
+	// rLinePF->setParameters(rLine_r);
+
+	// r1PF->connect({ n1PF, SP::SimNode::GND });
+	// rLinePF->connect({ n1PF, n2PF });
+	// c1PF->connect({ n1PF, SP::SimNode::GND });
+	// evsPF->connect({ SP::SimNode::GND, n2PF });
+	// auto systemPF = SystemTopology(50,
+	// 	SystemNodeList{n1PF, n2PF},
+	// 	SystemComponentList{c1PF, r1PF, rLinePF, evsPF});
+
+	// auto loggerPF = DataLogger::make(simNamePF);
+	// loggerPF->logAttribute("1_v_1", n1PF->mVoltage);
+	// loggerPF->logAttribute("2_v_2", n2PF->mVoltage);
+
+	// Simulation simPF(simNamePF, CPS::Logger::Level::debug);
+	// simPF.setSystem(systemPF);
+	// simPF.setTimeStep(0.1);
+	// simPF.setFinalTime(0.1);
+	// simPF.setDomain(Domain::SP);
+	// simPF.setSolverType(Solver::Type::NRP);
+	// simPF.doInitFromNodesAndTerminals(false);
+	// simPF.addLogger(loggerPF);
+	// simPF.run();
+	
+	
+	// ----- DYNAMIC SIMULATION -----
+	String simName = "Cosim_example1";
 	CPS::Logger::setLogDir("logs/"+simName);
 
 	// Nodes
-	auto n1 = SimNode::make("n1");
-	auto n2 = SimNode::make("n2");
+	auto n1 = EMT::SimNode::make("n1");
+	auto n2 = EMT::SimNode::make("n2");
 
 	// Components
-	auto evs = VoltageSource::make("v_intf");
+	auto evs = EMT::Ph1::VoltageSource::make("v_intf");
 	evs->setParameters(u10);
 
-	auto r1 =  Resistor::make("r_1");
-	r1->setParameters(0.1);
-	auto c1 = Capacitor::make("c_1");
-	c1->setParameters(1);
-	auto rLine = Resistor::make("r_line");
-	rLine->setParameters(0.1);
+	auto r1 =  EMT::Ph1::Resistor::make("r_1");
+	r1->setParameters(r1_r);
+	auto c1 = EMT::Ph1::Capacitor::make("c_1");
+	c1->setParameters(c1_c);
+	auto rLine = EMT::Ph1::Resistor::make("r_line");
+	rLine->setParameters(rLine_r);
 
 	// Topology
-	r1->connect({ n1, SimNode::GND });
+	r1->connect({ n1, EMT::SimNode::GND });
 	rLine->connect({ n1, n2 });
-	c1->connect({ n1, SimNode::GND });
-	evs->connect({ SimNode::GND, n2 });
+	c1->connect({ n1, EMT::SimNode::GND });
+	evs->connect({ EMT::SimNode::GND, n2 });
 
 	auto sys = SystemTopology(50,
-		SystemNodeList{SimNode::GND, n1, n2},
+		SystemNodeList{EMT::SimNode::GND, n1, n2},
 		SystemComponentList{r1, c1, rLine, evs});
 
-	Simulation sim(simName);
-	sim.setDomain(CPS::Domain::EMT);
-	sim.setSystem(sys);
-	sim.setTimeStep(ts);
-	sim.setFinalTime(0.1);
-	sim.doSteadyStateInit(true);
-	
 	auto logger = DataLogger::make(simName);
 	logger->logAttribute("1_v_1", n1->mVoltage);
 	logger->logAttribute("2_v_2", n2->mVoltage);
 	logger->logAttribute("3_i_rline", rLine->mIntfCurrent, 1, 1);
 	logger->logAttribute("4_i_evs", evs->mIntfCurrent, 1, 1);
 	logger->logAttribute("5_v_evs", evs->mIntfVoltage, 1, 1);
+	
+	Simulation sim(simName);
+	sim.setDomain(CPS::Domain::EMT);
 	sim.addLogger(logger);
+	sim.setSystem(sys);
+	sim.setTimeStep(ts);
+	sim.setFinalTime(0.1);
+	// sim.doSteadyStateInit(true);
+	sim.doInitFromNodesAndTerminals(false);
+
+	// initialize currents and voltages
+	// Eigen::VectorXd n_v0(2);
+	// n_v0 << 5, 5;
+
+	Eigen::MatrixXd n1_v0(1,1);
+	n1_v0(0,0) = 5;
+	Eigen::MatrixXd n2_v0(1,1);
+	n2_v0(0,0) = 2;
+
+	Eigen::MatrixXd ir1_0(1,1);
+	ir1_0(0,0) = n1_v0(0,0) / r1_r;
+	Eigen::MatrixXd irLine_0(1,1);
+	irLine_0(0,0) = (n1_v0(0,0) - n2_v0(0,0)) / rLine_r;
+
+	r1->setIntfVoltage(n1_v0);
+	r1->setIntfCurrent(ir1_0);
+	c1->setIntfVoltage(n1_v0);
+	c1->setIntfCurrent(ir1_0 - irLine_0);
+	rLine->setIntfVoltage(n1_v0 - n2_v0);
+	rLine->setIntfCurrent(irLine_0);
+	// cout << "rLine voltage: " << rLine->mIntfVoltage->toString() << endl;
+	// cout << "r1 current: " << r1->mIntfCurrent->toString() << endl;
+	// cout << "c1 current: " << c1->mIntfCurrent->toString() << endl;
+
+	evs->setIntfVoltage(n2_v0);
+	evs->setIntfCurrent(irLine_0);
+	cout << "evs current: " << evs->mIntfCurrent->toString() << endl;
+
+	// Eigen::MatrixXd r1_i(1,1);
+	// r1_i(0,0) = n1_v0(0,0) / r1_r;
+	// r1->setIntfCurrent(r1_i);
+	// c1->setIntfCurrent(Eigen::MatrixXd(20));
+	// rLine->setIntfVoltage(Eigen::MatrixXd(3));
+	// rLine->setIntfCurrent(Eigen::MatrixXd(30));
 
 	// Eigen::MatrixXcd intfCurrent0(1,1);
 	// intfCurrent0(0,0) = std::complex<double>(5.0,0.0);
