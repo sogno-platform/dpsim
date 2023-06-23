84dc8e490b72fdb249aff5a5623c8b7f9748f963
Martin Moraga <martin.moraga@eonerc.rwth-aachen.de>
2023-06-23 12:08:13 +0200

    remove unused methods and variables of MNASyncGenInterface
    Signed-off-by: Martin Moraga <martin.moraga@eonerc.rwth-aachen.de>

--- dpsim-models/include/dpsim-models/Solver/MNASyncGenInterface.h
+++ dpsim-models/include/dpsim-models/Solver/MNASyncGenInterface.h
@@ -16,25 +16,6 @@ namespace CPS {
 	/// Interface to be used by synchronous generators
 	class MNASyncGenInterface {
 	protected:
-		/// Matrix used when numerical method of predictor step = Euler
-		/// State Matrix backward euler: Edq(k) = mA_euler * Edq(k) + mB_euler * Idq + mC_euler * Ef
-		Matrix mA_euler;
-		Matrix mB_euler;
-		Matrix mC_euler;
-
-		/// Matrixes used when numerical method of corrector step = trapezoidal rule
-		/// State Matrix trapezoidal rule (corrector step): x(k+1) = mAPrevIter * Edq(k-1) + mA_corr * Edq_corr(k) + B_corr * Idq_corr(k) + mC_corr * Ef
-		Matrix mA_prev;
-		Matrix mA_corr;
-		Matrix mB_corr;
-		Matrix mC_corr;
-
-		/// Transformation matrix dp->dq
-		MatrixComp mDpToDq;
-
-		/// Vector to create abc vector from a component
-		MatrixComp mShiftVector;
-
 		///
 		Attribute<Int>::Ptr mNumIter;
 		///
@@ -62,29 +43,6 @@ namespace CPS {
 
 	protected:
 		/// Constructor
-		MNASyncGenInterface() {
-			// Vector to convert 1phase to 3phase
-			mShiftVector = Matrix::Zero(3,1);
-			mShiftVector << Complex(1., 0), SHIFT_TO_PHASE_B, SHIFT_TO_PHASE_C;
-		}
-
-		///
-		Matrix parkTransform(Real theta, const Matrix& abcVector) {
-			Matrix dq0Vector(3, 1);
-			Matrix dqVector(2, 1);
-			Matrix abcToDq0(3, 3);
-
-			// Park transform according to Kundur
-			abcToDq0 <<
-		 		2./3.*cos(theta),	2./3.*cos(theta - 2.*PI/3.),  2./3.*cos(theta + 2.*PI/3.),
-				-2./3.*sin(theta), -2./3.*sin(theta - 2.*PI/3.), -2./3.*sin(theta + 2.*PI/3.),
-		 		1./3., 			1./3., 						  1./3.;
-
-			dq0Vector = abcToDq0 * abcVector;
-			dqVector << dq0Vector(0,0), dq0Vector(1,0);
-
-			return dqVector;
-		}
-
+		MNASyncGenInterface() { }
 	};
 }
