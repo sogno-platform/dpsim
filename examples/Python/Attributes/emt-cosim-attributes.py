f7a02e9d4289d108b65fa282133007cf2faa29d6
pipeacosta <pipeacosta@gmail.com>
2023-12-08 08:50:11 +0100

    Improve and clean emt-cosim-attributes.py example
    Signed-off-by: pipeacosta <pipeacosta@gmail.com>

--- examples/Python/Attributes/emt-cosim-attributes.py
+++ examples/Python/Attributes/emt-cosim-attributes.py
@@ -3,8 +3,8 @@ import os.path
 import logging
 
 import dpsimpy
-import villas.dataprocessing.timeseries as ts
 import numpy as np
+import argparse
 
 base = os.path.splitext(os.path.basename(sys.argv[0]))[0]
 log = logging.getLogger(base)
@@ -56,33 +56,14 @@ def set_dpsim1(t_s, t_f, u_1_0):
     logger.log_attribute('v2', 'v', n2)
     logger.log_attribute('i', 'i_intf', evs)
     logger.log_attribute('ir', 'i_intf', r_line)
-    # logger.log_attribute('4_i_evs', 'i_intf', evs)
-    # logger.log_attribute('5_v_evs', 'v_intf', evs)
     
     sim.add_logger(logger)
-    
-    # Initialize currents and voltages
-    # r = np.array([[1/r_1_r, 0.0], [0.0, 1/r_line_r]])
-    # n_v_0 = np.array([5.0, 5.0 - u_1_0]).T
-    
-    # i_0 = np.matmul(r, n_v_0)
-    
     
     n1_v0 = np.array([5.0])
     n2_v0 = np.array([u_1_0])
     
     ir_1_0 = n1_v0 / r_1_r
     i_r_line_0 = (n1_v0 - n2_v0) / r_line_r
-    
-    # r_1.set_intf_voltage([n_v_0[0]])
-    # r_1.set_intf_current([i_0[0]])
-    # c_1.set_intf_voltage([n_v_0[0]])
-    # c_1.set_intf_current([i_0[0] - i_0[1]])
-    # r_line.set_intf_voltage([n_v_0[1]])
-    # r_line.set_intf_current([i_0[1]])
-    
-    # evs.set_intf_voltage([n_v_0[0] - n_v_0[1]])
-    # evs.set_intf_current([i_0[1]])
     
     r_1.set_intf_voltage(n1_v0)
     r_1.set_intf_current(ir_1_0)
@@ -107,7 +88,6 @@ def set_dpsim2(t_s, t_f):
     n2 = dpsimpy.emt.SimNode("n2")
 
     ecs = dpsimpy.emt.ph1.CurrentSource("i_intf", dpsimpy.LogLevel.info)
-    # ecs.set_parameters(u_2_0)
     c_2 = dpsimpy.emt.ph1.Capacitor("c_2", dpsimpy.LogLevel.info)
     c_2.set_parameters(c_2_c)
     r_load = dpsimpy.emt.ph1.Resistor("r_load", dpsimpy.LogLevel.info)
@@ -135,31 +115,33 @@ def set_dpsim2(t_s, t_f):
     logger.log_attribute('v2', 'v', n2)
     logger.log_attribute('i', 'i_intf', ecs)
     logger.log_attribute('v', 'v_intf', ecs)
-    # logger.log_attribute('2_i_rload', 'i_intf', r_load)
-    # logger.log_attribute('3_v_ecs', 'v_intf', ecs)
-    # logger.log_attribute('4_i_ecs', 'i_intf', ecs)
 
     sim.add_logger(logger)
     
     # Initialize currents and voltages
     n2_v0 = [2.0]
-    # i_r_line_0 = [u_2_0]
     i_r_load_0 = [n2_v0[0] / r_load_r]
     
     r_load.set_intf_voltage(n2_v0)
     r_load.set_intf_current(i_r_load_0)
     c_2.set_intf_voltage([n2_v0])
-    # c_2.set_intf_current([i_r_line_0[0] - i_r_load_0[0]])
     
     ecs.set_intf_voltage(n2_v0)
-    # ecs.set_intf_current(i_r_line_0)
       
     return sim
 
 if __name__ == '__main__':
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-t', '--timestep')
+    # parser.add_argument('-e', '--end')  
+    
+    # args = parser.parse_args()
+    
+    debug = True
+    
     logging.basicConfig(format='[%(asctime)s %(name)s %(levelname)s] %(message)s', datefmt='%H:%M:%S', level=logging.DEBUG)
 
-    time_step = 0.01
+    time_step = 0.001
     final_time = 1
     t_k = 0.0
     
@@ -167,39 +149,50 @@ if __name__ == '__main__':
     sim2 = set_dpsim2(time_step, final_time)
     sim2.start()
     y_2_0 = sim2.get_idobj_attr("i_intf", "v_intf").derive_coeff(0,0).get()
-    print("Output value from S2: {:f}".format(y_2_0))
+    
+    if debug:
+        print("Output value from S2: {:f}".format(y_2_0))
    
     # Communication y_2_0 -> S_1 and initialization of S_1
     u_1_0 = y_2_0
     sim1 = set_dpsim1(time_step, final_time, u_1_0)
     sim1.start()
     y_1_0 = sim1.get_idobj_attr("v_intf", "i_intf").derive_coeff(0,0).get()
-    print("Output value from S1: {:f}".format(y_1_0))
+    
+    if debug:
+        print("Output value from S1: {:f}".format(y_1_0))
     
     y_1 = y_1_0
     u_1 = y_1_0
     
     while t_k < final_time:
-        # Switch to S_2
-        u_2_prev = sim2.get_idobj_attr("i_intf", "i_intf").derive_coeff(0,0).get()
-        print("Input value in S2 before set: {:f}".format(u_2_prev))
+        # Switch to S_2        
+        if debug:
+            u_2_prev = sim2.get_idobj_attr("i_intf", "i_intf").derive_coeff(0,0).get()
+            print("Input value in S2 before set: {:f}".format(u_2_prev))
         
         u_2 = y_1
         sim2.get_idobj_attr("i_intf", "I_ref").set(complex(u_2,0))
-        u_2_test = sim2.get_idobj_attr("i_intf", "I_ref").get()
-        print("Input value in S2 after set: {:f}".format(u_2_test))
+        
+        if debug:
+            u_2_test = sim2.get_idobj_attr("i_intf", "I_ref").get()
+            print("Input value in S2 after set: {:f}".format(u_2_test))
         
         sim2.next()
-        y_2 = -sim2.get_idobj_attr("i_intf", "v_intf").derive_coeff(0,0).get()
-        print("Output value from S2: {:f}".format(y_2))
+        y_2 = sim2.get_idobj_attr("i_intf", "v_intf").derive_coeff(0,0).get()
+        
+        if debug:
+            print("Output value from S2: {:f}".format(y_2))
         
         u_1 = y_2
         
         sim1.get_idobj_attr("v_intf", "V_ref").set(complex(u_1,0))
         t_k = sim1.next()
         y_1 = sim1.get_idobj_attr("v_intf", "i_intf").derive_coeff(0,0).get()
-        print("Output value from S1: {:f}".format(y_1))
-        print("Time t={:f}".format(t_k))
+        
+        if debug:
+            print("Output value from S1: {:f}".format(y_1))
+            print("Time t={:f}".format(t_k))
 
     sim2.stop()
     sim1.stop()
