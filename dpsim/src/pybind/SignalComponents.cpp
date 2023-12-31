e8b91a4ba7ba2d3ec6ed3bea8026d3e4b186a843
Glebs Ogorodniks <glebs.ogorodniks@rwth-aachen.de>
2023-12-31 15:06:02 +0100

    extend doc of sg controllers
    Signed-off-by: Glebs Ogorodniks <glebs.ogorodniks@rwth-aachen.de>

--- dpsim/src/pybind/SignalComponents.cpp
+++ dpsim/src/pybind/SignalComponents.cpp
@@ -131,21 +131,47 @@ void addSignalComponents(py::module_ mSignal) {
       .def(py::init<std::string, CPS::Logger::Level>(), "name"_a,
            "loglevel"_a = CPS::Logger::Level::off);
 
-  py::class_<CPS::Signal::PSS1AParameters,
-             std::shared_ptr<CPS::Signal::PSS1AParameters>,
-             CPS::Base::PSSParameters>(mSignal, "PSS1AParameters",
-                                       py::multiple_inheritance())
+  py::class_<CPS::Signal::TurbineGovernorType1Parameters,
+             std::shared_ptr<CPS::Signal::TurbineGovernorType1Parameters>,
+             CPS::Base::GovernorParameters>(
+      mSignal, "TurbineGovernorType1Parameters", py::multiple_inheritance())
       .def(py::init())
-      .def_readwrite("Kp", &CPS::Signal::PSS1AParameters::Kp)
-      .def_readwrite("Kv", &CPS::Signal::PSS1AParameters::Kv)
-      .def_readwrite("Kw", &CPS::Signal::PSS1AParameters::Kw)
-      .def_readwrite("T1", &CPS::Signal::PSS1AParameters::T1)
-      .def_readwrite("T2", &CPS::Signal::PSS1AParameters::T2)
-      .def_readwrite("T3", &CPS::Signal::PSS1AParameters::T3)
-      .def_readwrite("T4", &CPS::Signal::PSS1AParameters::T4)
-      .def_readwrite("Tw", &CPS::Signal::PSS1AParameters::Tw)
-      .def_readwrite("Vs_max", &CPS::Signal::PSS1AParameters::Vs_max)
-      .def_readwrite("Vs_min", &CPS::Signal::PSS1AParameters::Vs_min);
+      .def_readwrite("Pmax", &CPS::Signal::TurbineGovernorType1Parameters::Pmax)
+      .def_readwrite("Pmin", &CPS::Signal::TurbineGovernorType1Parameters::Pmin)
+      .def_readwrite("R", &CPS::Signal::TurbineGovernorType1Parameters::R)
+      .def_readwrite("T3", &CPS::Signal::TurbineGovernorType1Parameters::T3)
+      .def_readwrite("T4", &CPS::Signal::TurbineGovernorType1Parameters::T4)
+      .def_readwrite("T5", &CPS::Signal::TurbineGovernorType1Parameters::T5)
+      .def_readwrite("Tc", &CPS::Signal::TurbineGovernorType1Parameters::Tc)
+      .def_readwrite("Ts", &CPS::Signal::TurbineGovernorType1Parameters::Ts)
+      .def_readwrite("OmRef",
+                     &CPS::Signal::TurbineGovernorType1Parameters::OmRef);
+  py::class_<CPS::Signal::HydroGorvernorParameters,
+             std::shared_ptr<CPS::Signal::HydroGorvernorParameters>,
+             CPS::Base::GovernorParameters>(mSignal, "HydroGorvernorParameters",
+                                            py::multiple_inheritance())
+      .def(py::init())
+      .def_readwrite("R", &CPS::Signal::HydroGorvernorParameters::R)
+      .def_readwrite("T1", &CPS::Signal::HydroGorvernorParameters::T1)
+      .def_readwrite("T2", &CPS::Signal::HydroGorvernorParameters::T2)
+      .def_readwrite("T3", &CPS::Signal::HydroGorvernorParameters::T3)
+      .def_readwrite("Pmax", &CPS::Signal::HydroGorvernorParameters::Pmax)
+      .def_readwrite("Pmin", &CPS::Signal::HydroGorvernorParameters::Pmin)
+      .def_readwrite("OmRef", &CPS::Signal::HydroGorvernorParameters::OmRef);
+  py::class_<CPS::Signal::SteamGorvernorParameters,
+             std::shared_ptr<CPS::Signal::SteamGorvernorParameters>,
+             CPS::Base::GovernorParameters>(mSignal, "SteamGorvernorParameters",
+                                            py::multiple_inheritance())
+      .def(py::init())
+      .def_readwrite("R", &CPS::Signal::SteamGorvernorParameters::R)
+      .def_readwrite("T1", &CPS::Signal::SteamGorvernorParameters::T1)
+      .def_readwrite("T2", &CPS::Signal::SteamGorvernorParameters::T2)
+      .def_readwrite("T3", &CPS::Signal::SteamGorvernorParameters::T3)
+      .def_readwrite("dPmax", &CPS::Signal::SteamGorvernorParameters::dPmax)
+      .def_readwrite("dPmin", &CPS::Signal::SteamGorvernorParameters::dPmin)
+      .def_readwrite("Pmax", &CPS::Signal::SteamGorvernorParameters::Pmax)
+      .def_readwrite("Pmin", &CPS::Signal::SteamGorvernorParameters::Pmin)
+      .def_readwrite("OmRef", &CPS::Signal::SteamGorvernorParameters::OmRef);
 
   // Governos
   py::class_<CPS::Signal::TurbineGovernorType1,
