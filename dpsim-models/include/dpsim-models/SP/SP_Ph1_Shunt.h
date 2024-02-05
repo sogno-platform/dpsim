358da4788994d45fc3072638e6ef4081bff1a417
martin.moraga <martin.moraga@rwth-aachen.de>
2024-02-05 14:28:55 +0100

    extend shunt SP domain for MNA solver
    Signed-off-by: martin.moraga <martin.moraga@rwth-aachen.de>

--- dpsim-models/include/dpsim-models/SP/SP_Ph1_Shunt.h
+++ dpsim-models/include/dpsim-models/SP/SP_Ph1_Shunt.h
@@ -7,51 +7,72 @@
  *********************************************************************************/
 
 #pragma once
-#include <dpsim-models/SimPowerComp.h>
+
+#include <dpsim-models/CompositePowerComp.h>
 #include <dpsim-models/Solver/PFSolverInterfaceBranch.h>
+#include <dpsim-models/SP/SP_Ph1_Capacitor.h>
+#include <dpsim-models/SP/SP_Ph1_Resistor.h>
 
 namespace CPS {
-
 namespace SP {
 namespace Ph1 {
 
-class Shunt : public SimPowerComp<Complex>,
-              public SharedFactory<Shunt>,
-              public PFSolverInterfaceBranch {
-public:
-  /// Conductance [S]
-  const Attribute<Real>::Ptr mConductance;
-  /// Susceptance [S]
-  const Attribute<Real>::Ptr mSusceptance;
-  /// Conductance [pu]
-  const Attribute<Real>::Ptr mConductancePerUnit;
-  /// Susceptance [pu]
-  const Attribute<Real>::Ptr mSusceptancePerUnit;
+	class Shunt : 
+		public CompositePowerComp<Complex>, 
+		public SharedFactory<Shunt>, 
+		public PFSolverInterfaceBranch {
+	public:
+		/// Conductance [S]
+		const Attribute<Real>::Ptr mConductance;
+		/// Susceptance [S]
+		const Attribute<Real>::Ptr mSusceptance;
+		/// Conductance [pu]
+		const Attribute<Real>::Ptr mConductancePerUnit;
+		/// Susceptance [pu]
+		const Attribute<Real>::Ptr mSusceptancePerUnit;
 
-private:
-  /// Base voltage [V]
-  Real mBaseVoltage;
+		/// Capacitor between terminal and ground
+		std::shared_ptr<Capacitor> mSubCapacitor;
+		/// Resistor between terminal and ground
+		std::shared_ptr<Resistor> mSubResistor;
 
-public:
-  /// Defines UID, name, component parameters and logging level
-  Shunt(String uid, String name, Logger::Level logLevel = Logger::Level::off);
+	private:
+        /// Base voltage [V]
+        Real mBaseVoltage;
 
-  /// Defines name and logging level
-  Shunt(String name, Logger::Level logLevel = Logger::Level::off)
-      : Shunt(name, name, logLevel) {}
+	public:
+		/// Defines UID, name, component parameters and logging level
+		Shunt(String uid, String name, Logger::Level logLevel = Logger::Level::off);
 
-  // #### General ####
-  /// Set shunt specific parameters
-  void setParameters(Real conductance, Real susceptance);
+		/// Defines name and logging level
+		Shunt(String name, Logger::Level logLevel = Logger::Level::off)
+			: Shunt(name, name, logLevel) { }
 
-  // #### Powerflow section ####
-  /// Set base voltage
-  void setBaseVoltage(Real baseVoltage);
-  /// Initializes component from power flow data
-  void calculatePerUnitParameters(Real baseApparentPower, Real baseOmega);
-  /// Stamps admittance matrix
-  void pfApplyAdmittanceMatrixStamp(SparseMatrixCompRow &Y);
-};
-} // namespace Ph1
-} // namespace SP
-} // namespace CPS
+		// #### General ####
+		/// Set shunt specific parameters
+		void setParameters(Real conductance, Real susceptance);
+
+		// #### Powerflow section ####
+		/// Set base voltage
+		void setBaseVoltage(Real baseVoltage);
+		/// Initializes component from power flow data
+		void calculatePerUnitParameters(Real baseApparentPower, Real baseOmega);
+		/// Stamps admittance matrix
+		void pfApplyAdmittanceMatrixStamp(SparseMatrixCompRow & Y);
+
+		// #### MNA section ####
+		/// Initializes component from power flow data
+		void initializeFromNodesAndTerminals(Real frequency) final;
+		/// Updates internal current variable of the component
+		void mnaCompUpdateCurrent(const Matrix& leftVector) final;
+		/// Updates internal voltage variable of the component
+		void mnaCompUpdateVoltage(const Matrix& leftVector) final;
+		/// MNA post-step operations
+		void mnaParentPostStep(Real time, Int timeStepCount, Attribute<Matrix>::Ptr &leftVector) final;
+		/// add MNA post-step dependencies
+		void mnaParentAddPostStepDependencies(AttributeBase::List &prevStepDependencies, AttributeBase::List &attributeDependencies, AttributeBase::List &modifiedAttributes, Attribute<Matrix>::Ptr &leftVector) final;
+
+	};
+}
+}
+}
