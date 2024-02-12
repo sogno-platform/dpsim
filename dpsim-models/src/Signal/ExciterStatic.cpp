48257f00c2b1e4ceebc6923a26830ffa5a2fa7b9
martin.moraga <martin.moraga@rwth-aachen.de>
2024-02-12 10:23:04 +0100

    improve implementation of exciter static
    Signed-off-by: martin.moraga <martin.moraga@rwth-aachen.de>

--- dpsim-models/src/Signal/ExciterStatic.cpp
+++ dpsim-models/src/Signal/ExciterStatic.cpp
@@ -20,15 +20,18 @@ void ExciterStatic::setParameters(std::shared_ptr<Base::ExciterParameters> param
 
 		SPDLOG_LOGGER_INFO(mSLog, 
 			"\nExciteStatic parameters:"
+			"\nTr: {:e}"
 			"\nTa: {:e}"
 			"\nTb: {:e}"
 			"\nTe: {:e}"
 			"\nKa: {:e}"
 			"\nMaximum EMF: {:e}"
-			"\nMinimum EMF: {:e}\n",
-			mParameters->Ta, mParameters->Tb, 
-			mParameters->Te, mParameters->Ka,
-			mParameters->MaxEfd, mParameters->MinEfd);
+			"\nMinimum EMF: {:e}"
+			"\nKbc: {:e}\n",
+			mParameters->Tr, mParameters->Ta, 
+			mParameters->Tb, mParameters->Te, 
+			mParameters->Ka, mParameters->MaxEfd, 
+			mParameters->MinEfd, mParameters->Kbc);
 	} else {
 		std::cout << "Type of parameters class of " << this->name() << " has to be ExciterStatic!" << std::endl;
 		throw CPS::TypeException();
@@ -39,86 +42,83 @@ void ExciterStatic::initialize(Real Vh_init, Real Ef_init) {
 	
 	mVh = Vh_init;
 	mEfd = Ef_init;
-	mEfd_next = Ef_init;
-	mEfd_lim_in=Ef_init;
-	mEfd_lim_in_next=Ef_init;
+	mEfdLim = mEfd;
 
-	SPDLOG_LOGGER_INFO(mSLog, 
-		"Initially set excitation system initial values:"
-		"\ninit Vh: {:e}"
-		"\ninit Ef: {:e}",
-		mVh, mEfd);
-
-	if ((Ef_init>mParameters->MaxEfd) || (Ef_init<mParameters->MinEfd))
-		SPDLOG_LOGGER_WARN(mSLog, 
-			"\nWARNING: The initialisation is bad, Ef_init out of allowed band ({}<Ef_init<{})."
-			"\nThe simulation will be continued ",
+	if ((Ef_init>mParameters->MaxEfd) || (Ef_init<mParameters->MinEfd)) {
+		SPDLOG_LOGGER_ERROR(mSLog, 
+			"\nERROR: The initialisation is bad, Ef_init out of allowed band ({}<Ef_init<{}).",
 			mParameters->MinEfd, mParameters->MaxEfd);
+		throw CPS::Exception();
+	}
 
 	// initialize auxiliar parameters
 	mCa = mParameters->Ta / mParameters->Tb;
 	mCb = (mParameters->Tb - mParameters->Ta) / mParameters->Tb;
 
-	///
-	mVref = mVh + mEfd / mParameters->Ka;
+	/// init value of transducer output
+	mVr = mVh;
+	mVr_prev = mVr;
+
+	// init value of the second lag block
+	mVe = mEfd / mParameters->Ka;
 
 	/// Input of the first lead lag block
-	mVin = mVref - mVh;
+	mVin = mVe / (mCa + mCb);
+
+	/// Initial reference value
+	mVref = mVin - mVr;
 
 	/// Initial value for auxilar state variable
-	mXb = (mEfd / mParameters->Ka - mVin * mCa) / mCb ;
-	mXb_next = mXb;
+	mXb = mVin;
+	mXb_prev = mXb;
 	
-	SPDLOG_LOGGER_INFO(mSLog, 
+	SPDLOG_LOGGER_INFO(mSLog,
 		"\nExciter Initialization"
 		"\nExciter type: ExciterStatic"
+		"\nInitially set excitation system initial values:"
+		"\ninit Vh: {:e}"
+		"\ninit Ef: {:e}",
 		"\nCalculated set poit and auxilary state variables:"
 		"\nVref : {:e}"
 		"\nXb : {:e}",
-		"\nVin = Vref-Vh : {:e}",
+		"\nVin = {:e}",
+		mVh, mEfd,
 		mVref, mXb, mVin);
 	mSLog->flush();
 }
 
 Real ExciterStatic::step(Real mVd, Real mVq, Real dt, Real Vpss) {
-	
-	//Wind-up constant
-	//FIX ME: when cKbc=0 the behaviour is not as expected. (Exprected is the behavious as without Windup)
-	const Real cKbc=10;
 
 	// Voltage magnitude calculation
 	mVh = sqrt(pow(mVd, 2.) + pow(mVq, 2.));
 
-	/// Update state variables that were calculated in last step
-	mEfd = mEfd_next;
-	mEfd_lim_in = mEfd_lim_in_next;
-	mXb = mXb_next;
+	// update state variables at time k
+	mVr_prev = mVr;
+	mXb_prev = mXb;
+
+	/// Compute Vr at time k
+	if (mParameters->Tr>0)
+		mVr = mVr_prev + dt / mParameters->Tr * (mVh - mVr_prev);
+	else
+		mVr = mVh;
+	
+	/// Compute Vin at time k
+	mVin = mVref - mVr + Vpss;
 
 	/// Compute Xb at time k+1 using euler forward
-	mVin = mVref - mVh + Vpss;
-	mXb_next = mXb + (mVin - mXb) * dt / mParameters->Tb;
+	mXb = mXb_prev + (mVin - mXb_prev) * dt / mParameters->Tb;
 	
-	// Compute Edf at time k+1 using euler forward
-	mVe = (mVin * mCa + mXb * mCb) * mParameters->Ka;
-
-	/*
-	/// Integrator for T_e time constant !without! Wind-up
-	mEfd_next = (mVe-mEfd) * dt / mParameters->Te + mEfd;
-	/// limiter for Efd
-	if (mEfd_next > mParameters->MaxEfd)
-		mEfd_next = mParameters->MaxEfd;
-	else if (mEfd_next < mParameters->MinEfd)
-		mEfd_next = mParameters->MinEfd;
-	*/
+	// Compute Efd at time k+1 using euler forward
+	mVe = mVin * mCa + mXb * mCb - mParameters->Kbc * (mEfd - mEfdLim);
 	
 	//Integrator for T_e time constant !with! Wind-up
-	mEfd_lim_in_next = dt*((mVe-mEfd) / mParameters->Te - cKbc*(mEfd_lim_in-mEfd)) + mEfd_lim_in;
-	if (mEfd_lim_in_next > mParameters->MaxEfd)
-		mEfd_next = mParameters->MaxEfd;
-	else if (mEfd_lim_in_next < mParameters->MinEfd)
-		mEfd_next = mParameters->MinEfd;
+	mEfd = mEfd + (dt / mParameters->Te) * (mParameters->Ka * mVe - mEfd);
+	if (mEfd > mParameters->MaxEfd)
+		mEfdLim = mParameters->MaxEfd;
+	else if (mEfd < mParameters->MinEfd)
+		mEfdLim = mParameters->MinEfd;
 	else
-		mEfd_next=mEfd_lim_in_next;
+		mEfdLim = mEfd;
 
-	return mEfd;
+	return mEfdLim;
 }
