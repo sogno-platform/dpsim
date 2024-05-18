a8f07478a7194516c4233def1364fe313cc0eaa5
Georgii Tishenin <georgii.tishenin@eonerc.rwth-aachen.de>
2024-05-18 13:49:18 +0200

    Add utility class for MNA matrix stamping operations
    Signed-off-by: Georgii Tishenin <georgii.tishenin@eonerc.rwth-aachen.de>

---
+++ dpsim-models/include/dpsim-models/MNAStampUtils.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <dpsim-models/MathUtils.h>
+
+namespace CPS {
+class MNAStampUtils {
+public:
+  static void stampConductance(Real conductance, SparseMatrixRow &mat,
+                               UInt node1Index, UInt node2Index,
+                               Bool isTerminal1NotGrounded,
+                               Bool isTerminal2NotGrounded);
+
+  static void stampAdmittance(Complex admittance, SparseMatrixRow &mat,
+                              UInt node1Index, UInt node2Index,
+                              Bool isTerminal1NotGrounded,
+                              Bool isTerminal2NotGrounded, Int maxFreq = 1,
+                              Int freqIdx = 0);
+
+  static void stampConductanceMatrix(const Matrix &conductanceMat,
+                                     SparseMatrixRow &mat, UInt node1Index,
+                                     UInt node2Index,
+                                     Bool isTerminal1NotGrounded,
+                                     Bool isTerminal2NotGrounded);
+
+  static void stampAdmittanceMatrix(const MatrixComp &admittanceMat,
+                                    SparseMatrixRow &mat, UInt node1Index,
+                                    UInt node2Index,
+                                    Bool isTerminal1NotGrounded,
+                                    Bool isTerminal2NotGrounded,
+                                    Int maxFreq = 1, Int freqIdx = 0);
+
+private:
+  template <typename T>
+  static void stampValue(T value, SparseMatrixRow &mat, UInt node1Index,
+                         UInt node2Index, Bool isTerminal1NotGrounded,
+                         Bool isTerminal2NotGrounded, Int maxFreq, Int freqIdx);
+
+  template <typename T>
+  static void
+  stampMatrix(const MatrixVar<T> &matrix, SparseMatrixRow &mat, UInt node1Index,
+              UInt node2Index, Bool isTerminal1NotGrounded,
+              Bool isTerminal2NotGrounded, Int maxFreq, Int freqIdx);
+
+  template <typename T>
+  static void stampValueNoConditions(T value, SparseMatrixRow &mat,
+                                     UInt node1Index, UInt node2Index,
+                                     Int maxFreq, Int freqIdx);
+
+  template <typename T>
+  static void stampValueOnDiagonalNoConditions(T value, SparseMatrixRow &mat,
+                                               UInt nodeIndex, Int maxFreq,
+                                               Int freqIdx);
+
+  template <typename T>
+  static void stampValueOffDiagonalNoConditions(T value, SparseMatrixRow &mat,
+                                                UInt node1Index,
+                                                UInt node2Index, Int maxFreq,
+                                                Int freqIdx);
+
+  static void addToMatrixElement(SparseMatrixRow &mat, Matrix::Index row,
+                          Matrix::Index column, Real value, Int maxFreq,
+                          Int freqIdx);
+
+  static void addToMatrixElement(SparseMatrixRow &mat, Matrix::Index row,
+                          Matrix::Index column, Complex value, Int maxFreq,
+                          Int freqIdx);
+};
+} // namespace CPS
