95fb64c28afb0e63342e77e4d13175bf0fffa505
Niklas Eiling <niklas.eiling@eonerc.rwth-aachen.de>
2024-06-06 15:41:12 +0200

    FpgaExample: allow different topologies to be tested
    Signed-off-by: Niklas Eiling <niklas.eiling@eonerc.rwth-aachen.de>

--- dpsim-villas/examples/cxx/FpgaExample.cpp
+++ dpsim-villas/examples/cxx/FpgaExample.cpp
@@ -18,8 +18,7 @@ using namespace DPsim;
 using namespace CPS::DP;
 using namespace CPS::DP::Ph1;
 
-int main(int argc, char *argv[]) {
-  CommandLineArgs args(argc, argv, "FpgaExample", 0.01, 10 * 60, 5.);
+const std::string buildFpgaConfig(CommandLineArgs &args) {
   std::filesystem::path fpgaIpPath =
       "/usr/local/etc/villas/node/etc/fpga/vc707-xbar-pcie-dino/"
       "vc707-xbar-pcie-dino.json";
@@ -27,32 +26,6 @@ int main(int argc, char *argv[]) {
   if (args.options.find("ips") != args.options.end()) {
     fpgaIpPath = std::filesystem::path(args.getOptionString("ips"));
   }
-  CPS::Logger::setLogDir("logs/" + args.name);
-
-  // Nodes
-  auto n1 = SimNode::make("n1");
-  auto n2 = SimNode::make("n2");
-
-  // Components
-  auto vs = VoltageSource::make("v_s");
-  vs->setParameters(Complex(10, 0), args.sysFreq);
-  auto rs = Resistor::make("r_s");
-  rs->setParameters(1);
-
-  auto cs = CurrentSource::make("i_l");
-  cs->setParameters(Complex(0, 0));
-
-  // Topology
-  vs->connect({SimNode::GND, n1});
-  rs->connect({n1, n2});
-  cs->connect({n2, SimNode::GND});
-
-  auto sys = SystemTopology(args.sysFreq, SystemNodeList{SimNode::GND, n1, n2},
-                            SystemComponentList{vs, rs, cs});
-
-  RealTimeSimulation sim(args.name, args);
-  sim.setSystem(sys);
-
   std::string cardConfig = fmt::format(
       R"STRING("card": {{
       "interface": "pcie",
@@ -105,7 +78,7 @@ int main(int argc, char *argv[]) {
       }}]
     }})STRING",
       args.sysFreq, args.timeStep);
-  std::string config = fmt::format(
+  const std::string config = fmt::format(
       R"STRING({{
     "type": "fpga",
     {},
@@ -114,23 +87,102 @@ int main(int argc, char *argv[]) {
   }})STRING",
       cardConfig, signalOutConfig, signalInConfig);
   DPsim::Logger::get("FpgaExample")->debug("Config for Node:\n{}", config);
+  return config;
+}
 
-  auto intf = std::make_shared<InterfaceVillas>(config);
+SystemTopology loopbackTopology(CommandLineArgs &args,
+                                std::shared_ptr<InterfaceVillas> intf,
+                                std::shared_ptr<DataLogger> logger) {
+  // Nodes
+  auto n1 = SimNode::make("n1");
+
+  // Components
+  auto vs = VoltageSource::make("v_s");
+  vs->setParameters(Complex(10, 0), args.sysFreq);
+  auto rl = Resistor::make("r_l");
+  rl->setParameters(1);
+
+  // Topology
+  vs->connect({SimNode::GND, n1});
+  rl->connect({n1, SimNode::GND});
+
+  // Interface
+  intf->importAttribute(vs->mVoltageRef, 0, false, false, "from_dino", "A");
+  intf->exportAttribute(n1->mVoltage->deriveCoeff<Complex>(0, 0), 0, true,
+                        "to_dino", "V");
+  intf->printVillasSignals();
+
+  // Logger
+  logger->logAttribute("v1", n1->mVoltage);
+  logger->logAttribute("rl_i", rl->mIntfCurrent);
+
+  return SystemTopology(args.sysFreq, SystemNodeList{SimNode::GND, n1},
+                        SystemComponentList{vs, rl});
+}
+
+SystemTopology hilTopology(CommandLineArgs &args,
+                           std::shared_ptr<InterfaceVillas> intf,
+                           std::shared_ptr<DataLogger> logger) {
+  // Nodes
+  auto n1 = SimNode::make("n1");
+  auto n2 = SimNode::make("n2");
+
+  // Components
+  auto vs = VoltageSource::make("v_s");
+  vs->setParameters(Complex(10, 0), args.sysFreq);
+  auto rs = Resistor::make("r_s");
+  rs->setParameters(1);
+
+  auto cs = CurrentSource::make("i_l");
+  cs->setParameters(Complex(0, 0));
+
+  // Topology
+  vs->connect({SimNode::GND, n1});
+  rs->connect({n1, n2});
+  cs->connect({n2, SimNode::GND});
 
   // Interface
   intf->importAttribute(cs->mCurrentRef, 0, false, false, "from_dino", "A");
   intf->exportAttribute(n2->mVoltage->deriveCoeff<Complex>(0, 0), 0, true,
                         "to_dino", "V");
-  sim.addInterface(intf);
   intf->printVillasSignals();
 
   // Logger
-  auto logger = DataLogger::make(args.name);
   logger->logAttribute("v1", n1->mVoltage);
   logger->logAttribute("v2", n2->mVoltage);
   logger->logAttribute("cs_i", cs->mIntfCurrent);
-  sim.addLogger(logger);
 
+  return SystemTopology(args.sysFreq, SystemNodeList{SimNode::GND, n1, n2},
+                        SystemComponentList{vs, rs, cs});
+}
+
+SystemTopology getTopology(CommandLineArgs &args,
+                           std::shared_ptr<InterfaceVillas> intf,
+                           std::shared_ptr<DataLogger> logger) {
+  if (args.options.find("topology") != args.options.end()) {
+    std::string topology = args.getOptionString("topology");
+    if (topology == "hil") {
+      return hilTopology(args, intf, logger);
+    } else if (topology == "loopback") {
+      return loopbackTopology(args, intf, logger);
+    }
+  }
+  return hilTopology(args, intf, logger);
+}
+
+int main(int argc, char *argv[]) {
+  CommandLineArgs args(argc, argv, "FpgaExample", 0.01, 10 * 60, 5.);
+  CPS::Logger::setLogDir("logs/" + args.name);
+
+  auto intf = std::make_shared<InterfaceVillas>(buildFpgaConfig(args));
+  auto logger = DataLogger::make(args.name);
+
+  auto sys = getTopology(args, intf, logger);
+
+  RealTimeSimulation sim(args.name, args);
+  sim.setSystem(sys);
+  sim.addInterface(intf);
+  sim.addLogger(logger);
   sim.run();
 
   //std::ofstream of("task_dependencies.svg");
