1aff0f6c350da37761c8d18cf8cf6c9d596241e1
Marvin Tollnitsch <marvin.tollnitsch@rwth-aachen.de>
2024-06-29 17:11:24 +0200

    [Initial commit] Added Ph1::Switch model
    - added Ph1::Switch header and cpp files
    - added header to Components.h
    - added cpp to CMakeLists.txt
    - made Ph1::Switch usable in pybind by adding it in EMTComponents.cpp
    - added simple example circuit (VS_sw_R_L) for new EMT::Ph1::Switch model
    
    Signed-off-by: Marvin Tollnitsch <marvin.tollnitsch@rwth-aachen.de>

---
+++ dpsim-models/include/dpsim-models/EMT/EMT_Ph1_Switch.h
@@ -0,0 +1,68 @@
+/* Copyright 2017-2021 Institute for Automation of Complex Power Systems,
+ *                     EONERC, RWTH Aachen University
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *********************************************************************************/
+
+#pragma once
+
+#include <dpsim-models/MNASimPowerComp.h>
+#include <dpsim-models/Solver/MNASwitchInterface.h>
+#include <dpsim-models/Solver/MNAInterface.h>
+#include <dpsim-models/Definitions.h>
+#include <dpsim-models/Logger.h>
+#include <dpsim-models/Base/Base_Ph1_Switch.h>
+
+namespace CPS {
+namespace EMT {
+namespace Ph1 {
+	/// \brief One phase EMT switch
+	///
+	/// The switch can be opened and closed.
+	/// Each state has a specific resistance value.
+	class Switch :
+		public MNASimPowerComp<Real>,
+		public Base::Ph1::Switch,
+		public SharedFactory<Switch>,
+		public MNASwitchInterface {
+
+	public:
+		/// Defines UID, name, component parameters and logging level
+		Switch(String uid, String name,	Logger::Level loglevel = Logger::Level::off);
+		/// Defines name, component parameters and logging level
+		Switch(String name, Logger::Level logLevel = Logger::Level::off)
+			: Switch(name, name, logLevel) { }
+
+		SimPowerComp<Real>::Ptr clone(String name);
+
+		// #### General ####
+		/// Initializes component from power flow data
+		void initializeFromNodesAndTerminals(Real frequency);
+
+		// #### General MNA section ####
+		void mnaCompInitialize(Real omega, Real timeStep, Attribute<Matrix>::Ptr leftVector);
+		/// Stamps system matrix
+		void mnaCompApplySystemMatrixStamp(SparseMatrixRow& systemMatrix);
+		/// Stamps right side (source) vector
+		void mnaCompApplyRightSideVectorStamp(Matrix& rightVector);
+		/// Update interface voltage from MNA system result
+		void mnaCompUpdateVoltage(const Matrix& leftVector);
+		/// Update interface current from MNA system result
+		void mnaCompUpdateCurrent(const Matrix& leftVector);
+
+		// #### MNA section for switches ####
+		/// Check if switch is closed
+		Bool mnaIsClosed() override;
+		/// Stamps system matrix considering the defined switch position
+		void mnaCompApplySwitchSystemMatrixStamp(Bool closed, SparseMatrixRow& systemMatrix, Int freqIdx);
+		/// MNA post step operations
+		void mnaCompPostStep(Real time, Int timeStepCount, Attribute<Matrix>::Ptr &leftVector) override;
+		
+		/// Add MNA post step dependencies
+		void mnaCompAddPostStepDependencies(AttributeBase::List &prevStepDependencies, AttributeBase::List &attributeDependencies, AttributeBase::List &modifiedAttributes, Attribute<Matrix>::Ptr &leftVector) override;
+	};
+}
+}
+}
---
+++ dpsim-models/src/EMT/EMT_Ph1_Switch.cpp
@@ -0,0 +1,135 @@
+/* Copyright 2017-2021 Institute for Automation of Complex Power Systems,
+ *                     EONERC, RWTH Aachen University
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *********************************************************************************/
+
+#include <dpsim-models/EMT/EMT_Ph1_Switch.h>
+
+using namespace CPS;
+
+// !!! TODO: 	Adaptions to use in EMT_Ph1 models phase-to-ground peak variables
+// !!! 			with initialization from phase-to-phase RMS variables
+
+///FIXME: Inconsistent naming in ph1 and p3 switches: (mIsClosed, mSwitchClosed)
+
+EMT::Ph1::Switch::Switch(String uid, String name, Logger::Level logLevel)
+	: MNASimPowerComp<Real>(uid, name, false, true, logLevel), Base::Ph1::Switch(mAttributes) {
+	setTerminalNumber(2);
+	**mIntfVoltage = Matrix::Zero(1,1);
+	**mIntfCurrent = Matrix::Zero(1,1);
+}
+
+SimPowerComp<Real>::Ptr EMT::Ph1::Switch::clone(String name) {
+	auto copy = Switch::make(name, mLogLevel);
+	copy->setParameters(**mOpenResistance, **mClosedResistance, **mIsClosed);
+	return copy;
+}
+
+void EMT::Ph1::Switch::initializeFromNodesAndTerminals(Real frequency) {
+
+	Real resistance = (**mIsClosed) ? **mClosedResistance : **mOpenResistance;
+
+	(**mIntfVoltage)(0,0) = (initialSingleVoltage(1) - initialSingleVoltage(0)).real();
+	(**mIntfCurrent)(0,0) = (**mIntfVoltage)(0, 0) / resistance;
+
+	SPDLOG_LOGGER_INFO(mSLog,
+		"\n--- Initialization from powerflow ---"
+		"\nVoltage across: {:s}"
+		"\nCurrent: {:s}"
+		"\nTerminal 0 voltage: {:s}"
+		"\nTerminal 1 voltage: {:s}"
+		"\n--- Initialization from powerflow finished ---",
+		Logger::matrixToString(**mIntfVoltage),
+		Logger::matrixToString(**mIntfCurrent),
+		Logger::phasorToString(initialSingleVoltage(0)),
+		Logger::phasorToString(initialSingleVoltage(1)));
+}
+
+void EMT::Ph1::Switch::mnaCompInitialize(Real omega, Real timeStep, Attribute<Matrix>::Ptr leftVector) {
+
+	updateMatrixNodeIndices();
+
+	mMnaTasks.push_back(std::make_shared<MnaPostStep>(*this, leftVector));
+}
+
+Bool EMT::Ph1::Switch::mnaIsClosed() { return **mIsClosed; }
+
+void EMT::Ph1::Switch::mnaCompApplySystemMatrixStamp(SparseMatrixRow& systemMatrix) {
+	Real conductance;
+
+	conductance = (**mIsClosed) ?
+		1./(**mClosedResistance) : 1./(**mOpenResistance);
+
+	// Set diagonal entries
+	if (terminalNotGrounded(0)) {
+		Math::addToMatrixElement(systemMatrix, matrixNodeIndex(0, 0), matrixNodeIndex(0, 0), conductance);
+	}
+	if (terminalNotGrounded(1)) {
+		Math::addToMatrixElement(systemMatrix, matrixNodeIndex(1, 0), matrixNodeIndex(1, 0), conductance);
+	}
+	if (terminalNotGrounded(0) && terminalNotGrounded(1)) {
+		Math::addToMatrixElement(systemMatrix, matrixNodeIndex(0, 0), matrixNodeIndex(1, 0), -conductance);
+		Math::addToMatrixElement(systemMatrix, matrixNodeIndex(1, 0), matrixNodeIndex(0, 0), -conductance);
+	}
+	SPDLOG_LOGGER_TRACE(mSLog, 
+		"\nConductance matrix: {:s}",
+		Logger::realToString(conductance));
+}
+
+void EMT::Ph1::Switch::mnaCompApplySwitchSystemMatrixStamp(Bool closed, SparseMatrixRow& systemMatrix, Int freqIdx) {
+	Real conductance;
+
+	conductance = (closed) ?
+		1./(**mClosedResistance) : 1./(**mOpenResistance);
+
+	// Set diagonal entries
+	if (terminalNotGrounded(0)) {
+		Math::addToMatrixElement(systemMatrix, matrixNodeIndex(0, 0), matrixNodeIndex(0, 0), conductance);
+
+	}
+	if (terminalNotGrounded(1)) {
+		Math::addToMatrixElement(systemMatrix, matrixNodeIndex(1, 0), matrixNodeIndex(1, 0), conductance);
+	}
+	// Set off diagonal blocks, 2x3x3 entries
+	if (terminalNotGrounded(0) && terminalNotGrounded(1)) {
+		Math::addToMatrixElement(systemMatrix, matrixNodeIndex(0, 0), matrixNodeIndex(1, 0), -conductance);
+		Math::addToMatrixElement(systemMatrix, matrixNodeIndex(1, 0), matrixNodeIndex(0, 0), -conductance);
+	}
+
+	SPDLOG_LOGGER_TRACE(mSLog, 
+		"\nConductance matrix: {:s}",
+		Logger::realToString(conductance));
+}
+
+void EMT::Ph1::Switch::mnaCompApplyRightSideVectorStamp(Matrix& rightVector) { }
+
+void EMT::Ph1::Switch::mnaCompAddPostStepDependencies(AttributeBase::List &prevStepDependencies, AttributeBase::List &attributeDependencies, AttributeBase::List &modifiedAttributes, Attribute<Matrix>::Ptr &leftVector) {
+	attributeDependencies.push_back(leftVector);
+	modifiedAttributes.push_back(mIntfVoltage);
+	modifiedAttributes.push_back(mIntfCurrent);
+}
+
+void EMT::Ph1::Switch::mnaCompPostStep(Real time, Int timeStepCount, Attribute<Matrix>::Ptr &leftVector) {
+	mnaCompUpdateVoltage(**leftVector);
+	mnaCompUpdateCurrent(**leftVector);
+}
+
+void EMT::Ph1::Switch::mnaCompUpdateVoltage(const Matrix& leftVector) {
+	// Voltage across component is defined as V1 - V0
+	**mIntfVoltage = Matrix::Zero(1, 1);
+	if (terminalNotGrounded(1)) {
+		(**mIntfVoltage)(0, 0) = Math::realFromVectorElement(leftVector, matrixNodeIndex(1, 0));
+	}
+	if (terminalNotGrounded(0)) {
+		(**mIntfVoltage)(0, 0) = (**mIntfVoltage)(0, 0) - Math::realFromVectorElement(leftVector, matrixNodeIndex(0, 0));
+	}
+}
+
+void EMT::Ph1::Switch::mnaCompUpdateCurrent(const Matrix& leftVector) {
+	(**mIntfCurrent)(0, 0) = (**mIsClosed) ?
+		(**mIntfVoltage)(0, 0)/(**mClosedResistance):
+		(**mIntfVoltage)(0, 0)/(**mOpenResistance);
+}
