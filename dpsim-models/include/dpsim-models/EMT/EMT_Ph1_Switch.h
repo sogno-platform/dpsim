f40ede3a97f66ba121775865e0d9f147de5482b7
Marvin Tollnitsch <marvin.tollnitsch@rwth-aachen.de>
2024-07-06 15:59:41 +0200

    Reformatted code using clang-format
    reformatted files:
    - EMT_Ph1_Switch.h
    - EMT_Ph1_Switch.cpp
    
    Signed-off-by: Marvin Tollnitsch <marvin.tollnitsch@rwth-aachen.de>

--- dpsim-models/include/dpsim-models/EMT/EMT_Ph1_Switch.h
+++ dpsim-models/include/dpsim-models/EMT/EMT_Ph1_Switch.h
@@ -8,61 +8,68 @@
 
 #pragma once
 
-#include <dpsim-models/MNASimPowerComp.h>
-#include <dpsim-models/Solver/MNASwitchInterface.h>
-#include <dpsim-models/Solver/MNAInterface.h>
+#include <dpsim-models/Base/Base_Ph1_Switch.h>
 #include <dpsim-models/Definitions.h>
 #include <dpsim-models/Logger.h>
-#include <dpsim-models/Base/Base_Ph1_Switch.h>
+#include <dpsim-models/MNASimPowerComp.h>
+#include <dpsim-models/Solver/MNAInterface.h>
+#include <dpsim-models/Solver/MNASwitchInterface.h>
 
 namespace CPS {
 namespace EMT {
 namespace Ph1 {
-	/// \brief One phase EMT switch
-	///
-	/// The switch can be opened and closed.
-	/// Each state has a specific resistance value.
-	class Switch :
-		public MNASimPowerComp<Real>,
-		public Base::Ph1::Switch,
-		public SharedFactory<Switch>,
-		public MNASwitchInterface {
+/// \brief One phase EMT switch
+///
+/// The switch can be opened and closed.
+/// Each state has a specific resistance value.
+class Switch : public MNASimPowerComp<Real>,
+               public Base::Ph1::Switch,
+               public SharedFactory<Switch>,
+               public MNASwitchInterface {
 
-	public:
-		/// Defines UID, name, component parameters and logging level
-		Switch(String uid, String name,	Logger::Level loglevel = Logger::Level::off);
-		/// Defines name, component parameters and logging level
-		Switch(String name, Logger::Level logLevel = Logger::Level::off)
-			: Switch(name, name, logLevel) { }
+public:
+  /// Defines UID, name, component parameters and logging level
+  Switch(String uid, String name, Logger::Level loglevel = Logger::Level::off);
+  /// Defines name, component parameters and logging level
+  Switch(String name, Logger::Level logLevel = Logger::Level::off)
+      : Switch(name, name, logLevel) {}
 
-		SimPowerComp<Real>::Ptr clone(String name) override;
+  SimPowerComp<Real>::Ptr clone(String name) override;
 
-		// #### General ####
-		/// Initializes component from power flow data
-		void initializeFromNodesAndTerminals(Real frequency) override;
+  // #### General ####
+  /// Initializes component from power flow data
+  void initializeFromNodesAndTerminals(Real frequency) override;
 
-		// #### General MNA section ####
-		void mnaCompInitialize(Real omega, Real timeStep, Attribute<Matrix>::Ptr leftVector) override;
-		/// Stamps system matrix
-		void mnaCompApplySystemMatrixStamp(SparseMatrixRow& systemMatrix) override;
-		/// Stamps right side (source) vector
-		void mnaCompApplyRightSideVectorStamp(Matrix& rightVector) override;
-		/// Update interface voltage from MNA system result
-		void mnaCompUpdateVoltage(const Matrix& leftVector) override;
-		/// Update interface current from MNA system result
-		void mnaCompUpdateCurrent(const Matrix& leftVector) override;
+  // #### General MNA section ####
+  void mnaCompInitialize(Real omega, Real timeStep,
+                         Attribute<Matrix>::Ptr leftVector) override;
+  /// Stamps system matrix
+  void mnaCompApplySystemMatrixStamp(SparseMatrixRow &systemMatrix) override;
+  /// Stamps right side (source) vector
+  void mnaCompApplyRightSideVectorStamp(Matrix &rightVector) override;
+  /// Update interface voltage from MNA system result
+  void mnaCompUpdateVoltage(const Matrix &leftVector) override;
+  /// Update interface current from MNA system result
+  void mnaCompUpdateCurrent(const Matrix &leftVector) override;
 
-		// #### MNA section for switches ####
-		/// Check if switch is closed
-		Bool mnaIsClosed() override;
-		/// Stamps system matrix considering the defined switch position
-		void mnaCompApplySwitchSystemMatrixStamp(Bool closed, SparseMatrixRow& systemMatrix, Int freqIdx) override;
-		/// MNA post step operations
-		void mnaCompPostStep(Real time, Int timeStepCount, Attribute<Matrix>::Ptr &leftVector) override;
-		
-		/// Add MNA post step dependencies
-		void mnaCompAddPostStepDependencies(AttributeBase::List &prevStepDependencies, AttributeBase::List &attributeDependencies, AttributeBase::List &modifiedAttributes, Attribute<Matrix>::Ptr &leftVector) override;
-	};
-}
-}
-}
+  // #### MNA section for switches ####
+  /// Check if switch is closed
+  Bool mnaIsClosed() override;
+  /// Stamps system matrix considering the defined switch position
+  void mnaCompApplySwitchSystemMatrixStamp(Bool closed,
+                                           SparseMatrixRow &systemMatrix,
+                                           Int freqIdx) override;
+  /// MNA post step operations
+  void mnaCompPostStep(Real time, Int timeStepCount,
+                       Attribute<Matrix>::Ptr &leftVector) override;
+
+  /// Add MNA post step dependencies
+  void
+  mnaCompAddPostStepDependencies(AttributeBase::List &prevStepDependencies,
+                                 AttributeBase::List &attributeDependencies,
+                                 AttributeBase::List &modifiedAttributes,
+                                 Attribute<Matrix>::Ptr &leftVector) override;
+};
+} // namespace Ph1
+} // namespace EMT
+} // namespace CPS
