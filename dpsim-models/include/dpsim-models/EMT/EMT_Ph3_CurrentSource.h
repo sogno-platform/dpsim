a8615ff6ca2eb58628de50bbdad877626cb5fcf3
Marvin Tollnitsch <marvin.tollnitsch@rwth-aachen.de>
2024-09-30 16:27:46 +0200

    Implementation and declaration adjustments for EMT Ph3 CurrentSource
    + add implementation of "setParameters(MatrixComp currentRef, Real srcFreq = 50.0)" method in "EMT_Ph3_CurrentSource.h". Arguments are reference current (complex static phasor, amplitude and initial angle) and frequency
    
    * rename "voltageRef" parameters to "currentRef" in all "setParameters()" method declarations in "EMT_Ph3_CurrentSource.h"
    * update "clone()" method to use the newly implemented "setParameters()" method in "EMT_Ph3_CurrentSource.cpp"
    
    Signed-off-by: Marvin Tollnitsch <marvin.tollnitsch@rwth-aachen.de>

--- dpsim-models/include/dpsim-models/EMT/EMT_Ph3_CurrentSource.h
+++ dpsim-models/include/dpsim-models/EMT/EMT_Ph3_CurrentSource.h
@@ -47,13 +47,13 @@ public:
   // #### General ####
   /// Initializes component from power flow data
   void initializeFromNodesAndTerminals(Real frequency) override;
-  /// Setter for reference voltage
-  void setParameters(MatrixComp voltageRef, Real srcFreq = 50.0);
+  /// Setter for reference current
+  void setParameters(MatrixComp currentRef, Real srcFreq = 50.0);
   /// Setter for reference signal of type frequency ramp
-  void setParameters(MatrixComp voltageRef, Real freqStart, Real rocof,
+  void setParameters(MatrixComp currentRef, Real freqStart, Real rocof,
                      Real timeStart, Real duration, bool smoothRamp = true);
   /// Setter for reference signal of type cosine frequency modulation
-  void setParameters(MatrixComp voltageRef, Real modulationFrequency,
+  void setParameters(MatrixComp currentRef, Real modulationFrequency,
                      Real modulationAmplitude, Real baseFrequency = 50.0,
                      bool zigzag = false);
 
