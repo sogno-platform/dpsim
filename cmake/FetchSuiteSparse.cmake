24a4fb1d8591ca658d180adbf246e36a8112864e
Steffen Vogel <post@steffenvogel.de>
2025-03-25 01:53:56 +0100

    chore(cmake): Rename "KLU" dependency to name of actual library "SuiteSparse"
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- cmake/FetchSuiteSparse.cmake
+++ cmake/FetchSuiteSparse.cmake
@@ -1,8 +1,9 @@
 include(FetchContent)
 
 FetchContent_Declare(
-	klu-module
+	suitesparse-module
 	URL https://github.com/dpsim-simulator/SuiteSparse/releases/download/release-v5.10.6/SuiteSparse-release-v5.10.6.tar.gz
 )
 
-FetchContent_MakeAvailable(klu-module)
+FetchContent_MakeAvailable(suitesparse-module)
+
