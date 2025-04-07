d0d3bac6c257f937aaf91b4292b26254dbc7f654
Leonardo Carreras <leonardo.carreras@eonerc.rwth-aachen.de>
2025-04-07 10:45:55 +0200

    Fixes in PFSolver
    Signed-off-by: Leonardo Carreras <leonardo.carreras@eonerc.rwth-aachen.de>

--- dpsim/src/PFSolver.cpp
+++ dpsim/src/PFSolver.cpp
@@ -324,15 +324,17 @@ void PFSolver::determineNodeBaseVoltages() {
       } else if (std::shared_ptr<CPS::SP::Ph1::Load> load =
                      std::dynamic_pointer_cast<CPS::SP::Ph1::Load>(comp)) {
         baseVoltage_ = load->attributeTyped<CPS::Real>("V_nom")->get();
-        mSLog->info("Choose base voltage of {}V to convert pu-solution of {}.",
-                    baseVoltage_, load->name(), node->name());
+        SPDLOG_LOGGER_INFO(
+            mSLog, "Choose base voltage of {}V to convert pu-solution of {}.",
+            baseVoltage_, load->name(), node->name());
         break;
       } else if (std::shared_ptr<CPS::SP::Ph1::NetworkInjection> extnet =
                      std::dynamic_pointer_cast<CPS::SP::Ph1::NetworkInjection>(
                          comp)) {
         baseVoltage_ = extnet->attributeTyped<CPS::Real>("base_Voltage")->get();
-        mSLog->info("Choose base voltage of {}V to convert pu-solution of {}.",
-                    baseVoltage_, extnet->name(), node->name());
+        SPDLOG_LOGGER_INFO(
+            mSLog, "Choose base voltage of {}V to convert pu-solution of {}.",
+            baseVoltage_, extnet->name(), node->name());
         break;
       } else {
         SPDLOG_LOGGER_WARN(mSLog, "Unable to get base voltage at {}",
