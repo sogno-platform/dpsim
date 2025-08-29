02d1fac3073bc8dfcae418e5d965972f22cd2582
Leonardo Carreras <leonardo.carreras@eonerc.rwth-aachen.de>
2025-08-29 19:04:28 +0200

    Reorganize structure of module
    Now the module has his own Config.h to expose options. This will allow in the future to have more control on the compilation of the modules and examples.
    
    A file that lists the intefaces is now the main include point of the module.
    
    The loading of the module dpsim-villas is done now before dpsim, to make it available as done with dpsim-models.
    
    Signed-off-by: Leonardo Carreras <leonardo.carreras@eonerc.rwth-aachen.de>

---
+++ dpsim-villas/include/dpsim-villas/Interfaces.h
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2025 Institute for Automation of Complex Power Systems, EONERC, RWTH Aachen University
+// SPDX-License-Identifier: MPL-2.0
+
+#pragma once
+
+#include <dpsim-villas/Config.h>
+
+#include <dpsim-villas/InterfaceVillas.h>
+#include <dpsim-villas/InterfaceVillasQueueless.h>
