fbeadb72842bf833eb0bda19cda7fab808be242f
Marvin Tollnitsch <marvin.tollnitsch@rwth-aachen.de>
2025-10-08 17:22:27 +0200

    Added SSN model of Ph1 serial RLC circuit and generalized two terminal SSN models
    + added "EMT::Ph1::SSN::Full_Serial_RLC" class with "EMT_Ph1_SSN_Full_Serial_RLC.h" and "EMT_Ph1_SSN_Full_Serial_RLC.cpp"
    + added "EMT::Ph1::SSNTypeI2T" class with "EMT_Ph1_SSNTypeI2T.h" and "EMT_Ph1_SSNTypeI2T.cpp" for a generalized model of an arbitrary two terminal I-type SSN component
    + added "EMT::Ph1::SSNTypeV2T" class with "EMT_Ph1_SSNTypeV2T.h" and "EMT_Ph1_SSNTypeV2T.cpp" for a generalized model of an arbitrary two terminal V-type SSN component
    
    + added above headers ("EMT_Ph1_SSN_Full_Serial_RLC.h", "EMT_Ph1_SSNTypeI2T.h", "EMT_Ph1_SSNTypeV2T.h") to "Components.h" to include them into the project
    + added above cpp files ("EMT_Ph1_SSN_Full_Serial_RLC.cpp", "EMT_Ph1_SSNTypeI2T.cpp", "EMT_Ph1_SSNTypeV2T.cpp") to the "CMakeLists.txt" for component models
    
    Signed-off-by: Marvin Tollnitsch <marvin.tollnitsch@rwth-aachen.de>

---
+++ dpsim-models/include/dpsim-models/EMT/EMT_Ph1_SSNTypeI2T.h
@@ -0,0 +1,96 @@
+/* Copyright 2017-2021 Institute for Automation of Complex Power Systems,
+ *                     EONERC, RWTH Aachen University
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *********************************************************************************/
+
+#pragma once
+
+#include <dpsim-models/MNASimPowerComp.h>
+#include <dpsim-models/Solver/MNAInterface.h>
+
+namespace CPS {
+namespace EMT {
+namespace Ph1 {
+/// \brief SSNTypeI2T
+/// Model for a one phase, two terminal I-type SSN component which can be represented using
+/// a state space equation system
+/// x' = A * x + B * u
+/// y = C * x + D * u
+/// with x: state vector, y: output vector, u: input vector,
+/// where u represents external current (mIntfCurrent),
+/// y represents external voltage (mIntfVoltage),
+/// x represents any component states.
+class SSNTypeI2T : public MNASimPowerComp<Real>,
+                   public SharedFactory<SSNTypeI2T> {
+protected:
+  Matrix mX;
+  Matrix mU;
+  Matrix mUOld;
+  Matrix mW;
+  Matrix mY_hist;
+
+public:
+  const CPS::Attribute<Matrix>::Ptr mA;
+  const CPS::Attribute<Matrix>::Ptr mB;
+  const CPS::Attribute<Matrix>::Ptr mC;
+  const CPS::Attribute<Matrix>::Ptr mD;
+
+  const CPS::Attribute<Matrix>::Ptr mdA;
+  const CPS::Attribute<Matrix>::Ptr mdB;
+  const CPS::Attribute<Matrix>::Ptr mdC;
+
+  /// Defines UID, name, component parameters and logging level
+  SSNTypeI2T(String uid, String name,
+             Logger::Level logLevel = Logger::Level::off);
+  /// Defines name and logging level
+  SSNTypeI2T(String name, Logger::Level logLevel = Logger::Level::off)
+      : SSNTypeI2T(name, name, logLevel) {}
+
+  SimPowerComp<Real>::Ptr clone(String name) override;
+
+  void manualInit(Matrix initialState, Matrix initialInput,
+                  Matrix initialOldInput, Real initCurr, Real initVol);
+  void ssnUpdateState();
+  void setSSNMatricesToZero();
+  // #### General ####
+  void setParameters(const Matrix A, const Matrix B, const Matrix C,
+                     const Matrix D);
+  /// Initializes component from power flow data
+  void initializeFromNodesAndTerminals(Real frequency) override;
+
+  // #### MNA section ####
+  /// Initializes internal variables of the component
+  void mnaCompInitialize(Real omega, Real timeStep,
+                         Attribute<Matrix>::Ptr leftVector) override;
+  /// Stamps system matrix
+  void mnaCompApplySystemMatrixStamp(SparseMatrixRow &systemMatrix) override;
+  /// Stamps right side (source) vector
+  void mnaCompApplyRightSideVectorStamp(Matrix &rightVector) override;
+  /// Update interface voltage from MNA system result
+  void mnaCompUpdateVoltage(const Matrix &leftVector) override;
+  /// Update interface current from MNA system result
+  void mnaCompUpdateCurrent(const Matrix &leftVector) override;
+
+  void mnaCompPreStep(Real time, Int timeStepCount) override;
+  void mnaCompPostStep(Real time, Int timeStepCount,
+                       Attribute<Matrix>::Ptr &leftVector) override;
+
+  /// Add MNA pre step dependencies
+  void mnaCompAddPreStepDependencies(
+      AttributeBase::List &prevStepDependencies,
+      AttributeBase::List &attributeDependencies,
+      AttributeBase::List &modifiedAttributes) override;
+
+  /// Add MNA post step dependencies
+  void
+  mnaCompAddPostStepDependencies(AttributeBase::List &prevStepDependencies,
+                                 AttributeBase::List &attributeDependencies,
+                                 AttributeBase::List &modifiedAttributes,
+                                 Attribute<Matrix>::Ptr &leftVector) override;
+};
+} // namespace Ph1
+} // namespace EMT
+} // namespace CPS
