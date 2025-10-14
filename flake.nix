fcea4198f0c9b908ad53cb17833bdcc2207f002f
Leonardo Carreras <leonardo.carreras@eonerc.rwth-aachen.de>
2025-10-14 19:46:51 +0200

    Implement suggestions regarding cmake, nix and dockerfiles
    Signed-off-by: Leonardo Carreras <leonardo.carreras@eonerc.rwth-aachen.de>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "DPsim - Solver library for dynamic power system simulation";
 
   inputs = {
-    nixpkgs.url     = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     villas-node = {
       url = "github:VILLASframework/node";
