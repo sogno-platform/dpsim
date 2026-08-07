95b90235f38ac3cb31f6d9c4304644ef222dcc40
Leonardo Carreras <leonardo.carreras@eonerc.rwth-aachen.de>
2026-08-07 21:58:37 +0200

    chore(models): add SPDX headers and CODEOWNERS entries for the half decoupling line
    Signed-off-by: Leonardo Carreras <leonardo.carreras@eonerc.rwth-aachen.de>

--- dpsim-models/src/EMT/EMT_Ph3_HalfDecouplingLine.cpp
+++ dpsim-models/src/EMT/EMT_Ph3_HalfDecouplingLine.cpp
@@ -1,10 +1,5 @@
-/* Copyright 2017-2020 Institute for Automation of Complex Power Systems,
- *                     EONERC, RWTH Aachen University
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
- *********************************************************************************/
+// SPDX-FileCopyrightText: 2026 Institute for Automation of Complex Power Systems, EONERC, RWTH Aachen University
+// SPDX-License-Identifier: MPL-2.0
 
 #include <dpsim-models/EMT/EMT_Ph3_HalfDecouplingLine.h>
 #include <dpsim-models/MathUtils.h>
